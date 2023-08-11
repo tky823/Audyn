28204e9fbeee9039b4c555b711150f0c0569a835
tky823 <tky823.main+github@gmail.com>
2023-08-12 02:42:24 +0900

    [recipes] Support complex system config for DDP

--- recipes/_common/is_distributed.py
+++ recipes/_common/is_distributed.py
@@ -1,11 +1,30 @@
 import hydra
+import torch
 from omegaconf import DictConfig
 
 
 @hydra.main(version_base="1.2", config_path=None, config_name="config")
 def main(config: DictConfig) -> None:
-    is_distributed = str(config.system.distributed.enable)
-    print(is_distributed.lower())
+    if torch.cuda.is_available():
+        num_gpus = torch.cuda.device_count()
+        availability = str(config.system.distributed.enable).lower()
+
+        if num_gpus > 1:
+            if availability == "false":
+                raise ValueError(
+                    "Set config.system.distributed.enable=true for multi GPU training."
+                )
+            else:
+                is_distributed = True
+        else:
+            if availability == "true":
+                is_distributed = True
+            else:
+                is_distributed = False
+    else:
+        is_distributed = False
+
+    print(str(is_distributed).lower())
 
 
 if __name__ == "__main__":
