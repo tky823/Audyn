893e6d6ecc149baf256d7fd8463837d873efa3d5
tky823 <tky823.main+github@gmail.com>
2023-08-12 16:55:23 +0900

    [src] Add FeatToWaveTrainer and FeatToWaveGenerator to __all__

--- audyn/utils/driver/__init__.py
+++ audyn/utils/driver/__init__.py
@@ -1,6 +1,9 @@
 from .base import BaseGenerator, BaseTrainer
+from .feat_to_wave import FeatToWaveGenerator, FeatToWaveTrainer
 
 __all__ = [
     "BaseTrainer",
     "BaseGenerator",
+    "FeatToWaveTrainer",
+    "FeatToWaveGenerator",
 ]
