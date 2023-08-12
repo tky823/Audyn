a2afe338f1891f197503c105f0c7fe796c4b94da
tky823 <tky823.main+github@gmail.com>
2023-08-12 19:52:14 +0900

    [recipes] Add train.py for training of WaveNet

---
+++ recipes/LJSpeech/WaveNet/local/train.py
@@ -0,0 +1,117 @@
+import functools
+from typing import Any, Dict, List
+
+import hydra
+import torch
+import torchaudio.functional as aF
+from omegaconf import DictConfig
+
+import audyn
+from audyn.utils.data import (
+    BaseDataLoaders,
+    default_collate_fn,
+    slice_feautures,
+    take_log_features,
+)
+from audyn.utils.distributed import is_distributed, setup_distributed
+from audyn.utils.driver import FeatToWaveTrainer
+from audyn.utils.model import set_device
+
+
+@audyn.main()
+def main(config: DictConfig) -> None:
+    if is_distributed(config.system):
+        setup_distributed(config.system)
+
+    torch.manual_seed(config.system.seed)
+
+    train_dataset = hydra.utils.instantiate(config.train.dataset.train)
+    validation_dataset = hydra.utils.instantiate(config.train.dataset.validation)
+
+    train_loader = hydra.utils.instantiate(
+        config.train.dataloader.train,
+        train_dataset,
+        collate_fn=functools.partial(collate_fn, data_config=config.data, random_slice=True),
+    )
+    validation_loader = hydra.utils.instantiate(
+        config.train.dataloader.validation,
+        validation_dataset,
+        collate_fn=functools.partial(collate_fn, data_config=config.data, random_slice=False),
+    )
+    loaders = BaseDataLoaders(train_loader, validation_loader)
+
+    model = hydra.utils.instantiate(config.model)
+    model = set_device(
+        model,
+        accelerator=config.system.accelerator,
+        is_distributed=config.system.distributed.enable,
+    )
+    optimizer = hydra.utils.instantiate(config.optimizer, model.parameters())
+    lr_scheduler = hydra.utils.instantiate(config.lr_scheduler, optimizer)
+    criterion = hydra.utils.instantiate(config.criterion)
+    criterion = set_device(
+        criterion,
+        accelerator=config.system.accelerator,
+        is_distributed=config.system.distributed.enable,
+    )
+
+    trainer = FeatToWaveTrainer(
+        loaders,
+        model,
+        optimizer,
+        lr_scheduler=lr_scheduler,
+        criterion=criterion,
+        config=config,
+    )
+    trainer.run()
+
+
+def collate_fn(batch: List[Any], data_config: DictConfig, random_slice: bool) -> Dict[str, Any]:
+    quantization_channels = data_config.quantization_channels
+
+    dict_batch = default_collate_fn(batch)
+
+    batch_size_keys = sorted(list(dict_batch.keys()))
+    batch_size_key = batch_size_keys[0]
+    batch_size = len(dict_batch[batch_size_key])
+
+    dict_batch = slice_feautures(
+        dict_batch,
+        slice_length=data_config.slice_length,
+        key_mapping={
+            "waveform": "waveform_slice",
+            "waveform_mulaw": "waveform_slice_mulaw",
+            "melspectrogram": "melspectrogram_slice",
+        },
+        hop_lengths={
+            "waveform": 1,
+            "waveform_mulaw": 1,
+            "melspectrogram": data_config.melspectrogram.hop_length,
+        },
+        length_mapping={
+            "waveform": "waveform_length",
+            "waveform_mulaw": "waveform_length",
+            "melspectrogram": "melspectrogram_length",
+        },
+        random_slice=random_slice,
+    )
+    dict_batch = take_log_features(
+        dict_batch,
+        key_mapping={
+            "melspectrogram": "log_melspectrogram",
+            "melspectrogram_slice": "log_melspectrogram_slice",
+        },
+    )
+
+    dict_batch["initial_waveform"] = torch.zeros((batch_size, 1), dtype=torch.float)
+    dict_batch["initial_waveform_mulaw"] = aF.mu_law_encoding(
+        dict_batch["initial_waveform"],
+        quantization_channels=quantization_channels,
+    )
+    dict_batch["max_waveform_slice_length"] = dict_batch["waveform_slice"].size(-1)
+
+    return dict_batch
+
+
+if __name__ == "__main__":
+    main()
