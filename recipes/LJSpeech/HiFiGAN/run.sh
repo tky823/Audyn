c6b627b00236d88ca702f580e9d8d001ad50c858
tky823 <tky823.main+github@gmail.com>
2023-09-04 21:54:30 +0900

    [recipes] Add recipes for HiFiGAN

---
+++ recipes/LJSpeech/HiFiGAN/run.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+stage=-1
+stop_stage=-1
+
+tag=""
+continue_from=""
+
+exp_dir="./exp"
+
+ljspeech_url="https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2"
+data_root="../data"
+dump_root="dump"
+
+system="defaults"
+preprocess="defaults"
+data="hifigan"
+train="hifigan"
+model="hifigan_v1"
+optimizer="hifigan"
+lr_scheduler="hifigan"
+criterion="hifigan"
+
+n_validation=500
+n_test=500
+
+. ../../_common/parse_options.sh || exit 1;
+
+ljspeech_root="${data_root}/LJSpeech-1.1"
+
+if [ ${stage} -le -1 ] && [ ${stop_stage} -ge -1 ]; then
+    echo "Stage -1"
+
+    (
+        . ../_common/download.sh \
+        --ljspeech-url "${ljspeech_url}" \
+        --data-root "${data_root}"
+    )
+fi
+
+if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
+    echo "Stage 0: Preprocessing"
+
+    (
+        . ./preprocess.sh \
+        --stage 1 \
+        --stop-stage 2 \
+        --data-root "${data_root}" \
+        --dump-root "${dump_root}" \
+        --preprocess "${preprocess}" \
+        --data "${data}" \
+        --n-validation ${n_validation} \
+        --n-test ${n_test}
+    )
+fi
+
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+    echo "Stage 1: Training"
+
+    (
+        . ./train.sh \
+        --tag "${tag}" \
+        --continue-from "${continue_from}" \
+        --exp-dir "${exp_dir}" \
+        --dump-root "${dump_root}" \
+        --system "${system}" \
+        --preprocess "${preprocess}" \
+        --data "${data}" \
+        --train "${train}" \
+        --model "${model}" \
+        --optimizer "${optimizer}" \
+        --lr_scheduler "${lr_scheduler}" \
+        --criterion "${criterion}"
+    )
+fi
