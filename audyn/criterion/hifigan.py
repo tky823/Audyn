3bd1c23f5453a6ad761e5f7dfb2b3071aa98b269
tky823 <tky823.main+github@gmail.com>
2023-09-05 01:34:24 +0900

    [src] Add MSELoss for HiFi-GAN

---
+++ audyn/criterion/hifigan.py
@@ -0,0 +1,59 @@
+from typing import Any, List, Union
+
+import torch
+import torch.nn as nn
+
+from .lsgan import MSELoss as BaseMSELoss
+
+__all__ = ["FeatureMatchingLoss"]
+
+
+class MSELoss(BaseMSELoss):
+    """Extension of MSELoss used for HiFi-GAN.
+
+    This criterion receives both list objects.
+    """
+
+    def __init__(self, target: float, reduction: str = "mean") -> None:
+        super().__init__(target, reduction=reduction)
+
+    def forward(self, input: Union[torch.Tensor, List[Any]]) -> torch.Tensor:
+        loss = 0
+
+        if type(input) is list:
+            for idx in range(len(input)):
+                loss = loss + super().forward(input[idx])
+        elif isinstance(input, torch.Tensor):
+            loss = loss + super().forward(input)
+        else:
+            raise TypeError("Invalid type {} is found.".format(type(input)))
+
+        return loss
+
+
+class FeatureMatchingLoss(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(
+        self, input: List[List[torch.Tensor]], target: List[List[torch.Tensor]]
+    ) -> torch.Tensor:
+        loss = self._forward(input, target)
+
+        return loss
+
+    def _forward(
+        self, input: Union[torch.Tensor, List], target: Union[torch.Tensor, List]
+    ) -> torch.Tensor:
+        loss = 0
+
+        if isinstance(input, torch.Tensor) or isinstance(target, torch.Tensor):
+            loss = torch.abs(input - target)
+            loss = loss.mean()
+        elif isinstance(input, list) or isinstance(target, list):
+            for _input, _target in zip(input, target):
+                loss = loss + self._forward(_input, _target)
+        else:
+            raise TypeError("Invalid type ({} and {}) is given.".format(type(input), type(target)))
+
+        return loss
---
+++ audyn/criterion/lsgan.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+
+class MSELoss(nn.Module):
+    def __init__(self, target: float, reduction: str = "mean") -> None:
+        super().__init__()
+
+        self.target = target
+        self.reduction = reduction
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        loss = (input - self.target) ** 2
+
+        if self.reduction == "mean":
+            loss = loss.mean()
+        elif self.reduction == "sum":
+            loss = loss.sum()
+        elif self.reduction != "none":
+            raise ValueError("Invalid redution.")
+
+        return loss
