f70e632e17e14288acf991c2cc76c6ffce941d13
tky823 <tky823.main+github@gmail.com>
2023-09-09 01:00:49 +0900

    [tests] Add test for ActNorm1d

--- tests/package/modules/test_glow.py
+++ tests/package/modules/test_glow.py
@@ -1,6 +1,6 @@
 import torch
 
-from audyn.modules.glow import InvertiblePointwiseConv1d, InvertiblePointwiseConv2d
+from audyn.modules.glow import ActNorm1d, InvertiblePointwiseConv1d, InvertiblePointwiseConv2d
 
 
 def test_invertible_pointwise_conv1d():
@@ -76,3 +76,40 @@ def test_invertible_pointwise_conv2d():
     assert z_logdet.size() == (batch_size,)
     assert torch.allclose(output, input, atol=1e-7)
     assert torch.allclose(logdet, zeros, atol=1e-7)
+
+
+def test_act_norm1d() -> None:
+    torch.manual_seed(0)
+
+    batch_size = 2
+    num_features = 6
+    length = 16
+
+    model = ActNorm1d(num_features)
+    input = torch.randn(batch_size, num_features, length)
+
+    z = model(input)
+    output = model(z, reverse=True)
+
+    assert output.size() == input.size()
+    assert z.size() == input.size()
+    assert torch.allclose(output, input)
+
+    zeros = torch.zeros((batch_size,))
+
+    z, z_logdet = model(
+        input,
+        logdet=zeros,
+    )
+    output, logdet = model(
+        z,
+        logdet=z_logdet,
+        reverse=True,
+    )
+
+    assert output.size() == input.size()
+    assert logdet.size() == (batch_size,)
+    assert z.size() == input.size()
+    assert z_logdet.size() == (batch_size,)
+    assert torch.allclose(output, input)
+    assert torch.allclose(logdet, zeros)
