46363c65104cec5646317fcf3f16dee48ce12139
tky823 <tky823.main+github@gmail.com>
2023-09-08 21:47:34 +0900

    [src] Set seed and backends

--- audyn/utils/__init__.py
+++ audyn/utils/__init__.py
@@ -1,6 +1,7 @@
 import torch
 from omegaconf import DictConfig
 
+from .data import select_accelerator
 from .distributed import is_distributed, setup_distributed
 from .hydra.utils import instantiate_cascade_text_to_wave, instantiate_model
 
@@ -23,7 +24,18 @@ def setup_system(config: DictConfig) -> None:
     else:
         system_config = config
 
+    accelerator = select_accelerator(system_config)
+
+    if accelerator == "gpu":
+        from torch.backends import cudnn
+
+        cudnn.benchmark = True
+        cudnn.deterministic = False
+
     if is_distributed(system_config):
         setup_distributed(system_config)
 
     torch.manual_seed(system_config.seed)
+
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(system_config.seed)
