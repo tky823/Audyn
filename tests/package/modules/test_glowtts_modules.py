758e8055755e690aee9fcb5ca8ee2c11dd784fd1
tky823 <tky823.main+github@gmail.com>
2023-09-09 22:19:52 +0900

    [tests] Add test for MaskedInvertiblePointwiseConv1d

--- tests/package/modules/test_glowtts_modules.py
+++ tests/package/modules/test_glowtts_modules.py
@@ -1,7 +1,7 @@
 import torch
 
-from audyn.modules.glow import ActNorm1d
-from audyn.modules.glowtts import MaskedActNorm1d
+from audyn.modules.glow import ActNorm1d, InvertiblePointwiseConv1d
+from audyn.modules.glowtts import MaskedActNorm1d, MaskedInvertiblePointwiseConv1d
 
 
 def test_masked_act_norm1d() -> None:
@@ -116,6 +116,95 @@ def test_masked_act_norm1d() -> None:
 
     masked_model = MaskedActNorm1d(num_features)
     non_masked_model = ActNorm1d(num_features)
+
+    input = torch.randn(batch_size, num_features, max_length)
+
+    masked_z = masked_model(input)
+    masked_output = masked_model(masked_z, reverse=True)
+    non_masked_z = non_masked_model(input)
+    non_masked_output = non_masked_model(non_masked_z, reverse=True)
+
+    assert torch.allclose(masked_z, non_masked_z)
+    assert torch.allclose(masked_output, non_masked_output)
+
+    zeros = torch.zeros((batch_size,))
+
+    masked_z, masked_z_logdet = masked_model(
+        input,
+        logdet=zeros,
+    )
+    masked_output, masked_logdet = masked_model(
+        masked_z,
+        logdet=masked_z_logdet,
+        reverse=True,
+    )
+    non_masked_z, non_masked_z_logdet = non_masked_model(
+        input,
+        logdet=zeros,
+    )
+    non_masked_output, non_masked_logdet = non_masked_model(
+        non_masked_z,
+        logdet=non_masked_z_logdet,
+        reverse=True,
+    )
+
+    assert torch.allclose(masked_z, non_masked_z)
+    assert torch.allclose(masked_output, non_masked_output)
+    assert torch.allclose(masked_logdet, non_masked_logdet)
+
+
+def test_masked_invertible_pointwise_conv1d() -> None:
+    torch.manual_seed(0)
+
+    batch_size = 2
+    num_features = 6
+    max_length = 16
+
+    # w/ 2D padding mask
+    model = MaskedInvertiblePointwiseConv1d(num_features)
+
+    length = torch.randint(1, max_length + 1, (batch_size,), dtype=torch.long)
+    max_length = torch.max(length)
+    input = torch.randn((batch_size, num_features, max_length))
+    padding_mask = torch.arange(max_length) >= length.unsqueeze(dim=-1)
+
+    input = input.masked_fill(padding_mask.unsqueeze(dim=1), 0)
+    z = model(input, padding_mask=padding_mask)
+    output = model(z, padding_mask=padding_mask, reverse=True)
+
+    assert output.size() == input.size()
+    assert z.size() == input.size()
+    assert torch.allclose(output, input, atol=1e-7)
+
+    zeros = torch.zeros((batch_size,))
+
+    z, z_logdet = model(
+        input,
+        padding_mask=padding_mask,
+        logdet=zeros,
+    )
+    output, logdet = model(
+        z,
+        padding_mask=padding_mask,
+        logdet=z_logdet,
+        reverse=True,
+    )
+
+    assert output.size() == input.size()
+    assert logdet.size() == (batch_size,)
+    assert z.size() == input.size()
+    assert z_logdet.size() == (batch_size,)
+    assert torch.allclose(output, input, atol=1e-7)
+    assert torch.allclose(logdet, zeros)
+
+    # w/o padding mask
+    batch_size = 2
+    num_features = 6
+    max_length = 16
+
+    masked_model = MaskedInvertiblePointwiseConv1d(num_features)
+    non_masked_model = InvertiblePointwiseConv1d(num_features)
+    non_masked_model.weight.data.copy_(masked_model.weight.data.detach())
 
     input = torch.randn(batch_size, num_features, max_length)
 
