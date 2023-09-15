80ae4fbf7760847783094ebd16ec3dadbe3815d0
tky823 <tky823.main+github@gmail.com>
2023-09-15 23:17:30 +0900

    [src] Add TextToWaveTrainer to __all__

--- audyn/utils/driver/__init__.py
+++ audyn/utils/driver/__init__.py
@@ -2,13 +2,14 @@ from .base import BaseGenerator, BaseTrainer
 from .feat_to_wave import FeatToWaveGenerator, FeatToWaveTrainer
 from .gan import GANTrainer
 from .text_to_feat import TextToFeatTrainer
-from .text_to_wave import CascadeTextToWaveGenerator
+from .text_to_wave import CascadeTextToWaveGenerator, TextToWaveTrainer
 
 __all__ = [
     "BaseTrainer",
     "BaseGenerator",
     "TextToFeatTrainer",
     "FeatToWaveTrainer",
+    "TextToWaveTrainer",
     "FeatToWaveGenerator",
     "CascadeTextToWaveGenerator",
     "GANTrainer",
