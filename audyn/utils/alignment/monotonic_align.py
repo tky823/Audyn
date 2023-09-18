972845a0031bd7f5db179567b349d2e53a6f5be6
tky823 <tky823.main+github@gmail.com>
2023-09-18 13:56:59 +0900

    [src] Add padding mask option to search_monotonic_alignment_by_viterbi

--- audyn/utils/alignment/monotonic_align.py
+++ audyn/utils/alignment/monotonic_align.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import torch
 
 from ..._cpp_extensions import monotonic_align as monotonic_align_cpp
@@ -8,12 +10,15 @@ __all__ = ["search_monotonic_alignment_by_viterbi"]
 @torch.no_grad()
 def search_monotonic_alignment_by_viterbi(
     probs: torch.Tensor,
+    padding_mask: Optional[torch.BoolTensor] = None,
     take_log: bool = False,
 ) -> torch.LongTensor:
     """Search monotonic alignment by Viterbi algorithm.
 
     Args:
         probs (torch.Tensor): Soft alignment matrix of shape (batch_size, tgt_length, src_length).
+        padding_mask (torch.Tensor): Padding mask to ignore padding grids.
+            The shape is (batch_size, tgt_length, src_length).
         take_log (bool): If ``True``, log of ``probs`` is treated as log probability.
             Otherwise, ``probs`` is treated as log probability. Default: ``False``.
 
@@ -21,6 +26,14 @@ def search_monotonic_alignment_by_viterbi(
         torch.LongTensor: Hard alignment matrix of shape (batch_size, tgt_length, src_length).
 
     """
+    if padding_mask is not None:
+        if take_log:
+            # Set p(x) as 0.
+            probs = probs.masked_fill(padding_mask, 0)
+        else:
+            # Set log(p(x)) as -inf.
+            probs = probs.masked_fill(padding_mask, -float("inf"))
+
     hard_alignment = monotonic_align_cpp.search_monotonic_alignment_by_viterbi(probs, take_log)
 
     return hard_alignment
