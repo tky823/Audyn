4e05aba2f5ac38444ff0729a0954d351127fdd77
tky823 <tky823.main+github@gmail.com>
2023-09-24 11:25:37 +0900

    [tests] Add test for variable input in MaskedLayerNorm

--- tests/package/modules/test_normalization.py
+++ tests/package/modules/test_normalization.py
@@ -8,10 +8,7 @@ from audyn.modules.normalization import MaskedLayerNorm
 def test_masked_layer_norm() -> None:
     torch.manual_seed(0)
 
-    batch_size, length, num_features = 4, 8, 3
-
-    input = torch.randn((batch_size, length, num_features))
-    padding_mask = torch.zeros((batch_size, length, num_features), dtype=torch.bool)
+    batch_size, max_length, num_features = 4, 8, 3
 
     layer_norm = nn.LayerNorm((num_features,))
     masked_layer_norm = MaskedLayerNorm((num_features,))
@@ -22,19 +19,32 @@ def test_masked_layer_norm() -> None:
     masked_layer_norm.weight.data.copy_(layer_norm.weight.data)
     masked_layer_norm.bias.data.copy_(layer_norm.bias.data)
 
-    output = layer_norm(input)
-    masked_output = masked_layer_norm(input, padding_mask=padding_mask)
+    length = torch.randint(1, max_length, (batch_size,))
+    max_length = torch.max(length).item()
+
+    input = torch.randn((batch_size, max_length, num_features))
+    padding_mask = torch.arange(max_length) >= length.unsqueeze(dim=-1)
+    masked_input = input.masked_fill(padding_mask.unsqueeze(dim=-1), 0)
+
+    output = []
+
+    for _input, _length in zip(input, length):
+        _length = _length.item()
+        _input = F.pad(_input, (0, 0, 0, _length - max_length))
+        _output = layer_norm(_input)
+        _output = F.pad(_output, (0, 0, 0, max_length - _length))
+        output.append(_output)
+
+    output = torch.stack(output, dim=0)
+
+    masked_output = masked_layer_norm(masked_input, padding_mask=padding_mask.unsqueeze(dim=-1))
 
     assert torch.allclose(output, masked_output)
 
-    actual_length = length - 2
-    masked_input = input
-    input = F.pad(input, (0, 0, 0, actual_length - length))
-    padding_mask = torch.arange(length) >= actual_length
+    input = torch.randn((batch_size, max_length, num_features))
+    padding_mask = torch.zeros((batch_size, max_length, num_features), dtype=torch.bool)
 
     output = layer_norm(input)
-    masked_output = masked_layer_norm(masked_input, padding_mask=padding_mask.unsqueeze(dim=-1))
-
-    output = F.pad(output, (0, 0, 0, length - actual_length))
+    masked_output = masked_layer_norm(input, padding_mask=padding_mask)
 
     assert torch.allclose(output, masked_output)
