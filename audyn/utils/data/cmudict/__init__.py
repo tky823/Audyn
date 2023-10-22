97a25411b1f63794c9acdb0901180c5321243d42
tky823 <tky823.main+github@gmail.com>
2023-10-23 01:09:45 +0900

    [src] Create CMUDict class

--- audyn/utils/data/cmudict/__init__.py
+++ audyn/utils/data/cmudict/__init__.py
@@ -1,3 +1,10 @@
+import os
+import re
+import urllib
+from typing import Dict, List, Optional
+
+from ....utils import audyn_cache_dir
+
 __all__ = [
     "symbols",
     "full_symbols",
@@ -6,6 +13,7 @@ __all__ = [
     "EOS_SYMBOL",
     "SPECIALS",
     "vocab_size",
+    "CMUDict",
 ]
 
 
@@ -102,3 +110,81 @@ SPECIALS = ["<PAD>"]
 full_symbols = symbols + BREAK_SYMBOLS + [BOS_SYMBOL] + [EOS_SYMBOL] + SPECIALS
 
 vocab_size = len(full_symbols)
+
+
+class CMUDict:
+    """Pronunciation dictionary using CMUDict.
+
+    For the detail about CMUDict,
+    see http://www.speech.cs.cmu.edu/cgi-bin/cmudict.
+    """
+
+    filename = "cmudict-0.7b"
+    cmudict_urls = ["https://svn.code.sf.net/p/cmusphinx/code/trunk/cmudict/cmudict-0.7b"]
+
+    def __init__(self, root: Optional[str] = None) -> None:
+        if root is None:
+            root = os.path.join(audyn_cache_dir, "data", "cmudict")
+
+        path = os.path.join(root, self.filename)
+
+        if not os.path.exists(path):
+            # TODO: additional sources
+            assert len(self.cmudict_urls) == 1
+
+            os.makedirs(root, exist_ok=True)
+
+            for url in self.cmudict_urls:
+                data = urllib.request.urlopen(url).read()
+
+                with open(path, mode="wb") as f:
+                    f.write(data)
+
+                break
+
+        self.cmudict = self.build_dict(path)
+
+    def __call__(self, word: str) -> str:
+        """Return phone from word."""
+        phone_candidates = self.cmudict.get(word.upper())
+
+        if phone_candidates is None:
+            phone = None
+        else:
+            phone = phone_candidates[0]
+
+        return phone
+
+    @staticmethod
+    def build_dict(path: str) -> Dict[str, List[str]]:
+        """Build pronunciation dictionary."""
+        cmudict = {}
+
+        with open(path, encoding="latin-1") as f:
+            for line in f:
+                line = line.strip()
+
+                if len(line) == 0:
+                    continue
+
+                if (line[0].isalpha() and line[0].isupper()) or line[0] == "'":
+                    valid_word = True
+                    word, pronunciation = line.split("  ")
+                    word = re.sub(r"\(\d+\)", "", word)
+                    phones = pronunciation.split(" ")
+
+                    for p in phones:
+                        if p not in symbols:
+                            valid_word = False
+
+                    if not valid_word:
+                        continue
+
+                    pronunciation = " ".join(phones)
+
+                    if word not in cmudict:
+                        cmudict[word] = []
+
+                    cmudict[word].append(pronunciation)
+
+        return cmudict
