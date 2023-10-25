a5bba65888bbdaa06894749ad961c8a92b74efcb
tky823 <tky823.main+github@gmail.com>
2023-10-25 23:47:55 +0900

    [tests] Add tests for slice_feautures

--- tests/package/utils/test_postprocess.py
+++ tests/package/utils/test_postprocess.py
@@ -1,3 +1,5 @@
+import math
+
 import pytest
 import torch
 
@@ -54,6 +56,65 @@ def test_slice_features(random_slice: bool) -> None:
     )
 
     assert torch.equal(batch["fine_slice"][:, 0], 2 * batch["coarse_slice"][:, 0])
+
+
+@pytest.mark.parametrize("random_slice", [True, False])
+def test_slice_features_length_dims(random_slice: bool) -> None:
+    torch.manual_seed(0)
+
+    batch_size = 4
+    slice_length = 5
+    max_fine_length = 4 * slice_length
+    key_mapping = {
+        "fine": "fine_slice",
+        "coarse": "coarse_slice",
+    }
+    hop_lengths = {
+        "fine": 1,
+        "coarse": 2,
+    }
+    length_mapping = {
+        "coarse": "coarse_length",
+    }
+    corse_length = torch.tensor(
+        [
+            max_fine_length // hop_lengths["coarse"],
+            (max_fine_length - 2) // hop_lengths["coarse"],
+            (max_fine_length - 4) // hop_lengths["coarse"],
+            (max_fine_length - 8) // hop_lengths["coarse"],
+        ],
+        dtype=torch.long,
+    )
+
+    batch = {
+        "fine": create_arange_batch(
+            max_fine_length,
+            batch_size=batch_size,
+        ),
+        "coarse": create_arange_batch(
+            max_fine_length // hop_lengths["coarse"],
+            batch_size=batch_size,
+        ),
+        "coarse_length": corse_length,
+    }
+    batch["coarse"] = batch["coarse"].unsqueeze(-1)
+    length_dims = {
+        "fine": -1,
+        "coarse": -2,
+    }
+
+    batch = slice_feautures(
+        batch,
+        slice_length,
+        key_mapping=key_mapping,
+        hop_lengths=hop_lengths,
+        length_mapping=length_mapping,
+        length_dims=length_dims,
+        random_slice=random_slice,
+    )
+
+    assert batch["fine_slice"].size(-1) == slice_length
+    assert batch["coarse_slice"].size(-2) == math.ceil(slice_length / hop_lengths["coarse"])
 
 
 def test_take_log_features() -> None:
