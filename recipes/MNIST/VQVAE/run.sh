2da49cb86b42618e6612e4fb5bd3eba04a8d9d23
tky823 <tky823.main+github@gmail.com>
2023-10-28 23:16:35 +0900

    [recipes] Add stage 3 to PixelCNN recipe

--- recipes/MNIST/VQVAE/run.sh
+++ recipes/MNIST/VQVAE/run.sh
@@ -68,3 +68,23 @@ if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
         --model "${model}"
     )
 fi
+
+if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
+    echo "Stage 3: Training of PixelCNN"
+
+    (
+        . ./train_pixelcnn.sh \
+        --tag "${tag}" \
+        --continue-from "${continue_from}" \
+        --exp-dir "${exp_dir}" \
+        --data-root "${data_root}" \
+        --system "${system}" \
+        --data "${data}" \
+        --train "${train}" \
+        --test "${test}" \
+        --model "${model}" \
+        --optimizer "${optimizer}" \
+        --lr_scheduler "${lr_scheduler}" \
+        --criterion "${criterion}"
+    )
+fi
