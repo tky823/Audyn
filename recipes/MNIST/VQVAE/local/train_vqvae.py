7605f8b9628afba787ca7b08906f6294ecb2aed9
tky823 <tky823.main+github@gmail.com>
2023-10-28 21:11:48 +0900

    [recipes] Add train_vqvae.py

---
+++ recipes/MNIST/VQVAE/local/train_vqvae.py
@@ -0,0 +1,107 @@
+import functools
+from typing import Dict, Iterable, List, Optional
+
+import hydra
+import torch
+from omegaconf import DictConfig
+
+import audyn
+from audyn.utils import instantiate_model, setup_system
+from audyn.utils.data import BaseDataLoaders, default_collate_fn
+from audyn.utils.driver import BaseTrainer
+from audyn.utils.model import set_device
+
+
+@audyn.main()
+def main(config: DictConfig) -> None:
+    setup_system(config)
+
+    train_dataset = hydra.utils.instantiate(config.train.dataset.train)
+    validation_dataset = hydra.utils.instantiate(config.train.dataset.validation)
+
+    down_scale = config.model.encoder.stride**config.model.encoder.num_layers
+    codebook_size = config.data.codebook.size
+
+    train_loader = hydra.utils.instantiate(
+        config.train.dataloader.train,
+        train_dataset,
+        collate_fn=functools.partial(
+            collate_fn,
+            down_scale=down_scale,
+            codebook_size=codebook_size,
+        ),
+    )
+    validation_loader = hydra.utils.instantiate(
+        config.train.dataloader.validation,
+        validation_dataset,
+        collate_fn=functools.partial(
+            collate_fn,
+            down_scale=down_scale,
+            codebook_size=codebook_size,
+        ),
+    )
+    loaders = BaseDataLoaders(train_loader, validation_loader)
+
+    model = instantiate_model(config.model)
+    model = set_device(
+        model,
+        accelerator=config.system.accelerator,
+        is_distributed=config.system.distributed.enable,
+    )
+
+    optimizer = hydra.utils.instantiate(config.optimizer, model.parameters())
+    lr_scheduler = hydra.utils.instantiate(config.lr_scheduler, optimizer)
+
+    criterion = hydra.utils.instantiate(config.criterion)
+    criterion = set_device(
+        criterion,
+        accelerator=config.system.accelerator,
+        is_distributed=config.system.distributed.enable,
+    )
+
+    trainer = BaseTrainer(
+        loaders,
+        model,
+        optimizer,
+        lr_scheduler=lr_scheduler,
+        criterion=criterion,
+        config=config,
+    )
+    trainer.run()
+
+
+def collate_fn(
+    list_batch: List[Dict[str, torch.Tensor]],
+    codebook_size: int,
+    down_scale: int,
+    keys: Optional[Iterable[str]] = None,
+) -> Dict[str, torch.Tensor]:
+    """Generate dict-based batch.
+
+    Args:
+        list_batch (list): Single batch to be collated.
+            Type of each data is expected ``Dict[str, torch.Tensor]``.
+        data_config (DictConfig): Config of data.
+        random_slice (bool): If ``random_slice=True``, waveform and
+            melspectrogram slices are selected at random. Default: ``True``.
+        std (float): Standard deviation of noise.
+
+    Returns:
+        Dict of batch.
+
+    """
+    dict_batch = default_collate_fn(list_batch, keys=keys)
+    batch_size, _, height, width = dict_batch["input"].size()
+
+    dict_batch["codebook_indices"] = torch.randint(
+        0,
+        codebook_size,
+        (batch_size, height // down_scale, width // down_scale),
+        dtype=torch.long,
+    )
+
+    return dict_batch
+
+
+if __name__ == "__main__":
+    main()
