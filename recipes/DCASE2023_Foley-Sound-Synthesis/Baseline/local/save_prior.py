1f7b1f4dcfeafa7fc47aeca4a0403c29bc21b198
tky823 <tky823.main+github@gmail.com>
2023-11-22 12:17:19 +0900

    [recipes] Fix shape of latent features in save_prior.py

--- recipes/DCASE2023_Foley-Sound-Synthesis/Baseline/local/save_prior.py
+++ recipes/DCASE2023_Foley-Sound-Synthesis/Baseline/local/save_prior.py
@@ -1,7 +1,9 @@
+import functools
 from typing import Any, Dict, Iterable, List, Optional
 
 import hydra
 import torch
+import torch.nn.functional as F
 from omegaconf import DictConfig
 from utils.driver import PriorSaver
 
@@ -15,11 +17,15 @@ from audyn.utils.model import set_device
 def main(config: DictConfig) -> None:
     setup_system(config)
 
+    down_scale = config.model.encoder.stride**config.model.encoder.num_stacks
+    n_frames = config.data.audio.length // config.data.melspectrogram.hop_length
+    n_frames = n_frames - (n_frames % down_scale)
+
     dataset = hydra.utils.instantiate(config.train.dataset)
     loader = hydra.utils.instantiate(
         config.train.dataloader,
         dataset,
-        collate_fn=collate_fn,
+        collate_fn=functools.partial(collate_fn, n_frames=n_frames),
     )
 
     model = instantiate_model(config.model)
@@ -35,6 +41,7 @@ def main(config: DictConfig) -> None:
 
 def collate_fn(
     list_batch: List[Dict[str, Any]],
+    n_frames: int,
     keys: Optional[Iterable[str]] = None,
 ) -> Dict[str, torch.Tensor]:
     """Generate dict-based batch.
@@ -42,6 +49,7 @@ def collate_fn(
     Args:
         list_batch (list): Single batch to be collated.
             Type of each data is expected ``Dict[str, torch.Tensor]``.
+        n_frames (int): Number of frames in Mel-spectrogram.
         keys (iterable, optional): Keys to generate batch.
             If ``None`` is given, all keys detected in ``batch`` are used.
             Default: ``None``.
@@ -51,6 +59,8 @@ def collate_fn(
 
     """
     dict_batch = default_collate_fn(list_batch, keys=keys)
+    n_frames_in = dict_batch["melspectrogram"].size(-1)
+    dict_batch["melspectrogram"] = F.pad(dict_batch["melspectrogram"], (0, n_frames - n_frames_in))
     dict_batch = take_log_features(
         dict_batch,
         key_mapping={
