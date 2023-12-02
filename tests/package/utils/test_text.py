2c956f2f70fc993ee9e84ffff734e0b84a181eff
tky823 <tky823.main+github@gmail.com>
2023-12-02 11:19:26 +0900

    [tests] Add error case of load_text

--- tests/package/utils/test_text.py
+++ tests/package/utils/test_text.py
@@ -1,6 +1,8 @@
 import os
 import tempfile
 
+import pytest
+
 from audyn.utils.text import load_text
 
 
@@ -23,3 +25,13 @@ def test_load_text() -> None:
         text = load_text(text_path)
 
         assert text == "All in the golden afternoon"
+
+        # error case: multiple lines
+        with open(text_path, mode="w") as f:
+            f.write("All in the golden afternoon\n")
+            f.write("Full leisurely we glide;\n")
+
+        with pytest.raises(ValueError) as e:
+            text = load_text(text_path)
+
+            assert str(e.value) == f"The text format of {text_path} is incorrect."
