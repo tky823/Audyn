92e4c2d5b09237fa90a0c20200eb1515c9f8b5c0
tky823 <tky823.main+github@gmail.com>
2023-12-02 14:36:37 +0900

    [src] Add abstract methods of normalizer and tokenizer

--- audyn/utils/text/normalization.py
+++ audyn/utils/text/normalization.py
@@ -1,7 +1,18 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 class BaseTextNormalizer(ABC):
     """Base class of text normalizer."""
 
-    pass
+    @abstractmethod
+    def __call__(self, text: str) -> str:
+        """Normalize text.
+
+        Args:
+            text (str): Text to be normalized.
+
+        Returns:
+            str: Normalized text.
+
+        """
+        pass
--- audyn/utils/text/tokenization.py
+++ audyn/utils/text/tokenization.py
@@ -1,7 +1,19 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from typing import List
 
 
 class BaseTextTokenizer(ABC):
     """Base class of text tokenizer."""
 
-    pass
+    @abstractmethod
+    def __call__(self, text: str) -> List[str]:
+        """Tokenize (normalized) text.
+
+        Args:
+            text (str): Text to be tokenized.
+
+        Returns:
+            list: Tokenized text.
+
+        """
+        pass
