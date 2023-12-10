bc145223a0c50e8e3ac83d5373aa181e4b4c4c18
tky823 <tky823.main+github@gmail.com>
2023-12-11 00:26:41 +0900

    [src] Add EnglishWordTokenizer

--- audyn/utils/text/tokenization.py
+++ audyn/utils/text/tokenization.py
@@ -1,12 +1,28 @@
+import re
 from abc import ABC, abstractmethod
 from typing import List
+
+_puctuation_split_re = re.compile(r"\b(?:\w+(?:'\w+)?)\b|[,.!?:;\(\)]\s*")
+_whitespace_split_re = re.compile(r"\s+")
 
 
 class BaseTextTokenizer(ABC):
     """Base class of text tokenizer."""
 
+    def __call__(self, *args, **kwargs) -> List[str]:
+        """Tokenize (normalized) text by calling ``self.tokenize`` method.
+
+        Args:
+            text (str): Text to be tokenized.
+
+        Returns:
+            list: Tokenized text.
+
+        """
+        return self.tokenize(*args, **kwargs)
+
     @abstractmethod
-    def __call__(self, text: str) -> List[str]:
+    def tokenize(self, text: str) -> List[str]:
         """Tokenize (normalized) text.
 
         Args:
@@ -17,3 +33,25 @@ class BaseTextTokenizer(ABC):
 
         """
         pass
+
+
+class EnglishWordTokenizer(BaseTextTokenizer):
+    """Tokenizer that split English sentence into words by white space."""
+
+    def tokenize(self, text: str) -> List[str]:
+        """Tokenize english sentence into words.
+
+        .. example::
+
+            >>> tokenizer = EnglishWordTokenizer()
+            >>> text = "It's the ghost!"
+            >>> tokenizer(text)
+            ["It's", 'the', 'ghost', '!']
+
+        """
+        tokens = []
+
+        for token in _whitespace_split_re.split(text):
+            tokens = tokens + _puctuation_split_re.findall(token)
+
+        return tokens
