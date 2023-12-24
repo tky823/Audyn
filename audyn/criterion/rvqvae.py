012e8c55d83d3461451506dbd23edecb0d6e60ee
tky823 <tky823.main+github@gmail.com>
2023-12-24 13:47:27 +0900

    [src] Add criterion for RVQVAE

---
+++ audyn/criterion/rvqvae.py
@@ -0,0 +1,62 @@
+import torch
+
+from .vqvae import CodebookLoss as VQVAECodebookLoss
+from .vqvae import CommitmentLoss as VQVAECommitmentLoss
+
+__all__ = ["CodebookLoss", "CommitmentLoss"]
+
+
+class CodebookLoss(VQVAECodebookLoss):
+    """Codebook loss to update embeddings in codebook."""
+
+    def __init__(self, layer_wise: bool = True, reduction: str = "mean") -> None:
+        super().__init__(reduction=reduction)
+
+        self.layer_wise = layer_wise
+
+    def forward(self, encoded: torch.Tensor, quantized: torch.Tensor) -> torch.Tensor:
+        """Forward pass of CodebookLoss.
+
+        Args:
+            encoded (torch.Tensor): Encoded feature. Any shape can be handled.
+                This tensor is atached in loss computation.
+            quantized (torch.Tensor): Quantized feature of same shape as encoded feature.
+
+        Returns:
+            torch.Tensor: Computed loss. The shape depends on ``reduction``.
+
+        """
+        if self.layer_wise:
+            quantized = quantized.sum(dim=1)
+
+        loss = super().forward(quantized, encoded)
+
+        return loss
+
+
+class CommitmentLoss(VQVAECommitmentLoss):
+    """Codebook loss to update encoded feature."""
+
+    def __init__(self, layer_wise: bool = True, reduction: str = "mean") -> None:
+        super().__init__(reduction=reduction)
+
+        self.layer_wise = layer_wise
+
+    def forward(self, encoded: torch.Tensor, quantized: torch.Tensor) -> torch.Tensor:
+        """Forward pass of CommitmentLoss.
+
+        Args:
+            encoded (torch.Tensor): Encoded feature. Any shape can be handled.
+            quantized (torch.Tensor): Quantized feature of same shape as encoded feature.
+                This tensor is atached in loss computation.
+
+        Returns:
+            torch.Tensor: Computed loss. The shape depends on ``reduction``.
+
+        """
+        if self.layer_wise:
+            quantized = quantized.sum(dim=1)
+
+        loss = super().forward(encoded, quantized)
+
+        return loss
