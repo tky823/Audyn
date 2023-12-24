ee9b2f416bb96f8eccf75df8bd0526e675e795a5
tky823 <tky823.main+github@gmail.com>
2023-12-24 11:32:37 +0900

    [tests] Fix assertion of outputs of VQVAE

--- tests/package/models/test_vqvae.py
+++ tests/package/models/test_vqvae.py
@@ -32,4 +32,4 @@ def test_vqvae():
     output_by_quantized = model.inference(quantized)
     output_by_indices = model.inference(indices)
 
-    assert output_by_indices.size() == output_by_quantized.size()
+    assert torch.allclose(output_by_indices, output_by_quantized)
