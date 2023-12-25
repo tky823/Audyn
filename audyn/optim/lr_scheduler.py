338f43e4107088e5e23b3c3515d490aa98b744e5
tky823 <tky823.main+github@gmail.com>
2023-12-25 20:57:42 +0900

    [src] Add MultiLRSchedulers

--- audyn/optim/lr_scheduler.py
+++ audyn/optim/lr_scheduler.py
@@ -1,10 +1,12 @@
+from typing import Any, Dict, List, Union
+
 from torch.optim import Optimizer
-from torch.optim.lr_scheduler import _LRScheduler
+from torch.optim.lr_scheduler import LRScheduler
 
 __all__ = ["TransformerLRScheduler", "GANLRScheduler"]
 
 
-class TransformerLRScheduler(_LRScheduler):
+class TransformerLRScheduler(LRScheduler):
     def __init__(
         self,
         optimizer: Optimizer,
@@ -28,8 +30,59 @@ class TransformerLRScheduler(_LRScheduler):
         return [lr for _ in self.optimizer.param_groups]
 
 
+class MultiLRSchedulers:
+    """Module to manage multiple learning rate schedulers."""
+
+    # TODO: improve design
+
+    def __init__(self, lr_schedulers: List[Union[Dict[str, Any], LRScheduler]]) -> None:
+        self.lr_schedulers = {}
+
+        for idx, lr_scheduler in enumerate(lr_schedulers):
+            if isinstance(lr_scheduler, LRScheduler):
+                k = str(idx)
+                v = lr_scheduler
+            elif isinstance(lr_scheduler, dict):
+                k = lr_scheduler["name"]
+                v = lr_scheduler["lr_scheduler"]
+            else:
+                raise ValueError(f"{type(lr_scheduler)} is not supported.")
+
+            if k in self.lr_schedulers.keys():
+                raise ValueError(f"Duplicate lr_scheduler name {k} is found.")
+
+            self.lr_schedulers[k] = v
+
+    def step(self, *args, **kwargs) -> None:
+        for lr_scheduler in self.lr_schedulers.values():
+            lr_scheduler: LRScheduler
+            lr_scheduler.step(*args, **kwargs)
+
+    def state_dict(self, *args, **kwargs) -> Dict[str, Dict[str, Any]]:
+        state_dict = {}
+
+        for name, lr_scheduler in self.lr_schedulers.items():
+            lr_scheduler: LRScheduler
+            state_dict[name] = lr_scheduler.state_dict(*args, **kwargs)
+
+        return state_dict
+
+    def load_state_dict(self, state_dict: Dict[str, Dict[str, Any]]) -> None:
+        r"""Loads the learning rate scheduler state.
+
+        Args:
+            state_dict (dict): Learning rate scheduler state. Should be an object returned
+                from a call to ``state_dict``.
+
+        """
+
+        for name, lr_scheduler in self.lr_schedulers.items():
+            lr_scheduler: LRScheduler
+            lr_scheduler.load_state_dict(state_dict[name])
+
+
 class GANLRScheduler:
-    def __init__(self, generator: _LRScheduler, discriminator: _LRScheduler) -> None:
+    def __init__(self, generator: LRScheduler, discriminator: LRScheduler) -> None:
         self.generator = generator
         self.discriminator = discriminator
 
