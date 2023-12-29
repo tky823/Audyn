7f5873274a2b0d785d64db08f1d57a5f084f7a87
tky823 <tky823.main+github@gmail.com>
2023-12-29 23:13:39 +0900

    [tests] Add more tests of MultiScaleSpectralLoss

--- tests/package/criterion/test_distance.py
+++ tests/package/criterion/test_distance.py
@@ -1,4 +1,6 @@
 import torch
+import torch.nn as nn
+import torchaudio.transforms as aT
 
 from audyn.criterion.distance import MultiScaleSpectralLoss
 
@@ -20,5 +22,24 @@ def test_multi_scale_spectral_loss() -> None:
     # 3D input
     input = torch.randn((batch_size, in_channels, length), dtype=torch.float)
     target = torch.randn((batch_size, in_channels, length), dtype=torch.float)
+
+    _ = criterion(input, target)
+
+    # specify transform
+    sample_rate = 8000
+    n_mels = 16
+
+    transform = []
+
+    for _n_fft in n_fft:
+        transform.append(
+            aT.MelSpectrogram(sample_rate, n_mels=n_mels, n_fft=_n_fft, hop_length=_n_fft // 4)
+        )
+
+    transform = nn.ModuleList(transform)
+    criterion = MultiScaleSpectralLoss(n_fft, transform=transform)
+
+    input = torch.randn((batch_size, length), dtype=torch.float)
+    target = torch.randn((batch_size, length), dtype=torch.float)
 
     _ = criterion(input, target)
