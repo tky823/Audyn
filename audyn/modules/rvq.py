347cb16eacee96f740d4c59dcd00acb7b94f721f
tky823 <tky823.main+github@gmail.com>
2023-12-30 14:11:46 +0900

    [src] Support k-means clustering initalization by VQ

--- audyn/modules/rvq.py
+++ audyn/modules/rvq.py
@@ -4,7 +4,7 @@ import torch
 import torch.distributed as dist
 import torch.nn as nn
 
-from ..functional.vector_quantization import quantize_residual_vector
+from ..functional.vector_quantization import quantize_residual_vector, quantize_vector
 
 __all__ = ["ResidualVectorQuantizer"]
 
@@ -17,6 +17,9 @@ class ResidualVectorQuantizer(nn.Module):
         embedding_dim (int): Number of embedding dimensions.
         num_stages (int): Number of residual steps.
         dropout (bool): Dropout of RVQ. Default: ``True``.
+        init_by_kmeans (int): Number of iterations in k-means clustering initialization.
+            If non-positive value is given, k-means clustering initialization is not used.
+        seed (int): Random seed for k-means clustering initialization.
 
     """
 
@@ -26,6 +29,8 @@ class ResidualVectorQuantizer(nn.Module):
         embedding_dim: int,
         num_stages: int,
         dropout: bool = True,
+        init_by_kmeans: int = 0,
+        seed: int = 0,
     ) -> None:
         super().__init__()
 
@@ -45,6 +50,18 @@ class ResidualVectorQuantizer(nn.Module):
 
         self.codebooks = nn.ModuleList(codebooks)
 
+        self.seed = seed
+        self.init_by_kmeans = init_by_kmeans
+
+        if self.init_by_kmeans > 0:
+            self.register_parameter(
+                "is_initialized", nn.Parameter(torch.tensor(False), requires_grad=False)
+            )
+        else:
+            self.register_parameter(
+                "is_initialized", nn.Parameter(torch.tensor(True), requires_grad=False)
+            )
+
     def forward(self, input: torch.Tensor) -> Tuple[torch.Tensor, torch.LongTensor]:
         """Forward pass of vector quantizer.
 
@@ -60,6 +77,13 @@ class ResidualVectorQuantizer(nn.Module):
                     To disable this feature, set ``dropout=False`` or call ``.eval()``.
 
         """
+        self.is_initialized: torch.Tensor
+        is_initialized: bool = self.is_initialized.item()
+
+        if not is_initialized:
+            self._initialize_parameters(input)
+            self.is_initialized.fill_(True)
+
         if self.dropout and self.training:
             num_stages = torch.randint(0, len(self.codebooks), ()) + 1
 
@@ -88,3 +112,46 @@ class ResidualVectorQuantizer(nn.Module):
         output, indices = quantize_residual_vector(input, weight)
 
         return output, indices
+
+    @torch.no_grad()
+    def _initialize_parameters(self, encoded: torch.Tensor) -> None:
+        # TODO: support DDP
+        self.is_initialized: torch.Tensor
+        is_initialized: bool = self.is_initialized.item()
+
+        assert self.init_by_kmeans > 0 and not is_initialized
+
+        g = torch.Generator(device=encoded.device)
+        g.manual_seed(self.seed)
+
+        batch_size, embedding_dim, *_ = encoded.size()
+        encoded = encoded.view(batch_size, embedding_dim, -1)
+        encoded = encoded.permute(0, 2, 1).contiguous()
+        encoded = encoded.view(-1, embedding_dim)
+        residual = encoded
+        reconstructed = 0
+
+        for codebook in self.codebooks:
+            # select ``codebook_size`` embeddings from encoded features
+            codebook: nn.Embedding
+            codebook_size = codebook.weight.size(0)
+            indices = torch.randperm(residual.size(0), generator=g, dtype=torch.long)
+            indices = indices[:codebook_size]
+            centroids = residual[indices]
+
+            for _ in range(self.init_by_kmeans):
+                norm = torch.sum(centroids**2, dim=-1)
+                dot = torch.matmul(residual, centroids.transpose(1, 0))
+                distance = norm - 2 * dot
+                indices = torch.argmin(distance, dim=-1)
+                eye = torch.eye(codebook_size, device=residual.device, dtype=residual.dtype)
+                assignments = eye[indices]
+                assignments = assignments.permute(1, 0)
+                num_assignments = assignments.sum(dim=-1, keepdim=True)
+                centroids = torch.matmul(assignments, residual) / num_assignments
+
+            codebook.weight.data.copy_(centroids)
+
+            output, _ = quantize_vector(residual, codebook.weight)
+            reconstructed = reconstructed + output
+            residual = encoded - reconstructed
--- audyn/modules/vq.py
+++ audyn/modules/vq.py
@@ -15,16 +15,37 @@ class VectorQuantizer(nn.Module):
     Args:
         codebook_size (int): Size of codebook.
         embedding_dim (int): Number of embedding dimensions.
+        init_by_kmeans (int): Number of iterations in k-means clustering initialization.
+            If non-positive value is given, k-means clustering initialization is not used.
+        seed (int): Random seed for k-means clustering initialization.
 
     """
 
-    def __init__(self, codebook_size: int, embedding_dim: int) -> None:
+    def __init__(
+        self,
+        codebook_size: int,
+        embedding_dim: int,
+        init_by_kmeans: int = 0,
+        seed: int = 0,
+    ) -> None:
         super().__init__()
 
         self.codebook = nn.Embedding(
             num_embeddings=codebook_size,
             embedding_dim=embedding_dim,
         )
+
+        self.seed = seed
+        self.init_by_kmeans = init_by_kmeans
+
+        if self.init_by_kmeans > 0:
+            self.register_parameter(
+                "is_initialized", nn.Parameter(torch.tensor(False), requires_grad=False)
+            )
+        else:
+            self.register_parameter(
+                "is_initialized", nn.Parameter(torch.tensor(True), requires_grad=False)
+            )
 
     def forward(self, input: torch.Tensor) -> Tuple[torch.Tensor, torch.LongTensor]:
         """Forward pass of vector quantizer.
@@ -39,6 +60,48 @@ class VectorQuantizer(nn.Module):
                 - torch.LongTensor: Indices of indices in codebook of shape (batch_size, *).
 
         """
+        self.is_initialized: torch.Tensor
+        is_initialized: bool = self.is_initialized.item()
+
+        if not is_initialized:
+            self._initialize_parameters(input)
+            self.is_initialized.fill_(True)
+
         output, indices = quantize_vector(input, self.codebook.weight)
 
         return output, indices
+
+    @torch.no_grad()
+    def _initialize_parameters(self, encoded: torch.Tensor) -> None:
+        # TODO: support DDP
+        self.is_initialized: torch.Tensor
+        is_initialized: bool = self.is_initialized.item()
+
+        assert self.init_by_kmeans > 0 and not is_initialized
+
+        g = torch.Generator(device=encoded.device)
+        g.manual_seed(self.seed)
+
+        batch_size, embedding_dim, *_ = encoded.size()
+        encoded = encoded.view(batch_size, embedding_dim, -1)
+        encoded = encoded.permute(0, 2, 1).contiguous()
+        encoded = encoded.view(-1, embedding_dim)
+
+        # select ``codebook_size`` embeddings from encoded features
+        codebook_size = self.codebook.weight.size(0)
+        indices = torch.randperm(encoded.size(0), generator=g, dtype=torch.long)
+        indices = indices[:codebook_size]
+        centroids = encoded[indices]
+
+        for _ in range(self.init_by_kmeans):
+            norm = torch.sum(centroids**2, dim=-1)
+            dot = torch.matmul(encoded, centroids.transpose(1, 0))
+            distance = norm - 2 * dot
+            indices = torch.argmin(distance, dim=-1)
+            eye = torch.eye(codebook_size, device=encoded.device, dtype=encoded.dtype)
+            assignments = eye[indices]
+            assignments = assignments.permute(1, 0)
+            num_assignments = assignments.sum(dim=-1, keepdim=True)
+            centroids = torch.matmul(assignments, encoded) / num_assignments
+
+        self.codebook.weight.data.copy_(centroids)
