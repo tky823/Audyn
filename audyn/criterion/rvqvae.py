462463105454b53811f3d4c665a8ee50f3389459
tky823 <tky823.main+github@gmail.com>
2023-12-31 01:35:57 +0900

    [src] Add CodebookEntropyLoss for VQVAE

--- audyn/criterion/rvqvae.py
+++ audyn/criterion/rvqvae.py
@@ -1,5 +1,6 @@
 import torch
 
+from .vqvae import CodebookEntropyLoss as VQVAECodebookEntropyLoss
 from .vqvae import CodebookLoss as VQVAECodebookLoss
 from .vqvae import CommitmentLoss as VQVAECommitmentLoss
 
@@ -58,5 +59,33 @@ class CommitmentLoss(VQVAECommitmentLoss):
             quantized = quantized.sum(dim=1)
 
         loss = super().forward(encoded, quantized)
+
+        return loss
+
+
+class CodebookEntropyLoss(VQVAECodebookEntropyLoss):
+    def forward(self, input: torch.LongTensor) -> torch.Tensor:
+        """Forward pass of CodebookEntropyLoss.
+
+        .. note::
+
+            This loss is not differentiable, so use this for monitoring.
+
+        Args:
+            input (torch.Tensor): Selected codebook indices of shape (batch_size, num_stages, *).
+
+        Returns:
+            torch.Tensor: Entropy of shape ().
+
+        """
+        input = input.transpose(1, 0).contiguous()
+        num_stages = input.size(0)
+
+        loss = 0
+
+        for _input in input:
+            loss = loss + super().forward(_input)
+
+        loss = loss / num_stages
 
         return loss
