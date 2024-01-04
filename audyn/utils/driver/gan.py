2e40500fdbdc5c3364ccc4ecb3d0c5be63a0023b
tky823 <tky823.main+github@gmail.com>
2024-01-05 00:42:11 +0900

    [src] Create driver for inference of generator in GAN

--- audyn/utils/driver/gan.py
+++ audyn/utils/driver/gan.py
@@ -18,7 +18,7 @@ from ..clip_grad import GANGradClipper
 from ..data import BaseDataLoaders
 from ..hydra.utils import instantiate_grad_clipper
 from ..model import unwrap
-from .base import BaseTrainer, _is_audyn_clip_gradient, _is_torch_clip_gradient
+from .base import BaseGenerator, BaseTrainer, _is_audyn_clip_gradient, _is_torch_clip_gradient
 
 
 class GANTrainer(BaseTrainer):
@@ -983,3 +983,35 @@ class GANTrainer(BaseTrainer):
 
         s = f"Save model: {save_path}."
         self.logger.info(s)
+
+
+class GANGenerator(BaseGenerator):
+    """Waveform synthesis using generator of GAN."""
+
+    @torch.no_grad()
+    def run(self) -> None:
+        self.model.eval()
+
+        unwrapped_generator = unwrap(self.unwrapped_model.generator)
+
+        for named_data in self.loader:
+            named_data = self.move_data_to_device(named_data, self.device)
+            named_input = self.map_to_named_input(
+                named_data, key_mapping=self.config.test.key_mapping.inference
+            )
+            named_identifier = self.map_to_named_identifier(
+                named_data, key_mapping=self.config.test.key_mapping.inference
+            )
+
+            if hasattr(unwrapped_generator, "inference"):
+                output = unwrapped_generator.inference(**named_input)
+            else:
+                output = unwrapped_generator(**named_input)
+
+            named_output = self.map_to_named_output(
+                output, key_mapping=self.config.test.key_mapping.inference
+            )
+
+            self.save_inference_audio_if_necessary(
+                named_output, named_data, named_identifier, config=self.config.test.output
+            )
