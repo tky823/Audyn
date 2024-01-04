ffdf4002ee41012578f2fde913ee7987b96961d6
tky823 <tky823.main+github@gmail.com>
2024-01-04 19:31:41 +0900

    [tests] Ensure output of GlowTTS

--- tests/package/models/test_glowtts.py
+++ tests/package/models/test_glowtts.py
@@ -5,6 +5,7 @@ from typing import List, Optional
 import pytest
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from dummy import allclose
 
 from audyn.models.fastspeech import LengthRegulator
@@ -105,13 +106,13 @@ def test_glowtts(scaling: bool, channel_dependent_scaling: bool) -> None:
         tgt_length=tgt_length,
     )
     src_latent, tgt_latent = latent
-    log_est_duration, log_ml_duration = log_duration
+    log_est_duration, ml_duration = log_duration
     src_padding_mask, tgt_padding_mask = padding_mask
 
     assert src_latent.size() == (batch_size, max_src_length, embedding_dim)
     assert tgt_latent.size() == (batch_size, max_tgt_length, latent_channels)
     assert log_est_duration.size() == (batch_size, max_src_length)
-    assert log_ml_duration.size() == (batch_size, max_src_length)
+    assert ml_duration.size() == (batch_size, max_src_length)
     assert src_padding_mask.size() == (batch_size, max_src_length)
     assert tgt_padding_mask.size() == (batch_size, max_tgt_length)
     assert logdet.size() == (batch_size,)
@@ -132,6 +133,124 @@ def test_glowtts(scaling: bool, channel_dependent_scaling: bool) -> None:
 
         state_dict = torch.load(path, map_location="cpu")
         model.load_state_dict(state_dict)
+
+
+def test_glowtts_unbatched() -> None:
+    """Ensure output of GlowTTS does not depend on padding length."""
+    torch.manual_seed(0)
+
+    batch_first = True
+    batch_size = 4
+    num_embeddings = 5
+    padding_idx = 0
+    embedding_dim, n_mels = 2, 6
+    latent_channels = n_mels
+
+    # Encoder
+    num_enc_layers = 6
+
+    # Decoder
+    hidden_channels = 3
+    num_flows, num_dec_layers, num_splits = 3, 2, 2
+    down_scale = 2
+    scaling_channels = None
+
+    # Duration Predictor
+    kernel_size = 3
+
+    max_src_length = 20
+    max_tgt_length = 2 * max_src_length
+
+    src_length = torch.randint(
+        max_src_length // 2, max_src_length + 1, (batch_size,), dtype=torch.long
+    )
+    tgt_length = torch.randint(
+        max_tgt_length // 2, max_tgt_length + 1, (batch_size,), dtype=torch.long
+    )
+    max_src_length = torch.max(src_length).item()
+    max_tgt_length = torch.max(tgt_length).item()
+    src = torch.randint(1, num_embeddings, (batch_size, max_src_length))
+    tgt = torch.randn((batch_size, n_mels, max_tgt_length))
+
+    encoder = build_encoder(
+        num_embeddings,
+        embedding_dim,
+        latent_channels,
+        padding_idx=padding_idx,
+        num_layers=num_enc_layers,
+    )
+    decoder = build_decoder(
+        n_mels,
+        hidden_channels,
+        num_flows=num_flows,
+        num_layers=num_dec_layers,
+        num_splits=num_splits,
+        down_scale=down_scale,
+        scaling=False,
+        scaling_channels=scaling_channels,
+    )
+    duration_predictor = FastSpeechDurationPredictor(
+        [embedding_dim, 2],
+        kernel_size=kernel_size,
+        batch_first=batch_first,
+    )
+    length_regulator = LengthRegulator(batch_first=batch_first)
+    transform_middle = FastSpeechWaveNetBridge(take_exp=False)
+
+    model = GlowTTS(
+        encoder,
+        decoder,
+        duration_predictor,
+        length_regulator,
+        transform_middle=transform_middle,
+    )
+    model.eval()
+
+    latent, log_duration, padding_mask, logdet = model(
+        src,
+        tgt,
+        src_length=src_length,
+        tgt_length=tgt_length,
+    )
+    src_latent, tgt_latent = latent
+    log_est_duration, ml_duration = log_duration
+    src_padding_mask, tgt_padding_mask = padding_mask
+
+    unbatched_src, _ = torch.split(src, [1, batch_size - 1], dim=0)
+    unbatched_tgt, _ = torch.split(tgt, [1, batch_size - 1], dim=0)
+    unbatched_src_length, _ = torch.split(src_length, [1, batch_size - 1], dim=0)
+    unbatched_tgt_length, _ = torch.split(tgt_length, [1, batch_size - 1], dim=0)
+
+    # pad value for test
+    unbatched_src = F.pad(unbatched_src, (0, 1))
+    unbatched_tgt = F.pad(unbatched_tgt, (0, 1))
+
+    unbatched_latent, unbatched_log_duration, unbatched_padding_mask, unbatched_logdet = model(
+        unbatched_src,
+        unbatched_tgt,
+        src_length=unbatched_src_length,
+        tgt_length=unbatched_tgt_length,
+    )
+
+    unbatched_src_latent, unbatched_tgt_latent = unbatched_latent
+    unbatched_log_est_duration, unbatched_ml_duration = unbatched_log_duration
+    unbatched_src_padding_mask, unbatched_tgt_padding_mask = unbatched_padding_mask
+
+    unbatched_src_latent = unbatched_src_latent.squeeze(dim=0)
+    unbatched_tgt_latent = unbatched_tgt_latent.squeeze(dim=0)
+    unbatched_log_est_duration = unbatched_log_est_duration.squeeze(dim=0)
+    unbatched_ml_duration = unbatched_ml_duration.squeeze(dim=0)
+    unbatched_src_padding_mask = unbatched_src_padding_mask.squeeze(dim=0)
+    unbatched_tgt_padding_mask = unbatched_tgt_padding_mask.squeeze(dim=0)
+    unbatched_logdet = unbatched_logdet.squeeze(dim=0)
+
+    assert torch.allclose(unbatched_src_latent[:-1], src_latent[0])
+    assert torch.allclose(unbatched_tgt_latent[:-1], tgt_latent[0])
+    assert torch.allclose(unbatched_log_est_duration[:-1], log_est_duration[0])
+    assert torch.allclose(unbatched_ml_duration[:-1], ml_duration[0])
+    assert torch.allclose(unbatched_src_padding_mask[:-1], src_padding_mask[0])
+    assert torch.allclose(unbatched_tgt_padding_mask[:-1], tgt_padding_mask[0])
+    assert torch.allclose(unbatched_logdet, logdet[0])
 
 
 @pytest.mark.parametrize("use_relative_position", [True, False])
