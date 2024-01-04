6ab1c73931004b8442ac35c7264dde3a00f06583
tky823 <tky823.main+github@gmail.com>
2024-01-04 16:30:33 +0900

    [src] Pad duration for exp(-inf)

--- audyn/utils/duration/__init__.py
+++ audyn/utils/duration/__init__.py
@@ -22,10 +22,16 @@ def transform_log_duration(
     Returns:
         torch.Tensor: Duration in linear-domain.
 
+    .. note::
+
+        ``exp(-inf)`` is treated as ``0`` even if ``min_duration`` is specified.
+
     """
+    padding_mask = torch.isneginf(log_duration)
     linear_duration = torch.exp(log_duration)
     linear_duration = torch.round(linear_duration.detach())
     linear_duration = torch.clip(linear_duration, min=min_duration, max=max_duration)
+    linear_duration = linear_duration.masked_fill(padding_mask, 0)
 
     if dtype is not None:
         linear_duration = linear_duration.to(dtype)
