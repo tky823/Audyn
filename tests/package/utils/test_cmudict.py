2126b0a64fb2a5ae0374f189277fe4bc63391d17
tky823 <tky823.main+github@gmail.com>
2024-01-08 15:15:20 +0900

    [tests] Ensure order of CMUDictIndexer in test

--- tests/package/utils/test_cmudict.py
+++ tests/package/utils/test_cmudict.py
@@ -1,4 +1,4 @@
-from audyn.utils.data.cmudict import CMUDict
+from audyn.utils.data.cmudict import BOS_IDX, BOS_SYMBOL, EOS_IDX, EOS_SYMBOL, CMUDict
 from audyn.utils.data.cmudict.indexing import CMUDictIndexer
 
 
@@ -9,9 +9,14 @@ def test_cmudict() -> None:
 
 
 def test_cmudict_indexer() -> None:
+    """Ensure order of full_symbols and CMUDictIndexer."""
     pron_dict = CMUDict()
     phonemes = pron_dict["Hello"]
+    phonemes.insert(0, BOS_SYMBOL)
+    phonemes.append(EOS_SYMBOL)
     indexer = CMUDictIndexer()
     indices = indexer(phonemes)
 
-    assert indices == [43, 10, 54, 60]
+    assert indices[0] == BOS_IDX
+    assert indices[-1] == EOS_IDX
+    assert indices[1:-1] == [42, 9, 53, 59]
