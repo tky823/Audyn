895a564de0f5bd56dde48c05143227815620eaf1
tky823 <tky823.main+github@gmail.com>
2024-01-08 14:52:14 +0900

    [tests] Add test of CMUDictIndexer

--- tests/package/utils/test_cmudict.py
+++ tests/package/utils/test_cmudict.py
@@ -1,7 +1,17 @@
 from audyn.utils.data.cmudict import CMUDict
+from audyn.utils.data.cmudict.indexing import CMUDictIndexer
 
 
 def test_cmudict() -> None:
     pron_dict = CMUDict()
 
     assert pron_dict["Hello"] == ["HH", "AH0", "L", "OW1"]
+
+
+def test_cmudict_indexer() -> None:
+    pron_dict = CMUDict()
+    phonemes = pron_dict["Hello"]
+    indexer = CMUDictIndexer()
+    indices = indexer(phonemes)
+
+    assert indices == [43, 10, 54, 60]
