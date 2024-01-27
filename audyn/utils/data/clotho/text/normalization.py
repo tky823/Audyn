3429ad4e4b599d2b6cca119853b16f81b1f369a5
tky823 <tky823.main+github@gmail.com>
2024-01-28 02:37:35 +0900

    [src] Trim punctuations in ClothoTextNormalizer

--- audyn/utils/data/clotho/text/normalization.py
+++ audyn/utils/data/clotho/text/normalization.py
@@ -4,6 +4,10 @@ from ....text.normalization import BaseTextNormalizer
 
 __all__ = ["ClothoTextNormalizer"]
 
+_normalize_re = re.compile(r'\s([,.!?;:"](?:\s|$))')
+_whitespace_re = re.compile(r"\s+")
+_punctuation_re = re.compile('[,.!?;:"]')
+
 
 class ClothoTextNormalizer(BaseTextNormalizer):
     """Text normalizer for Clotho dataset.
@@ -12,15 +16,10 @@ class ClothoTextNormalizer(BaseTextNormalizer):
     https://github.com/audio-captioning/dcase-2020-baseline/blob/654e808924f7c0d4e6e3d90cdcdaf28c9165a0ce/tools/dataset_creation.py. # noqa: E501
     """
 
-    def __init__(self) -> None:
-        super().__init__()
-
-        self.sub_re = re.compile(r'\s([,.!?;:"](?:\s|$))')
-        self.continuous_space_re = re.compile(r"\s{2,}")
-
     def normalize(self, text: str) -> str:
-        text = self.sub_re.sub(r"\1", text)
-        text = self.continuous_space_re.sub(" ", text)
         text = text.lower()
+        text = _normalize_re.sub(r"\1", text)
+        text = _whitespace_re.sub(" ", text)
+        text = _punctuation_re.sub("", text)
 
         return text
