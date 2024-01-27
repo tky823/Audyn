ad68997f7f6e5c9b6decdd7a187eb6f67752629c
tky823 <tky823.main+github@gmail.com>
2024-01-28 04:14:49 +0900

    [recipes] Add stage 0 to baseline of ClothoV2

--- recipes/ClothoV2/Baseline/run.sh
+++ recipes/ClothoV2/Baseline/run.sh
@@ -17,6 +17,10 @@ dump_root="dump"
 
 dump_format="torch"
 
+system="defaults"
+preprocess="clotho-v2"
+data="clotho-v2"
+
 . ../../_common/parse_options.sh || exit 1;
 
 if [ ${stage} -le -1 ] && [ ${stop_stage} -ge -1 ]; then
@@ -26,5 +30,21 @@ if [ ${stage} -le -1 ] && [ ${stop_stage} -ge -1 ]; then
         . ../_common/download.sh \
         --clotho-url "${clotho_url}" \
         --data-root "${data_root}"
+    )
+fi
+
+if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
+    echo "Stage 0: Preprocessing"
+
+    (
+        . ./preprocess.sh \
+        --stage 1 \
+        --stop-stage 3 \
+        --data-root "${data_root}" \
+        --dump-root "${dump_root}" \
+        --dump-format "${dump_format}" \
+        --system "${system}" \
+        --preprocess "${preprocess}" \
+        --data "${data}"
     )
 fi
