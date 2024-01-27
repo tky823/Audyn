c4a42350912b9def0f357eedf0ef39603f8348b1
tky823 <tky823.main+github@gmail.com>
2024-01-28 03:53:11 +0900

    [src] Fix args in ClothoTextPreprocessor

--- audyn/utils/data/clotho/__init__.py
+++ audyn/utils/data/clotho/__init__.py
@@ -1,6 +1,4 @@
-from typing import Union
-
-import torchtext
+from typing import Optional
 
 from ...text import TextPreprocessor
 from .text.indexing import ClothoTextIndexer
@@ -11,10 +9,10 @@ __all__ = ["ClothoTextNormalizer", "ClothoTextTokenizer", "ClothoTextIndexer"]
 
 
 class ClothoTextPreprocessor(TextPreprocessor):
-    def __init__(self, vocab: Union[str, torchtext.vocab.Vocab]) -> None:
+    def __init__(self, root: Optional[str] = None) -> None:
         normalizer = ClothoTextNormalizer()
         tokenizer = ClothoTextTokenizer()
-        indexer = ClothoTextIndexer(vocab)
+        indexer = ClothoTextIndexer(root)
 
         super().__init__(
             normalizer=normalizer,
