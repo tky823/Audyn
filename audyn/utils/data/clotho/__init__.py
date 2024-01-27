4517234b857227c8ed3d264810a7f80a20a69de9
tky823 <tky823.main+github@gmail.com>
2024-01-28 01:19:12 +0900

    [src] Define ClothoTextPreprocessor

--- audyn/utils/data/clotho/__init__.py
+++ audyn/utils/data/clotho/__init__.py
@@ -1,4 +1,24 @@
+from typing import Union
+
+import torchtext
+
+from ...text import TextPreprocessor
+from .text.indexing import ClothoTextIndexer
 from .text.normalization import ClothoTextNormalizer
 from .text.tokenization import ClothoTextTokenizer
 
-__all__ = ["ClothoTextNormalizer", "ClothoTextTokenizer"]
+__all__ = ["ClothoTextNormalizer", "ClothoTextTokenizer", "ClothoTextIndexer"]
+
+
+class ClothoTextPreprocessor(TextPreprocessor):
+    def __init__(self, vocab: Union[str, torchtext.vocab.Vocab]) -> None:
+        normalizer = ClothoTextNormalizer()
+        tokenizer = ClothoTextTokenizer()
+        indexer = ClothoTextIndexer(vocab)
+
+        super().__init__(
+            normalizer=normalizer,
+            tokenizer=tokenizer,
+            phonemizer=None,
+            indexer=indexer,
+        )
