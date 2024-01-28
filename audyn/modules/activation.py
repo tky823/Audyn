d1bfdcc8e5e265a0f70097274ea1bcabc9b9cd87
tky823 <tky823.main+github@gmail.com>
2024-01-28 23:28:16 +0900

    [src] Add RotaryPositionalMultiheadSelfAttention

--- audyn/modules/activation.py
+++ audyn/modules/activation.py
@@ -6,10 +6,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 from packaging import version
 
+from .positional_encoding import RotaryPositionalEmbedding
+
 __all__ = [
     "MultiheadSelfAttention",
     "AbsolutePositionalMultiheadSelfAttention",
     "RelativePositionalMultiheadSelfAttention",
+    "RotaryPositionalMultiheadSelfAttention",
 ]
 
 
@@ -595,3 +598,191 @@ class RelativePositionalMultiheadSelfAttention(MultiheadSelfAttention):
         pos_embedding = torch.flip(pos_embedding, dims=(-2,))
 
         return pos_embedding
+
+
+class RotaryPositionalMultiheadSelfAttention(MultiheadSelfAttention):
+    """Multihead self-attention using rotary positional representation."""
+
+    def __init__(
+        self,
+        embed_dim: int,
+        num_heads: int,
+        dropout: float = 0,
+        bias: bool = True,
+        add_bias_kv: bool = False,
+        add_zero_attn: bool = False,
+        base: int = 10000,
+        share_heads: bool = False,
+        batch_first: bool = False,
+        device: Optional[torch.device] = None,
+        dtype: Optional[torch.dtype] = None,
+    ) -> None:
+        factory_kwargs = {
+            "device": device,
+            "dtype": dtype,
+        }
+        super().__init__(
+            embed_dim,
+            num_heads,
+            dropout=dropout,
+            bias=bias,
+            add_bias_kv=add_bias_kv,
+            add_zero_attn=add_zero_attn,
+            batch_first=batch_first,
+            **factory_kwargs,
+        )
+
+        if add_bias_kv:
+            raise NotImplementedError("add_bias_kv is not supported.")
+
+        if add_zero_attn:
+            raise NotImplementedError("add_zero_attn is not supported.")
+
+        self.rope = RotaryPositionalEmbedding(base=base, batch_first=batch_first)
+
+        self.share_heads = share_heads
+
+    def forward(
+        self,
+        input: torch.Tensor,
+        padding_mask: Optional[torch.Tensor] = None,
+        need_weights: bool = True,
+        attn_mask: Optional[torch.Tensor] = None,
+        average_attn_weights: bool = True,
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor]]:
+        """Forward pass of RotaryPositionalMultiheadSelfAttention.
+
+        Args:
+            input (torch.Tensor): Sequence of shape (batch_size, length, embed_dim)
+                if ``batch_first=True``, otherwise (length, batch_size, embed_dim).
+            padding_mask (torch.BoolTensor, optional): Padding mask of shape (batch_size, length).
+            attn_mask (torch.BoolTensor, optional): Attention padding mask of
+                shape (length, length) or (batch_size * num_heads, length, length).
+
+        Returns:
+            tuple: Tuple of tensors containing
+
+                - torch.Tensor: Sequence of same shape as input.
+                - torch.Tensor: Attention weights of shape (batch_size, num_heads, length, length)
+                    if ``average_attn_weights=True``, otherwise (batch_size, length, length).
+
+        """
+        embed_dim = self.embed_dim
+        dropout = self.dropout
+        batch_first = self.batch_first
+        num_heads = self.num_heads
+        in_proj_weight = self.in_proj_weight
+        in_proj_bias = self.in_proj_bias
+
+        head_dim = embed_dim // num_heads
+
+        if batch_first:
+            x = input.transpose(1, 0)
+        else:
+            x = input
+
+        length, batch_size, _ = x.size()
+
+        padding_mask = F._canonical_mask(
+            mask=padding_mask,
+            mask_name="padding_mask",
+            other_type=F._none_or_dtype(attn_mask),
+            other_name="attn_mask",
+            target_type=x.dtype,
+        )
+
+        attn_mask = F._canonical_mask(
+            mask=attn_mask,
+            mask_name="attn_mask",
+            other_type=None,
+            other_name="",
+            target_type=x.dtype,
+            check_other=False,
+        )
+
+        x = F.linear(x, in_proj_weight, bias=in_proj_bias)
+
+        q, k, v = torch.chunk(x, chunks=3, dim=-1)
+
+        q = self._apply_pos_emb(q.contiguous())
+        k = self._apply_pos_emb(k.contiguous())
+
+        q = q.view(length, batch_size, num_heads, head_dim)
+        k = k.view(length, batch_size, num_heads, head_dim)
+        v = v.view(length, batch_size, num_heads, head_dim)
+        q = q.permute(1, 2, 0, 3)
+        k = k.permute(1, 2, 3, 0)
+        v = v.permute(1, 2, 0, 3)
+        qk = torch.matmul(q, k) / math.sqrt(head_dim)
+
+        if padding_mask is not None:
+            padding_mask = padding_mask.view(batch_size, 1, 1, length)
+
+            if attn_mask is None:
+                attn_mask = padding_mask
+            else:
+                if attn_mask.dim() == 3:
+                    attn_mask.view(batch_size, num_heads, length, length)
+                else:
+                    assert attn_mask.dim() == 2
+
+                attn_mask = attn_mask + padding_mask
+
+        if attn_mask is not None:
+            qk = qk + attn_mask
+
+        attn_weights = F.softmax(qk, dim=-1)
+
+        if dropout > 0:
+            attn_weights = F.dropout(attn_weights, p=dropout, training=self.training)
+
+        qkv = torch.matmul(attn_weights, v)
+
+        if batch_first:
+            qkv = qkv.permute(0, 2, 1, 3).contiguous()
+            qkv = qkv.view(batch_size, length, embed_dim)
+        else:
+            qkv = qkv.permute(2, 0, 1, 3).contiguous()
+            qkv = qkv.view(length, batch_size, embed_dim)
+
+        output = self.out_proj(qkv)
+
+        if average_attn_weights:
+            attn_weights = attn_weights.mean(dim=1)
+
+        if not need_weights:
+            attn_weights = None
+
+        return output, attn_weights
+
+    def _apply_pos_emb(self, input: torch.Tensor) -> torch.Tensor:
+        """Apply positional embedding to input.
+
+        Args:
+            input (torch.Tensor): Sequence of shape (length, batch_size, num_heads, head_dim).
+
+        Returns:
+            torch.Tensor: Output sequence same shape as input.
+
+        """
+        share_heads = self.share_heads
+        num_heads = self.num_heads
+        input_length, batch_size, embed_dim = input.size()
+        head_dim = embed_dim // num_heads
+
+        if share_heads:
+            x = input.view(input_length, batch_size * num_heads, head_dim)
+        else:
+            x = input.view(input_length, batch_size, num_heads * head_dim)
+
+        if self.batch_first:
+            x = x.transpose(1, 0)
+
+        x = self.rope(x)
+
+        if self.batch_first:
+            x = x.transpose(1, 0).contiguous()
+
+        output = x.view(input_length, batch_size, embed_dim)
+
+        return output
