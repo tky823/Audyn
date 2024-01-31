342a5aa9f13d971fe7bf138f9e5ff57eb8f80797
tky823 <tky823.main+github@gmail.com>
2024-02-01 01:46:20 +0900

    [recipes] Add scripts of CLAP

---
+++ recipes/ClothoV2/CLAP/run.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+stage=-1
+stop_stage=-1
+
+tag=""
+continue_from=""
+
+exp_dir="./exp"
+
+clotho_url="https://zenodo.org/records/4783391/files"
+data_root="../data"
+dump_root="dump"
+
+dump_format="torch"
+
+system="defaults"
+preprocess="clotho-v2"
+data="clotho-v2"
+
+. ../../_common/parse_options.sh || exit 1;
+
+set +u
+
+# path to local scripts
+export PYTHONPATH="./:${PYTHONPATH}"
+
+set -u
+
+if [ ${stage} -le -1 ] && [ ${stop_stage} -ge -1 ]; then
+    echo "Stage -1"
+
+    (
+        . ../_common/download.sh \
+        --clotho-url "${clotho_url}" \
+        --data-root "${data_root}"
+    )
+fi
+
+if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
+    echo "Stage 0: Preprocessing"
+
+    (
+        . ./preprocess.sh \
+        --stage 1 \
+        --stop-stage 3 \
+        --data-root "${data_root}" \
+        --dump-root "${dump_root}" \
+        --dump-format "${dump_format}" \
+        --system "${system}" \
+        --preprocess "${preprocess}" \
+        --data "${data}"
+    )
+fi
