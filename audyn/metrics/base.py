69a6c3514327d08fc57397bd7d03d6148a050234
tky823 <tky823.main+github@gmail.com>
2024-02-03 11:57:34 +0900

    [src] Add device parameter to StatefulMetric

--- audyn/metrics/base.py
+++ audyn/metrics/base.py
@@ -1,10 +1,17 @@
 from abc import ABC, abstractmethod
-from typing import Any
+from typing import Any, Optional
+
+import torch
 
 __all__ = ["StatefulMetric"]
 
 
 class StatefulMetric(ABC):
+    def __init__(self, device: Optional[torch.device] = None) -> None:
+        super().__init__()
+
+        self.device = device
+
     @abstractmethod
     def update(self, *args, **kwargs) -> Any:
         """Update state of metric."""
@@ -13,3 +20,7 @@ class StatefulMetric(ABC):
     @abstractmethod
     def reset(self) -> None:
         """Reset states."""
+
+    def to(self, device: torch.device) -> "StatefulMetric":
+        """Change device parameter."""
+        self.device = device
