a69a467edea2014cc5a766a0fa45837cd4021e60
tky823 <tky823.main+github@gmail.com>
2024-02-04 02:14:16 +0900

    [src] Add BaseMetricWrapper as wrapper class of StatefulMetric

--- audyn/metrics/base.py
+++ audyn/metrics/base.py
@@ -1,9 +1,9 @@
 from abc import ABC, abstractmethod
-from typing import Any, Optional
+from typing import Any, Dict, Optional
 
 import torch
 
-__all__ = ["StatefulMetric"]
+__all__ = ["StatefulMetric", "BaseMetricWrapper"]
 
 
 class StatefulMetric(ABC):
@@ -15,12 +15,46 @@ class StatefulMetric(ABC):
     @abstractmethod
     def update(self, *args, **kwargs) -> Any:
         """Update state of metric."""
-        pass
 
     @abstractmethod
     def reset(self) -> None:
         """Reset states."""
 
+    @abstractmethod
+    def compute(self) -> Any:
+        """Compute metric by current states."""
+
     def to(self, device: torch.device) -> "StatefulMetric":
         """Change device parameter."""
         self.device = device
+
+
+class BaseMetricWrapper(StatefulMetric):
+    """Wrapper class to handle multiple metrics by MultiMetrics."""
+
+    def __init__(
+        self,
+        metric: StatefulMetric,
+        key_mapping: Dict[str, Any],
+        weight: float = 1,
+    ) -> None:
+        super().__init__()
+
+        self.metric = metric
+        self.key_mapping = key_mapping
+        self.weight = weight
+
+    def reset(self) -> None:
+        """Reset states."""
+
+    def update(self, *args, **kwargs) -> Any:
+        """Update state of metric."""
+        self.metric.update(*args, **kwargs)
+
+    def compute(self, *args, **kwargs) -> Any:
+        """Compute metric by current states."""
+        self.metric.compute(*args, **kwargs)
+
+    def to(self, device: torch.device) -> StatefulMetric:
+        """Change device parameter."""
+        self.metric.to(device=device)
