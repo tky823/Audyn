f93eb5e9939aee6515784fdd9def56655aae9da5
tky823 <tky823.main+github@gmail.com>
2024-02-06 19:47:46 +0900

    [tests] Add test for mask token in ClothoTextPreprocessor

--- tests/package/utils/test_clotho.py
+++ tests/package/utils/test_clotho.py
@@ -1,3 +1,5 @@
+import pytest
+
 from audyn.utils.data.clotho import ClothoTextPreprocessor
 from audyn.utils.data.clotho.text.indexing import ClothoTextIndexer
 from audyn.utils.data.clotho.text.normalization import ClothoTextNormalizer
@@ -5,11 +7,15 @@ from audyn.utils.data.clotho.text.symbols import BOS_SYMBOL, EOS_SYMBOL
 from audyn.utils.data.clotho.text.tokenization import ClothoTextTokenizer
 
 
-def test_clotho_text_preprocessor() -> None:
-    text_preprocessor = ClothoTextPreprocessor()
+@pytest.mark.parametrize("include_mask_token", [True, False])
+def test_clotho_text_preprocessor(include_mask_token: bool) -> None:
+    text_preprocessor = ClothoTextPreprocessor(include_mask_token=include_mask_token)
 
     text = "A car passes in front of me, then a bird starts singing."
     expected_indices = [0, 2, 525, 2553, 1857, 1535, 2432, 2206, 3846, 2, 314, 3612, 3352, 1]
+
+    if include_mask_token:
+        expected_indices = [index + 1 for index in expected_indices]
 
     indices = text_preprocessor(text, return_type=int)
 
@@ -38,8 +44,9 @@ def test_clotho_text_tokenizer() -> None:
     assert tokens == expected_tokens
 
 
-def test_clotho_text_indexer() -> None:
-    indexer = ClothoTextIndexer()
+@pytest.mark.parametrize("include_mask_token", [True, False])
+def test_clotho_text_indexer(include_mask_token: bool) -> None:
+    indexer = ClothoTextIndexer(include_mask_token=include_mask_token)
 
     tokens = [
         "a",
@@ -58,6 +65,10 @@ def test_clotho_text_indexer() -> None:
     inserted_tokens = [BOS_SYMBOL] + tokens + [EOS_SYMBOL]
     expected_indices = [2, 525, 2553, 1857, 1535, 2432, 2206, 3846, 2, 314, 3612, 3352]
     expected_inserted_indices = [0] + expected_indices + [1]
+
+    if include_mask_token:
+        expected_indices = [index + 1 for index in expected_indices]
+        expected_inserted_indices = [index + 1 for index in expected_inserted_indices]
 
     indices = indexer(tokens, insert_bos_token=False, insert_eos_token=False)
     assert indices == expected_indices
