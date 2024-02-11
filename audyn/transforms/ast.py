8b6b5c5b6a87172edf28b892e74eb6bfe5aad4de
tky823 <tky823.main+github@gmail.com>
2024-02-12 01:21:27 +0900

    [src] Add maskings to SSASTMelSpectrogram

--- audyn/transforms/ast.py
+++ audyn/transforms/ast.py
@@ -3,6 +3,7 @@ from typing import Optional
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchaudio.transforms as aT
 
 from .kaldi import KaldiMelSpectrogram
 
@@ -22,6 +23,10 @@ class SelfSupervisedAudioSpectrogramTransformerMelSpectrogram(nn.Module):
             torchaudio.compliance.kaldi.fbank.
         n_frames (int, optional): Number of time frames. Shorter spectrogram is padded
             with 0. Longer spectrogram is trimed.
+        freq_mask_param (int, optional): Parameter of frequency masking. This masking is
+            deactivated when ``self.training`` is ``False``.
+        time_mask_param (int, optional): Parameter of time masking. This masking is
+            deactivated when ``self.training`` is ``False``.
         mean (float): Mean of spectrogram in dataset. Default: ``0``.
         std (float): Standard deviation of spectrogram in dataset. Default: ``1``.
         take_log (bool): Whether to take log features.
@@ -31,7 +36,7 @@ class SelfSupervisedAudioSpectrogramTransformerMelSpectrogram(nn.Module):
     # ported from
     # https://github.com/YuanGongND/ssast/blob/a1a3eecb94731e226308a6812f2fbf268d789caf/src/dataloader.py#L95-L144
     # and
-    # https://github.com/YuanGongND/ssast/blob/a1a3eecb94731e226308a6812f2fbf268d789caf/src/dataloader.py#L198-L202
+    # https://github.com/YuanGongND/ssast/blob/a1a3eecb94731e226308a6812f2fbf268d789caf/src/dataloader.py#L183-L202
     # TODO: Mel-spectrogram transform for AST
 
     def __init__(
@@ -39,6 +44,8 @@ class SelfSupervisedAudioSpectrogramTransformerMelSpectrogram(nn.Module):
         sample_rate: int,
         n_mels: Optional[int] = None,
         n_frames: Optional[int] = None,
+        freq_mask_param: Optional[int] = None,
+        time_mask_param: Optional[int] = None,
         mean: float = 0,
         std: float = 1,
         take_log: bool = True,
@@ -59,6 +66,16 @@ class SelfSupervisedAudioSpectrogramTransformerMelSpectrogram(nn.Module):
         }
 
         self.transform = KaldiMelSpectrogram(sample_rate, n_mels=n_mels, fbank_kwargs=fbank_kwargs)
+
+        if freq_mask_param is None:
+            self.frequency_masking = None
+        else:
+            self.frequency_masking = aT.FrequencyMasking(freq_mask_param)
+
+        if time_mask_param is None:
+            self.time_masking = None
+        else:
+            self.time_masking = aT.TimeMasking(time_mask_param)
 
     def forward(self, waveform: torch.Tensor) -> torch.Tensor:
         """Mel-spectrogram transform.
@@ -83,6 +100,13 @@ class SelfSupervisedAudioSpectrogramTransformerMelSpectrogram(nn.Module):
         if n_frames is not None:
             padding = n_frames - spectrogram.size(-1)
             spectrogram = F.pad(spectrogram, (0, padding))
+
+        if self.training:
+            if self.frequency_masking is not None:
+                spectrogram = self.frequency_masking(spectrogram)
+
+            if self.time_masking is not None:
+                spectrogram = self.time_masking(spectrogram)
 
         spectrogram = (spectrogram - mean) / (2 * std)
 
