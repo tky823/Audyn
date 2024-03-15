23a525901717b1f19cd83486a566035c6d5cb8f0
tky823 <tky823.main+github@gmail.com>
2024-03-15 23:27:54 +0900

    [tests] Fix timing to set seeds

--- tests/package/modules/test_rvq_modules.py
+++ tests/package/modules/test_rvq_modules.py
@@ -86,10 +86,8 @@ def test_residual_vector_quantizer() -> None:
 
 def test_residual_vector_quantizer_ddp() -> None:
     """Ensure ResidualVectorQuantizer works well for DDP."""
-    torch.manual_seed(0)
-
     port = str(torch.randint(0, 2**16, ()).item())
-    world_size = 2
+    world_size = 4
     seed, another_seed = 0, 1
 
     codebook_size = 5
