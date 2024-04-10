1acad539c982eeca78a97ab7f922db76bb58f514
tky823 <tky823.main+github@gmail.com>
2024-04-10 20:57:41 +0900

    [tests] Add tests of AST

--- tests/package/models/test_ast.py
+++ tests/package/models/test_ast.py
@@ -1,9 +1,137 @@
 import torch
+import torch.nn as nn
 
-from audyn.models.ast import PositionalPatchEmbedding
+from audyn.models.ast import (
+    AudioSpectrogramTransformer,
+    HeadTokensAggregator,
+    MLPHead,
+    PositionalPatchEmbedding,
+)
 
 
-def test_ssast_positional_patch_embedding() -> None:
+def test_official_ast() -> None:
+    torch.manual_seed(0)
+
+    d_model, out_channels = 768, 35
+    n_bins, n_frames = 128, 100
+    kernel_size = (16, 16)
+    stride = (10, 10)
+
+    insert_cls_token = True
+    insert_dist_token = True
+
+    nhead = 12
+    dim_feedforward = 3072
+    num_layers = 12
+
+    expected_num_parameters = 85366307
+
+    patch_embedding = PositionalPatchEmbedding(
+        d_model,
+        kernel_size=kernel_size,
+        stride=stride,
+        insert_cls_token=insert_cls_token,
+        insert_dist_token=insert_dist_token,
+        n_bins=n_bins,
+        n_frames=n_frames,
+    )
+    encoder_layer = nn.TransformerEncoderLayer(
+        d_model,
+        nhead,
+        dim_feedforward=dim_feedforward,
+        activation=nn.GELU(),
+        batch_first=True,
+    )
+    norm = nn.LayerNorm(d_model)
+    transformer = nn.TransformerEncoder(
+        encoder_layer,
+        num_layers=num_layers,
+        norm=norm,
+    )
+    aggregator = HeadTokensAggregator(
+        insert_cls_token=insert_cls_token, insert_dist_token=insert_dist_token
+    )
+    head = MLPHead(d_model, out_channels)
+    model = AudioSpectrogramTransformer(
+        patch_embedding,
+        transformer,
+        aggregator=aggregator,
+        head=head,
+    )
+
+    num_parameters = 0
+
+    for p in model.parameters():
+        if p.requires_grad:
+            num_parameters += p.numel()
+
+    assert num_parameters == expected_num_parameters
+
+    # build_from_pretrained
+    model = AudioSpectrogramTransformer.build_from_pretrained(
+        "ast-base-stride10",
+        stride=stride,
+        n_bins=n_bins,
+        n_frames=n_frames,
+        head=head,
+    )
+
+    num_parameters = 0
+
+    for p in model.parameters():
+        if p.requires_grad:
+            num_parameters += p.numel()
+
+    assert num_parameters == expected_num_parameters
+
+
+def test_ast() -> None:
+    torch.manual_seed(0)
+
+    d_model, out_channels = 8, 10
+    n_bins, n_frames = 8, 30
+    kernel_size = (4, 4)
+    insert_cls_token, insert_dist_token = True, True
+    batch_size = 4
+
+    nhead = 2
+    dim_feedforward = 5
+    num_layers = 2
+
+    patch_embedding = PositionalPatchEmbedding(
+        d_model,
+        kernel_size=kernel_size,
+        insert_cls_token=insert_cls_token,
+        insert_dist_token=insert_dist_token,
+        n_bins=n_bins,
+        n_frames=n_frames,
+    )
+    norm = nn.LayerNorm(d_model)
+    encoder_layer = nn.TransformerEncoderLayer(
+        d_model, nhead, dim_feedforward=dim_feedforward, batch_first=True
+    )
+    transformer = nn.TransformerEncoder(
+        encoder_layer,
+        num_layers=num_layers,
+        norm=norm,
+    )
+    aggregator = HeadTokensAggregator(
+        insert_cls_token=insert_cls_token,
+        insert_dist_token=insert_dist_token,
+    )
+    head = MLPHead(d_model, out_channels)
+
+    model = AudioSpectrogramTransformer(
+        patch_embedding, transformer, aggregator=aggregator, head=head
+    )
+
+    input = torch.randn((batch_size, n_bins, n_frames))
+    output = model(input)
+
+    assert output.size() == (batch_size, out_channels)
+
+
+def test_ast_positional_patch_embedding() -> None:
     torch.manual_seed(0)
 
     d_model = 8
