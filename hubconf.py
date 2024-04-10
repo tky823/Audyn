4c8bc5800235648c9b4cc2f39e75d63b1ca10728
tky823 <tky823.main+github@gmail.com>
2024-04-10 20:58:56 +0900

    [src] Add ast_base to hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -3,10 +3,44 @@ from typing import Optional
 import torch.nn as nn
 from torch.nn.common_types import _size_2_t
 
+from audyn.models.ast import AudioSpectrogramTransformer as _AST
 from audyn.models.ssast import (
     MultiTaskSelfSupervisedAudioSpectrogramTransformerMaskedPatchModel as _MultiTaskSSASTMPM,
 )
 from audyn.models.ssast import SelfSupervisedAudioSpectrogramTransformer as _SSAST
+
+
+def ast_base(
+    pretrained_stride: int = 10,
+    stride: Optional[_size_2_t] = None,
+    n_bins: Optional[int] = None,
+    n_frames: Optional[int] = None,
+    aggregator: Optional[nn.Module] = None,
+    head: Optional[nn.Module] = None,
+) -> _AST:
+    """Build AudioSpectrogramTransformer.
+
+    Args:
+        pretrained_stride (int): Stride used in pretraining.
+        aggregator (nn.Module, optional): Aggregator module.
+        head (nn.Module, optional): Head module.
+
+    """
+    if pretrained_stride == 10:
+        pretrained_model_name = "ast-base-stride10"
+    else:
+        raise ValueError(f"{pretrained_stride} is not supported as pretrained_stride.")
+
+    model = _AST.build_from_pretrained(
+        pretrained_model_name,
+        stride=stride,
+        n_bins=n_bins,
+        n_frames=n_frames,
+        aggregator=aggregator,
+        head=head,
+    )
+
+    return model
 
 
 def multitask_ssast_base_400(
