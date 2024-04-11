c97608e0d9acce66e82d861886d8bab86d7d23da
tky823 <tky823.main+github@gmail.com>
2024-04-12 01:06:24 +0900

    [recipes] Add train.py for SSAST training

---
+++ recipes/AudioSet/SSAST/local/train.py
@@ -0,0 +1,69 @@
+from omegaconf import DictConfig
+
+import audyn
+from audyn.utils import (
+    instantiate,
+    instantiate_criterion,
+    instantiate_grad_clipper,
+    instantiate_lr_scheduler,
+    instantiate_model,
+    instantiate_optimizer,
+    setup_system,
+)
+from audyn.utils.data import BaseDataLoaders
+from audyn.utils.driver import BaseTrainer
+from audyn.utils.model import set_device
+
+
+@audyn.main()
+def main(config: DictConfig) -> None:
+    setup_system(config)
+
+    dataset_config = config.train.dataset
+    dataloader_config = config.train.dataloader
+
+    train_dataset = instantiate(dataset_config.train)
+    validation_dataset = instantiate(dataset_config.validation)
+
+    train_loader = instantiate(
+        dataloader_config.train,
+        train_dataset,
+    )
+    validation_loader = instantiate(
+        dataloader_config.validation,
+        validation_dataset,
+    )
+    loaders = BaseDataLoaders(train_loader, validation_loader)
+
+    model = instantiate_model(config.model)
+    model = set_device(
+        model,
+        accelerator=config.system.accelerator,
+        is_distributed=config.system.distributed.enable,
+        ddp_kwargs=config.train.ddp_kwargs,
+    )
+    optimizer = instantiate_optimizer(config.optimizer, model)
+    lr_scheduler = instantiate_lr_scheduler(config.lr_scheduler, optimizer)
+    grad_clipper = instantiate_grad_clipper(config.train.clip_gradient, model.parameters())
+    criterion = instantiate_criterion(config.criterion)
+    criterion = set_device(
+        criterion,
+        accelerator=config.system.accelerator,
+        is_distributed=config.system.distributed.enable,
+        ddp_kwargs=config.train.ddp_kwargs,
+    )
+
+    trainer = BaseTrainer(
+        loaders,
+        model,
+        optimizer,
+        lr_scheduler=lr_scheduler,
+        grad_clipper=grad_clipper,
+        criterion=criterion,
+        config=config,
+    )
+    trainer.run()
+
+
+if __name__ == "__main__":
+    main()
