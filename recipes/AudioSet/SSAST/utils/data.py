2641b38dd9d5f8a8450e690116c556481b6c4cea
tky823 <tky823.main+github@gmail.com>
2024-04-12 23:24:38 +0900

    [recipes] Fix args of SSASTAudioSetCollater

--- recipes/AudioSet/SSAST/utils/data.py
+++ recipes/AudioSet/SSAST/utils/data.py
@@ -20,23 +20,32 @@ class SSASTAudioSetCollater(BaseCollater):
             aT.MelSpectrogram,
             nn.Module,
         ],
-        waveform_key: str = "waveform",
-        melspectrogram_key: str = "melspectrogram",
-        filename_key: str = "filename",
+        audio_key_in: str = "audio.m4a",
+        sample_rate_key_in: str = "sample_rate.pth",
+        filename_key_in: str = "__key__",
+        waveform_key_out: str = "waveform",
+        melspectrogram_key_out: str = "melspectrogram",
+        filename_key_out: str = "filename",
         duration: float = 10,
     ) -> None:
         super().__init__()
 
         self.melspectrogram_transform = melspectrogram_transform
-        self.waveform_key = waveform_key
-        self.melspectrogram_key = melspectrogram_key
-        self.filename_key = filename_key
+        self.audio_key_in = audio_key_in
+        self.sample_rate_key_in = sample_rate_key_in
+        self.filename_key_in = filename_key_in
+        self.waveform_key_out = waveform_key_out
+        self.melspectrogram_key_out = melspectrogram_key_out
+        self.filename_key_out = filename_key_out
         self.duration = duration
 
     def __call__(self, batch: List[Dict[str, Any]]) -> Dict[str, Any]:
-        waveform_key = self.waveform_key
-        melspectrogram_key = self.melspectrogram_key
-        filename_key = self.filename_key
+        audio_key_in = self.audio_key_in
+        sample_rate_key_in = self.sample_rate_key_in
+        filename_key_in = self.filename_key_in
+        waveform_key_out = self.waveform_key_out
+        melspectrogram_key_out = self.melspectrogram_key_out
+        filename_key_out = self.filename_key_out
         melspectrogram_transform = self.melspectrogram_transform
         duration = self.duration
 
@@ -54,9 +63,9 @@ class SSASTAudioSetCollater(BaseCollater):
         batched_filename = []
 
         for sample in batch:
-            waveform = sample["audio.m4a"]
-            sample_rate = sample["sample_rate.pth"]
-            filename = sample["__key__"]
+            waveform = sample[audio_key_in]
+            sample_rate = sample[sample_rate_key_in]
+            filename = sample[filename_key_in]
 
             if (
                 melspectrogram_sample_rate is not None
@@ -76,9 +85,9 @@ class SSASTAudioSetCollater(BaseCollater):
         batched_melspectrogram = torch.stack(batched_melspectrogram, dim=0)
 
         output = {
-            waveform_key: batched_waveform,
-            melspectrogram_key: batched_melspectrogram,
-            filename_key: batched_filename,
+            waveform_key_out: batched_waveform,
+            melspectrogram_key_out: batched_melspectrogram,
+            filename_key_out: batched_filename,
         }
 
         return output
