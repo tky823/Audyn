02bbb8b4c4f2122cc5f027ae8989b2797f4b06d2
tky823 <tky823.main+github@gmail.com>
2024-04-19 00:37:01 +0900

    [tests] Add test of build_from_pretrained for PaSST

--- tests/package/models/test_passt.py
+++ tests/package/models/test_passt.py
@@ -4,7 +4,93 @@ import torch.nn as nn
 
 from audyn.models.ast import HeadTokensAggregator, MLPHead
 from audyn.models.passt import DisentangledPositionalPatchEmbedding, PaSST
-from audyn.modules.passt import UnstructuredPatchout
+from audyn.modules.passt import StructuredPatchout, UnstructuredPatchout
+
+
+def test_official_passt() -> None:
+    torch.manual_seed(0)
+
+    d_model, out_channels = 768, 527
+    n_bins, n_frames = 128, 998
+    kernel_size = (16, 16)
+    stride = (10, 10)
+    insert_cls_token, insert_dist_token = True, True
+
+    nhead = 12
+    dim_feedforward = 3072
+    num_layers = 12
+    num_frequency_drops = 4
+    num_time_drops = 40
+    sample_wise = False
+
+    expected_num_parameters = 85746959
+
+    patch_embedding = DisentangledPositionalPatchEmbedding(
+        d_model,
+        kernel_size=kernel_size,
+        stride=stride,
+        insert_cls_token=insert_cls_token,
+        insert_dist_token=insert_dist_token,
+        n_bins=n_bins,
+        n_frames=n_frames,
+    )
+    norm = nn.LayerNorm(d_model)
+    dropout = StructuredPatchout(
+        num_frequency_drops=num_frequency_drops,
+        num_time_drops=num_time_drops,
+        sample_wise=sample_wise,
+    )
+    encoder_layer = nn.TransformerEncoderLayer(
+        d_model,
+        nhead,
+        dim_feedforward=dim_feedforward,
+        dropout=0,
+        batch_first=True,
+    )
+    transformer = nn.TransformerEncoder(
+        encoder_layer,
+        num_layers=num_layers,
+        norm=norm,
+    )
+    aggregator = HeadTokensAggregator(
+        insert_cls_token=insert_cls_token,
+        insert_dist_token=insert_dist_token,
+    )
+    head = MLPHead(d_model, out_channels)
+
+    model = PaSST(
+        patch_embedding,
+        dropout,
+        transformer,
+        aggregator=aggregator,
+        head=head,
+    )
+
+    num_parameters = 0
+
+    for p in model.parameters():
+        if p.requires_grad:
+            num_parameters += p.numel()
+
+    assert num_parameters == expected_num_parameters
+
+    # build_from_pretrained
+    model = PaSST.build_from_pretrained(
+        "passt-base-stride10-struct-ap0.476-swa",
+        stride=stride,
+        n_bins=n_bins,
+        n_frames=n_frames,
+        aggregator=aggregator,
+        head=head,
+    )
+
+    num_parameters = 0
+
+    for p in model.parameters():
+        if p.requires_grad:
+            num_parameters += p.numel()
+
+    assert num_parameters == expected_num_parameters
 
 
 @pytest.mark.parametrize("sample_wise", [True, False])
@@ -39,6 +125,7 @@ def test_passt(sample_wise: bool) -> None:
         d_model,
         nhead,
         dim_feedforward=dim_feedforward,
+        dropout=0,
         batch_first=True,
     )
     transformer = nn.TransformerEncoder(
