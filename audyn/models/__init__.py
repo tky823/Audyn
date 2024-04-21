b26f13fde1fc95a68c540b4f254c29b7f95f226e
tky823 <tky823.main+github@gmail.com>
2024-04-21 14:24:07 +0900

    [src] Import HiFi-GAN from audyn.models

--- audyn/models/__init__.py
+++ audyn/models/__init__.py
@@ -1,5 +1,6 @@
 from .ast import AST, AudioSpectrogramTransformer
 from .fastspeech import FastSpeech, MultiSpeakerFastSpeech
+from .hifigan import HiFiGANDiscriminator, HiFiGANGenerator
 from .roformer import RoFormerDecoder, RoFormerDecoderLayer, RoFormerEncoder, RoFormerEncoderLayer
 from .rvqvae import RVQVAE
 from .soundstream import SoundStream
@@ -15,12 +16,18 @@ from .waveglow import MultiSpeakerWaveGlow, WaveGlow
 from .wavenet import MultiSpeakerWaveNet, WaveNet
 
 __all__ = [
+    # WaveNet
     "WaveNet",
     "MultiSpeakerWaveNet",
+    # WaveGlow
     "WaveGlow",
     "MultiSpeakerWaveGlow",
+    # FastSpeech
     "FastSpeech",
     "MultiSpeakerFastSpeech",
+    # HiFi-GAN
+    "HiFiGANGenerator",
+    "HiFiGANDiscriminator",
     "BaseVAE",
     "VQVAE",
     "RVQVAE",
