4a92536d7b0e2a90861ae2cbeaa569633cf9cd26
tky823 <tky823.main+github@gmail.com>
2024-04-21 14:12:56 +0900

    [src] Import major models by audyn.models

--- audyn/models/__init__.py
+++ audyn/models/__init__.py
@@ -1,6 +1,13 @@
+from .ast import AST, AudioSpectrogramTransformer
 from .fastspeech import FastSpeech, MultiSpeakerFastSpeech
 from .rvqvae import RVQVAE
 from .soundstream import SoundStream
+from .ssast import (
+    SSAST,
+    SSASTMPM,
+    MultiTaskSelfSupervisedAudioSpectrogramTransformerMaskedPatchModel,
+    SelfSupervisedAudioSpectrogramTransformer,
+)
 from .vae import BaseVAE
 from .vqvae import VQVAE
 from .waveglow import MultiSpeakerWaveGlow, WaveGlow
@@ -17,4 +24,10 @@ __all__ = [
     "VQVAE",
     "RVQVAE",
     "SoundStream",
+    "AudioSpectrogramTransformer",
+    "AST",
+    "MultiTaskSelfSupervisedAudioSpectrogramTransformerMaskedPatchModel",
+    "SelfSupervisedAudioSpectrogramTransformer",
+    "SSASTMPM",
+    "SSAST",
 ]
