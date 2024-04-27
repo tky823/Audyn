98c1b9eec59f96649e5a1ede78a2aeea42ec95ac
tky823 <tky823.main+github@gmail.com>
2024-04-27 23:23:11 +0900

    [src] Rename collater

--- audyn/utils/data/collater.py
+++ audyn/utils/data/collater.py
@@ -1,14 +1,30 @@
-from abc import abstractmethod
-from typing import Any, List
+from typing import Any, List, Optional
+
+from . import default_collate_fn
+from .dataset import Composer
 
 __all__ = [
-    "BaseCollater",
+    "Collater",
 ]
 
 
-class BaseCollater:
+class Collater:
     """Base class of collater."""
 
-    @abstractmethod
+    def __init__(self, composer: Optional[Composer] = None) -> None:
+        self.composer = composer
+
     def __call__(self, batch: List[Any]) -> None:
-        pass
+        composer = self.composer
+
+        if composer is None:
+            list_batch = batch
+        else:
+            list_batch = []
+
+            for sample in composer(batch):
+                list_batch.append(sample)
+
+        dict_batch = default_collate_fn(list_batch)
+
+        return dict_batch
