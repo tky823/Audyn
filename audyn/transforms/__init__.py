1f0b889fa09cc9ff4ffd777997d5112ec7d78ac6
tky823 <tky823.main+github@gmail.com>
2024-05-12 22:25:24 +0900

    [src] Implement MelSpectrogram compatible with librosa

--- audyn/transforms/__init__.py
+++ audyn/transforms/__init__.py
@@ -2,10 +2,13 @@ from .ast import ASTMelSpectrogram, AudioSpectrogramTransformerMelSpectrogram
 from .cqt import CQT, ConstantQTransform
 from .hubert import HuBERTMFCC
 from .kaldi import KaldiMelSpectrogram, KaldiMFCC
+from .librosa import LibrosaMelSpectrogram
 
 __all__ = [
     "ConstantQTransform",
     "CQT",
+    # librosa
+    "LibrosaMelSpectrogram",
     # kaldi
     "KaldiMelSpectrogram",
     "KaldiMFCC",
