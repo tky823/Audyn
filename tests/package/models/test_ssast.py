c3fda8e83dec31307386ff68226eaa37d5dc140f
tky823 <tky823.main+github@gmail.com>
2024-05-13 19:53:20 +0900

    [tests] Use looser atol in test_ssast_length

--- tests/package/models/test_ssast.py
+++ tests/package/models/test_ssast.py
@@ -484,7 +484,7 @@ def test_ssast_length(backbone: str, aggregation: str) -> None:
         input, _ = torch.split(longer_input, [n_frames, n_frames], dim=-1)
         output = model(input, length=length)
 
-    allclose(output, longer_output)
+    allclose(output, longer_output, atol=1e-6)
 
 
 @pytest.mark.parametrize("sample_wise", [True, False])
