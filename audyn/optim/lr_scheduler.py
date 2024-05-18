96253e6af8519986907fac6450e3bd022bcb0925
tky823 <tky823.main+github@gmail.com>
2024-05-18 16:35:00 +0900

    [src] Use LambdaLR as backbone of _DummyLRScheduler

--- audyn/optim/lr_scheduler.py
+++ audyn/optim/lr_scheduler.py
@@ -1,6 +1,7 @@
 from typing import Any, Dict, List, Optional, Union
 
 from torch.optim import Optimizer
+from torch.optim.lr_scheduler import LambdaLR
 
 try:
     from torch.optim.lr_scheduler import LRScheduler as _LRScheduler
