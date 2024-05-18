8cd4f11f02599a7f39fc61cf70924718aada524c
tky823 <tky823.main+github@gmail.com>
2024-05-19 03:27:32 +0900

    [tests] Add assertion of output size of BirdCLEF2024BaselineMelSpectrogram

--- tests/package/transforms/test_birdclef_transforms.py
+++ tests/package/transforms/test_birdclef_transforms.py
@@ -6,12 +6,13 @@ from audyn.transforms.birdclef import BirdCLEF2024BaselineMelSpectrogram
 def test_birdclef2024_baseline_melspectrogram() -> None:
     torch.manual_seed(0)
 
+    batch_size = 4
     sample_rate = 32000
     duration = 15
 
     transform = BirdCLEF2024BaselineMelSpectrogram(sample_rate=sample_rate)
-    waveform = torch.randn((duration,))
+    waveform = torch.randn((batch_size, int(sample_rate * duration)))
 
     melspectrogram = transform(waveform)
 
-    print(melspectrogram.size())
+    assert melspectrogram.size()[:2] == (batch_size, 256)
