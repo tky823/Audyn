382574ac694f2a7119e44e168c780a8c77aff6d7
tky823 <tky823.main+github@gmail.com>
2024-05-29 21:19:11 +0900

    [src] Add BirdCLEF2023PrimaryLabelDataset to audyn.utils.data.birdclef

--- audyn/utils/data/birdclef/__init__.py
+++ audyn/utils/data/birdclef/__init__.py
@@ -2,6 +2,7 @@ from .birdclef2022 import num_primary_labels as num_birdclef2022_primary_labels
 from .birdclef2022 import primary_labels as birdclef2022_primary_labels
 from .birdclef2023 import num_primary_labels as num_birdclef2023_primary_labels
 from .birdclef2023 import primary_labels as birdclef2023_primary_labels
+from .birdclef2023.dataset import BirdCLEF2023PrimaryLabelDataset
 from .birdclef2024 import num_primary_labels as num_birdclef2024_primary_labels
 from .birdclef2024 import primary_labels as birdclef2024_primary_labels
 from .birdclef2024.collator import BirdCLEF2024BaselineCollator
@@ -15,6 +16,7 @@ __all__ = [
     # for BirdCLEF2023
     "birdclef2023_primary_labels",
     "num_birdclef2023_primary_labels",
+    "BirdCLEF2023PrimaryLabelDataset",
     # for BirdCLEF2024
     "birdclef2024_primary_labels",
     "num_birdclef2024_primary_labels",
