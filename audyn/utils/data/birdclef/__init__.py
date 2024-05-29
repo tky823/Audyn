793e5ba0314e6e12fac5f5c31c815198ecdd55ad
tky823 <tky823.main+github@gmail.com>
2024-05-29 20:21:56 +0900

    [src] Add subpackage for BirdCLEF challenges

---
+++ audyn/utils/data/birdclef/__init__.py
@@ -0,0 +1,26 @@
+from .birdclef2022 import num_primary_labels as num_birdclef2022_primary_labels
+from .birdclef2022 import primary_labels as birdclef2022_primary_labels
+from .birdclef2023 import num_primary_labels as num_birdclef2023_primary_labels
+from .birdclef2023 import primary_labels as birdclef2023_primary_labels
+from .birdclef2024 import num_primary_labels as num_birdclef2024_primary_labels
+from .birdclef2024 import primary_labels as birdclef2024_primary_labels
+from .birdclef2024.collator import BirdCLEF2024BaselineCollator
+from .birdclef2024.composer import BirdCLEF2024AudioComposer, BirdCLEF2024PrimaryLabelComposer
+from .birdclef2024.dataset import BirdCLEF2024AudioDataset, BirdCLEF2024PrimaryLabelDataset
+
+__all__ = [
+    # for BirdCLEF2022
+    "birdclef2022_primary_labels",
+    "num_birdclef2022_primary_labels",
+    # for BirdCLEF2023
+    "birdclef2023_primary_labels",
+    "num_birdclef2023_primary_labels",
+    # for BirdCLEF2024
+    "birdclef2024_primary_labels",
+    "num_birdclef2024_primary_labels",
+    "BirdCLEF2024PrimaryLabelDataset",
+    "BirdCLEF2024AudioDataset",
+    "BirdCLEF2024PrimaryLabelComposer",
+    "BirdCLEF2024AudioComposer",
+    "BirdCLEF2024BaselineCollator",
+]
