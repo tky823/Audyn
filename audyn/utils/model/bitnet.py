33feef77c5b53a4bb7f31e5d841359ca932590e3
tky823 <tky823.main+github@gmail.com>
2024-05-30 23:39:41 +0900

    [src] Support remove_bias in convert_to_bit_linear_158

--- audyn/utils/model/bitnet.py
+++ audyn/utils/model/bitnet.py
@@ -12,16 +12,40 @@ def convert_to_bit_linear_158(
     module: nn.Module,
     bits: int = 8,
     eps: float = 1e-5,
+    remove_bias: bool = False,
 ) -> nn.Module:
-    """Convert nn.Linear to BitLinearB158 in given module."""
+    """Convert nn.Linear to BitLinearB158 in given module.
+
+    Args:
+        module (nn.Module): Module to be converted.
+        bits (int): Number of quantization bits in scale parameter.
+        eps (float): Tiny value to avoid zero division.
+        remove_bias (bool): If ``True``, bias is forced to be removed.
+
+    Returns:
+        nn.Module: Converted module.
+
+    """
     if isinstance(module, nn.Linear):
-        module = convert_linear_to_bit_linear_158(module, bits, eps=eps)
+        module = convert_linear_to_bit_linear_158(
+            module,
+            bits,
+            eps=eps,
+            remove_bias=remove_bias,
+        )
     else:
         for name, child_module in module.named_children():
             if isinstance(child_module, nn.Linear):
-                converted = convert_linear_to_bit_linear_158(child_module, bits, eps=eps)
+                converted = convert_linear_to_bit_linear_158(
+                    child_module,
+                    bits=bits,
+                    eps=eps,
+                    remove_bias=remove_bias,
+                )
             else:
-                converted = convert_to_bit_linear_158(child_module, bits, eps=eps)
+                converted = convert_to_bit_linear_158(
+                    child_module, bits=bits, eps=eps, remove_bias=remove_bias
+                )
 
             setattr(module, name, converted)
 
@@ -32,6 +56,7 @@ def convert_linear_to_bit_linear_158(
     module: nn.Linear,
     bits: int = 8,
     eps: float = 1e-5,
+    remove_bias: bool = False,
 ) -> BitLinearB158:
     """Convert nn.Linear to BitLinearB158."""
     factory_kwargs = {
@@ -57,7 +82,7 @@ def convert_linear_to_bit_linear_158(
     )
     converted.weight.data.copy_(module.weight.data)
 
-    if bias:
+    if bias and not remove_bias:
         converted.bias.data.copy_(module.bias.data)
 
     return converted
