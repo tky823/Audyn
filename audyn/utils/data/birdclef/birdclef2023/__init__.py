51fc370b58d312f44c0fb27812171fb58b25b9c4
tky823 <tky823.main+github@gmail.com>
2024-05-30 19:23:18 +0900

    [src] Add decode_csv_line for each BirdCLEF challenge

--- audyn/utils/data/birdclef/birdclef2023/__init__.py
+++ audyn/utils/data/birdclef/birdclef2023/__init__.py
@@ -1,16 +1,16 @@
 import csv
-from typing import List, Tuple
+from typing import Any, Dict, List, Tuple
 
 import torch
 
-from .._common import decode_csv_line
+from .._common import decode_csv_line as _decode_csv_line
 from ._download import download_birdclef2023_primary_labels
 
 __all__ = [
     "primary_labels",
     "num_primary_labels",
     "stratified_split",
-    "decode_csv_line",  # for compatibility with birdclef2024
+    "decode_csv_line",
 ]
 
 primary_labels = download_birdclef2023_primary_labels()
@@ -65,3 +65,32 @@ def stratified_split(
             validation_filenames.append(_filenames[idx])
 
     return train_filenames, validation_filenames
+
+
+def decode_csv_line(line: List[str]) -> Dict[str, Any]:
+    """Decode line of train_metadata.csv in BirdCLEF2023.
+
+    Args:
+        line (list): One line of train_metadata.csv split by comma (,).
+
+    Returns:
+        dict: Dictionary containing metadata of given line.
+
+    .. note::
+
+        Returned dictionary contains following values.
+
+            - filename (str): Filename with out extension. e.g. ``abethr1/XC128013``.
+            - primary_label (str): Primary label of bird species.
+            - secondary_label (list): Secondary labels of bird species.
+            - type (list): Chirp types.
+            - latitude (float, optional): Latitude of recording.
+            - longitude (float, optional): Longitude of recording.
+            - scientific_name (str): Scientific name of bird.
+            - common_name (str): Common name of bird.
+            - rating (float): Rating.
+            - path (str): Path to audio file equivalent to ``filename`` + ``.ogg``.
+                e.g. ``abethr1/XC128013.ogg``.
+
+    """
+    return _decode_csv_line(line, version=2023)
