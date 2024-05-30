c58d885c6bce71c57c09fbd1f76b5aa8afc175ce
tky823 <tky823.main+github@gmail.com>
2024-05-30 23:32:46 +0900

    [tests] Add test of BitLinearB158

---
+++ tests/package/utils/test_bitnet_utils.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+
+from audyn.modules.bitnet import BitLinearB158
+from audyn.utils.model.bitnet import convert_to_bit_linear_158
+
+
+def test_convert_to_bit_linear_158() -> None:
+    torch.manual_seed(0)
+
+    model = nn.Sequential(
+        nn.Linear(10, 12, bias=True),
+        nn.Linear(12, 5, bias=False),
+        nn.Sequential(
+            nn.Linear(5, 3, bias=True),
+            nn.Linear(3, 2, bias=False),
+        ),
+    )
+    expected_model = nn.Sequential(
+        BitLinearB158(10, 12, bias=True),
+        BitLinearB158(12, 5, bias=False),
+        nn.Sequential(
+            BitLinearB158(5, 3, bias=True),
+            BitLinearB158(3, 2, bias=False),
+        ),
+    )
+
+    model = convert_to_bit_linear_158(model, bits=8)
+
+    for p, p_expected in zip(model.parameters(), expected_model.parameters()):
+        assert p.size() == p_expected.size()
