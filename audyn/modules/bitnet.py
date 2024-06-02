8a5727c4662e953468443421955d768bca2b5636
tky823 <tky823.main+github@gmail.com>
2024-06-02 12:57:07 +0900

    [src] Add BitMultiheadAttentionB158

--- audyn/modules/bitnet.py
+++ audyn/modules/bitnet.py
@@ -1,13 +1,17 @@
 import math
-from typing import Optional
+from typing import Optional, Tuple
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
+from ..functional.activation import scaled_dot_product_attention
 from ..functional.bitnet import bit_linear_b158
+from .activation import _MultiheadAttention
 
 __all__ = [
     "BitLinearB158",
+    "BitMultiheadAttentionB158",
     "RoundClip",
 ]
 
@@ -80,6 +84,186 @@ class BitLinearB158(nn.Module):
         bias = self.bias is not None
 
         return f"in_features={in_features}, out_features={out_features}, bits={bits}, bias={bias}"
+
+
+class BitMultiheadAttentionB158(_MultiheadAttention):
+    """Multihead attention using BitLinearB158.
+
+    For parameters, see details of nn.MultiheadAttention.
+    """
+
+    def __init__(
+        self,
+        embed_dim: int,
+        num_heads: int,
+        dropout: float = 0.0,
+        bias: bool = True,
+        add_bias_kv: bool = False,
+        add_zero_attn: bool = False,
+        kdim: Optional[int] = None,
+        vdim: Optional[int] = None,
+        batch_first: bool = False,
+        bits: int = 8,
+        eps: float = 1e-5,
+        **kwargs,
+    ) -> None:
+        super().__init__(
+            embed_dim,
+            num_heads,
+            dropout=dropout,
+            bias=bias,
+            add_bias_kv=add_bias_kv,
+            add_zero_attn=add_zero_attn,
+            kdim=kdim,
+            vdim=vdim,
+            batch_first=batch_first,
+            **kwargs,
+        )
+
+        from ..utils.model.bitnet import convert_linear_to_bit_linear_158
+
+        self.out_proj = convert_linear_to_bit_linear_158(
+            self.out_proj,
+            bits=bits,
+            eps=eps,
+            remove_bias=False,
+        )
+
+        self.bits = bits
+        self.eps = eps
+
+        if add_bias_kv:
+            raise NotImplementedError("add_bias_kv is not supported.")
+
+        if add_zero_attn:
+            raise NotImplementedError("add_zero_attn is not supported.")
+
+    def forward(
+        self,
+        query: torch.Tensor,
+        key: torch.Tensor,
+        value: torch.Tensor,
+        key_padding_mask: Optional[torch.Tensor] = None,
+        need_weights: bool = True,
+        attn_mask: Optional[torch.Tensor] = None,
+        average_attn_weights: bool = True,
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor]]:
+        embed_dim = self.embed_dim
+        dropout = self.dropout
+        batch_first = self.batch_first
+        num_heads = self.num_heads
+        in_proj_weight = self.in_proj_weight
+        in_proj_bias = self.in_proj_bias
+        bits = self.bits
+        eps = self.eps
+
+        head_dim = embed_dim // num_heads
+
+        if batch_first:
+            # make sure that the transpose op does not affect the "is" property
+            if key is value:
+                if query is key:
+                    query = key = value = query.transpose(1, 0)
+                else:
+                    query, key = (x.transpose(1, 0) for x in (query, key))
+                    value = key
+            else:
+                query, key, value = (x.transpose(1, 0) for x in (query, key, value))
+
+        query_length, batch_size, _ = query.size()
+        key_length, _, _ = key.size()
+
+        key_padding_mask = F._canonical_mask(
+            mask=key_padding_mask,
+            mask_name="key_padding_mask",
+            other_type=F._none_or_dtype(attn_mask),
+            other_name="attn_mask",
+            target_type=query.dtype,
+        )
+
+        attn_mask = F._canonical_mask(
+            mask=attn_mask,
+            mask_name="attn_mask",
+            other_type=None,
+            other_name="",
+            target_type=query.dtype,
+            check_other=False,
+        )
+
+        if self._qkv_same_embed_dim:
+            q_proj_weight, k_proj_weight, v_proj_weight = torch.split(
+                in_proj_weight, [embed_dim] * 3, dim=-2
+            )
+        else:
+            q_proj_weight = self.q_proj_weight
+            k_proj_weight = self.k_proj_weight
+            v_proj_weight = self.v_proj_weight
+
+        if self.in_proj_bias is None:
+            q_proj_bias, k_proj_bias, v_proj_bias = None, None, None
+        else:
+            q_proj_bias, k_proj_bias, v_proj_bias = torch.split(
+                in_proj_bias, [embed_dim] * 3, dim=0
+            )
+
+        q = bit_linear_b158(
+            query,
+            q_proj_weight,
+            bias=q_proj_bias,
+            bits=bits,
+            eps=eps,
+        )
+        k = bit_linear_b158(
+            key,
+            k_proj_weight,
+            bias=k_proj_bias,
+            bits=bits,
+            eps=eps,
+        )
+        v = bit_linear_b158(
+            value,
+            v_proj_weight,
+            bias=v_proj_bias,
+            bits=bits,
+            eps=eps,
+        )
+
+        q = q.view(query_length, batch_size, num_heads, head_dim)
+        k = k.view(key_length, batch_size, num_heads, head_dim)
+        v = v.view(key_length, batch_size, num_heads, head_dim)
+
+        q = q.permute(1, 2, 0, 3)
+        k = k.permute(1, 2, 0, 3)
+        v = v.permute(1, 2, 0, 3)
+
+        dropout_p = dropout if self.training else 0
+
+        qkv, attn_weights = scaled_dot_product_attention(
+            q,
+            k,
+            v,
+            key_padding_mask=key_padding_mask,
+            attn_mask=attn_mask,
+            dropout_p=dropout_p,
+            need_weights=need_weights,
+        )
+
+        if batch_first:
+            qkv = qkv.permute(0, 2, 1, 3).contiguous()
+            qkv = qkv.view(batch_size, query_length, embed_dim)
+        else:
+            qkv = qkv.permute(2, 0, 1, 3).contiguous()
+            qkv = qkv.view(query_length, batch_size, embed_dim)
+
+        output = self.out_proj(qkv)
+
+        if average_attn_weights and need_weights:
+            attn_weights = attn_weights.mean(dim=1)
+
+        if not need_weights:
+            attn_weights = None
+
+        return output, attn_weights
 
 
 class RoundClip(nn.Module):
