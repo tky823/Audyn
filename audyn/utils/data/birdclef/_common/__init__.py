80a52a3c5ad729cb871297dcec0d4edebea512ad
tky823 <tky823.main+github@gmail.com>
2024-06-07 00:13:04 +0900

    [src] Fix decoding of csv line for BirdCLEF2021

--- audyn/utils/data/birdclef/_common/__init__.py
+++ audyn/utils/data/birdclef/_common/__init__.py
@@ -60,12 +60,13 @@ def decode_csv_line(
             common_name,
             _,
             _,
-            path,
+            filename,
             _,
             rating,
             _,
             _,
         ) = line
+        path = os.path.join(primary_label, filename)
     elif version == 2022:
         (
             primary_label,
