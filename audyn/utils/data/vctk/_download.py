be45409f9830a5ceaceb3d2af9e21ad37a3825e3
tky823 <tky823.main+github@gmail.com>
2024-06-16 22:41:33 +0900

    [src] Create base function to download metadata of VCTK

--- audyn/utils/data/vctk/_download.py
+++ audyn/utils/data/vctk/_download.py
@@ -19,14 +19,53 @@ def download_speakers(
     if version != "0.92":
         raise ValueError("Only version=0.92 is supported.")
 
-    filename = f"vctk-{version}-speakers.txt"
+    filename = f"vctk-{version}_speakers.txt"
 
     if root is None:
         root = os.path.join(audyn_cache_dir, "data", "vctk")
 
     if url is None:
-        url = f"https://github.com/tky823/Audyn/releases/download/v0.0.1.dev6/vctk-{version}-speakers.txt"  # noqa: E501
+        url = f"https://github.com/tky823/Audyn/releases/download/v0.0.1.dev7/vctk-{version}_speakers.txt"  # noqa: E501
 
+    speakers = _download_speakers(root, url, filename)
+
+    return speakers
+
+
+def download_valid_speakers(
+    root: Optional[str] = None,
+    url: Optional[str] = None,
+    version: str = "0.92",
+) -> List[str]:
+    """
+
+    Args:
+        version (str): Version of VCTK dataset. Only ``0.92`` is supported.
+
+    """
+    if version != "0.92":
+        raise ValueError("Only version=0.92 is supported.")
+
+    filename = f"vctk-{version}_valid-speakers.txt"
+
+    if root is None:
+        root = os.path.join(audyn_cache_dir, "data", "vctk")
+
+    if url is None:
+        url = f"https://github.com/tky823/Audyn/releases/download/v0.0.1.dev7/vctk-{version}_valid-speakers.txt"  # noqa: E501
+
+    speakers = _download_speakers(root, url, filename)
+
+    return speakers
+
+
+def _download_speakers(root: str, url: str, filename: str) -> List[str]:
+    """
+
+    Args:
+        version (str): Version of VCTK dataset. Only ``0.92`` is supported.
+
+    """
     path = os.path.join(root, filename)
 
     if not os.path.exists(path):
--- docs/audyn.utils.data.vctk.rst
+++ docs/audyn.utils.data.vctk.rst
@@ -2,3 +2,11 @@ audyn.utils.data.vctk
 =====================
 
 ``audyn.utils.data.vctk`` provides utilities for VCTK dataset.
+
+.. code-block::
+
+    >>> from audyn.utils.data.vctk import speakers, num_speakers
+    >>> print(speakers[:5])
+    ['p225', 'p226', 'p227', 'p228', 'p229']
+    >>> print(num_speakers)
+    110
