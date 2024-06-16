a7f47de717c131e7f35611aca4ebeeb4c2a73208
tky823 <tky823.main+github@gmail.com>
2024-06-17 00:03:16 +0900

    [recipes] Use mic2 for VCTK

--- recipes/VCTK/HiFiGAN/preprocess.sh
+++ recipes/VCTK/HiFiGAN/preprocess.sh
@@ -54,7 +54,7 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
 
         all_list_path=/tmp/$(uuidgen).txt
 
-        find "${wav_dir}/${spk}" -name "*.flac" | \
+        find "${wav_dir}/${spk}" -name "*_mic2.flac" | \
         sort | \
         sed "s/\.wav//" | \
         awk -F "/" -v spk=${spk} '{printf "%s/%s\n", spk, $NF}' \
