5c18bcbcc10faa35d72e43f7d8209604ec389ad7
tky823 <tky823.main+github@gmail.com>
2024-06-23 00:51:47 +0900

    [src] Define build_from_config in GANTrainer

--- audyn/utils/driver/gan.py
+++ audyn/utils/driver/gan.py
@@ -14,10 +14,18 @@ from ...metrics import MeanMetric
 from ...models.gan import BaseGAN
 from ...optim.lr_scheduler import GANLRScheduler
 from ...optim.optimizer import GANOptimizer, MovingAverageWrapper, MultiOptimizers
-from ...utils import instantiate
+from ...utils.hydra.utils import (
+    instantiate,
+    instantiate_criterion,
+    instantiate_gan_discriminator,
+    instantiate_gan_generator,
+    instantiate_grad_clipper,
+    instantiate_lr_scheduler,
+    instantiate_optimizer,
+)
+from ...utils.model import set_device
 from ..clip_grad import GANGradClipper
 from ..data import BaseDataLoaders
-from ..hydra.utils import instantiate_grad_clipper
 from ..model import unwrap
 from .base import BaseGenerator, BaseTrainer, _is_audyn_clip_gradient, _is_torch_clip_gradient
 
@@ -48,6 +56,108 @@ class GANTrainer(BaseTrainer):
             criterion=criterion,
             config=config,
         )
+
+    @classmethod
+    def build_from_config(cls, config: DictConfig) -> "GANTrainer":
+        """Instantiate Trainer from config.
+
+        Args:
+            config (DictConfig): Config to instantiate trainer.
+
+        Returns:
+            GANTrainer: Built trainer.
+
+        """
+        train_dataset = instantiate(config.train.dataset.train)
+        validation_dataset = instantiate(config.train.dataset.validation)
+
+        train_loader = instantiate(
+            config.train.dataloader.train,
+            train_dataset,
+        )
+        validation_loader = instantiate(
+            config.train.dataloader.validation,
+            validation_dataset,
+        )
+        loaders = BaseDataLoaders(train_loader, validation_loader)
+
+        generator = instantiate_gan_generator(config.model)
+        generator = set_device(
+            generator,
+            accelerator=config.system.accelerator,
+            is_distributed=config.system.distributed.enable,
+            ddp_kwargs=config.train.ddp_kwargs,
+        )
+        generator_optimizer = instantiate_optimizer(
+            config.optimizer.generator, generator.parameters()
+        )
+        generator_lr_scheduler = instantiate_lr_scheduler(
+            config.lr_scheduler.generator, generator_optimizer
+        )
+
+        if hasattr(config.train, "clip_gradient"):
+            # for backward compatibility
+            generator_grad_clipper = instantiate_grad_clipper(
+                config.train.clip_gradient.generator, generator.parameters()
+            )
+        else:
+            generator_grad_clipper = None
+
+        generator_criterion = instantiate_criterion(config.criterion.generator)
+        generator_criterion = set_device(
+            generator_criterion,
+            accelerator=config.system.accelerator,
+            is_distributed=config.system.distributed.enable,
+            ddp_kwargs=config.train.ddp_kwargs,
+        )
+
+        discriminator = instantiate_gan_discriminator(config.model)
+        discriminator = set_device(
+            discriminator,
+            accelerator=config.system.accelerator,
+            is_distributed=config.system.distributed.enable,
+            ddp_kwargs=config.train.ddp_kwargs,
+        )
+        discriminator_optimizer = instantiate_optimizer(
+            config.optimizer.discriminator, discriminator.parameters()
+        )
+        discriminator_lr_scheduler = instantiate_lr_scheduler(
+            config.lr_scheduler.discriminator, discriminator_optimizer
+        )
+
+        if hasattr(config.train, "clip_gradient"):
+            # for backward compatibility
+            discriminator_grad_clipper = instantiate_grad_clipper(
+                config.train.clip_gradient.discriminator, discriminator.parameters()
+            )
+        else:
+            discriminator_grad_clipper = None
+
+        discriminator_criterion = instantiate_criterion(config.criterion.discriminator)
+        discriminator_criterion = set_device(
+            discriminator_criterion,
+            accelerator=config.system.accelerator,
+            is_distributed=config.system.distributed.enable,
+            ddp_kwargs=config.train.ddp_kwargs,
+        )
+
+        model = BaseGAN(generator, discriminator)
+        optimizer = GANOptimizer(generator_optimizer, discriminator_optimizer)
+        lr_scheduler = GANLRScheduler(generator_lr_scheduler, discriminator_lr_scheduler)
+        grad_clipper = GANGradClipper(generator_grad_clipper, discriminator_grad_clipper)
+        criterion = GANCriterion(generator_criterion, discriminator_criterion)
+
+        trainer = GANTrainer(
+            loaders,
+            model,
+            optimizer,
+            lr_scheduler=lr_scheduler,
+            grad_clipper=grad_clipper,
+            criterion=criterion,
+            config=config,
+        )
+
+        return trainer
 
     def run(self) -> None:
         train_key, validation_key = "train", "validation"
