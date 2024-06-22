597fdaf1360963220322795898b68c36f35c62aa
tky823 <tky823.main+github@gmail.com>
2024-06-23 00:50:52 +0900

    [src] Define AutoTrainer

--- audyn/utils/driver/__init__.py
+++ audyn/utils/driver/__init__.py
@@ -1,10 +1,11 @@
-from .base import BaseGenerator, BaseTrainer
+from .base import AutoTrainer, BaseGenerator, BaseTrainer
 from .feat_to_wave import FeatToWaveGenerator, FeatToWaveTrainer
 from .gan import GANGenerator, GANTrainer
 from .text_to_feat import TextToFeatTrainer
 from .text_to_wave import CascadeTextToWaveGenerator, TextToWaveTrainer
 
 __all__ = [
+    "AutoTrainer",
     "BaseTrainer",
     "BaseGenerator",
     "TextToFeatTrainer",
