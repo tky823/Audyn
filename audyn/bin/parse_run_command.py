a54d27b9a451484d613814f3f1fafe33bc55da7c
tky823 <tky823.main+github@gmail.com>
2024-07-01 20:02:42 +0900

    [src] Start limited support of multi-node training

--- audyn/bin/parse_run_command.py
+++ audyn/bin/parse_run_command.py
@@ -1,3 +1,5 @@
+import warnings
+
 import torch
 from omegaconf import DictConfig
 
@@ -9,29 +11,51 @@ from audyn.utils.distributed import is_distributed
 def main(config: DictConfig) -> None:
     """Determine command to run script.
 
-    If ``config.system`` is distributed,
-    ``torchrun --standalone --nnodes=1 --nproc_per_node={nproc_per_node}``
-    is returned to stdout. ``torch.cuda.device_count()`` is used as ``nproc_per_node``.
+    If ``config.system`` is distributed, ``torchrun --nnodes=...`` is returned to stdout.
     Otherwise, ``python`` is returned.
 
     """
     if is_distributed(config.system):
-        nproc_per_node = torch.cuda.device_count()
+        distributed_config = config.system.distributed
 
-        if config.system.distributed.nodes is None:
+        if distributed_config.nodes is None:
             nnodes = 1
         else:
-            nnodes = config.system.distributed.nodes
+            nnodes = distributed_config.nodes
 
-        if nnodes > 1:
-            raise NotImplementedError("Only nnodes=1 is supported.")
+        if distributed_config.nproc_per_node is None:
+            nproc_per_node = torch.cuda.device_count()
+        else:
+            nproc_per_node = distributed_config.nproc_per_node
 
         cmd = "torchrun"
 
-        if nnodes == 1:
+        if nnodes > 1:
+            warnings.warn(
+                "Support of multi-node training is limited.",
+                UserWarning,
+                stacklevel=2,
+            )
+            rdzv_id = distributed_config.rdzv_id
+            rdzv_backend = distributed_config.rdzv_backend
+            rdzv_endpoint = distributed_config.rdzv_endpoint
+            max_restarts = distributed_config.max_restarts
+
+            if rdzv_id is not None:
+                cmd += f" --rdzv-id={rdzv_id}"
+
+            if rdzv_backend is None:
+                cmd += f" --rdzv-backend={rdzv_backend}"
+
+            if rdzv_endpoint is None:
+                cmd += f" --rdzv-endpoint={rdzv_endpoint}"
+
+            if max_restarts is not None:
+                cmd += f" --max-restarts={max_restarts}"
+        else:
             cmd += " --standalone"
 
-        cmd += f" --nnodes={nnodes} --nproc_per_node={nproc_per_node}"
+        cmd += f" --nnodes={nnodes} --nproc-per-node={nproc_per_node}"
     else:
         cmd = "python"
 
