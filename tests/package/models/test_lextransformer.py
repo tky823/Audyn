f65f83fc3b7fa0bc96165fc7c621245c059523ad
tky823 <tky823.main+github@gmail.com>
2024-07-05 18:27:34 +0900

    [tests] Create tests for audyn.modules.lextransformer

--- tests/package/models/test_lextransformer.py
+++ tests/package/models/test_lextransformer.py
@@ -1,6 +1,5 @@
 import pytest
 import torch
-from dummy import allclose
 
 from audyn.models.lextransformer import (
     LEXTransformerDecoder,
@@ -9,224 +8,91 @@ from audyn.models.lextransformer import (
     LEXTransformerEncoderLayer,
 )
 
+expected_warning_message = (
+    "audyn.modeles.lextransformer.{class_name} is deprecated."
+    " Use audyn.modules.lextransformer.{class_name} instead."
+)
 
-@pytest.mark.parametrize("batch_first", [True, False])
-def test_lextransformer_encoder_layer(batch_first: bool) -> None:
+
+def test_legacy_lextransformer_encoder_layer() -> None:
     torch.manual_seed(0)
 
     d_model, nhead, dim_feedforward = 8, 2, 3
-    batch_size, src_length = 4, 16
     xpos_base = 100
-    shift_size = src_length
 
-    model = LEXTransformerEncoderLayer(
-        d_model,
-        nhead,
-        dim_feedforward,
-        xpos_base=xpos_base,
-        batch_first=batch_first,
+    with pytest.warns(DeprecationWarning) as e:
+        _ = LEXTransformerEncoderLayer(
+            d_model,
+            nhead,
+            dim_feedforward,
+            xpos_base=xpos_base,
+        )
+
+    assert len(e.list) > 0
+    assert str(e.list[-1].message) == expected_warning_message.format(
+        class_name="LEXTransformerEncoderLayer"
     )
 
-    # to deactivate dropout
-    model.eval()
 
-    if batch_first:
-        src = torch.randn((batch_size, src_length, d_model))
-    else:
-        src = torch.randn((src_length, batch_size, d_model))
-
-    output = model(src)
-
-    if batch_first:
-        random_padding = torch.randn((batch_size, shift_size, d_model))
-        padded_src = torch.cat([random_padding, src], dim=1)
-    else:
-        random_padding = torch.randn((shift_size, batch_size, d_model))
-        padded_src = torch.cat([random_padding, src], dim=0)
-
-    src_key_padding_mask = torch.arange(src_length + shift_size) < shift_size
-    src_key_padding_mask = src_key_padding_mask.expand((batch_size, src_length + shift_size))
-
-    padded_output = model(padded_src, src_key_padding_mask=src_key_padding_mask)
-
-    if batch_first:
-        _, padded_output = torch.split(padded_output, [shift_size, src_length], dim=1)
-    else:
-        _, padded_output = torch.split(padded_output, [shift_size, src_length], dim=0)
-
-    allclose(output, padded_output, atol=1e-5)
-
-
-@pytest.mark.parametrize("batch_first", [True, False])
-def test_lextransformer_decoder_layer(batch_first: bool) -> None:
+def test_legacy_lextransformer_decoder_layer() -> None:
     torch.manual_seed(0)
 
     d_model, nhead, dim_feedforward = 8, 2, 3
-    batch_size, tgt_length, memory_length = 4, 16, 20
     xpos_base = 100
-    shift_size = tgt_length
 
-    model = LEXTransformerDecoderLayer(
-        d_model,
-        nhead,
-        dim_feedforward,
-        xpos_base=xpos_base,
-        batch_first=batch_first,
+    with pytest.warns(DeprecationWarning) as e:
+        _ = LEXTransformerDecoderLayer(
+            d_model,
+            nhead,
+            dim_feedforward,
+            xpos_base=xpos_base,
+        )
+
+    assert len(e.list) > 0
+    assert str(e.list[-1].message) == expected_warning_message.format(
+        class_name="LEXTransformerDecoderLayer"
     )
 
-    # to deactivate dropout
-    model.eval()
 
-    if batch_first:
-        tgt = torch.randn((batch_size, tgt_length, d_model))
-        memory = torch.randn((batch_size, memory_length, d_model))
-    else:
-        tgt = torch.randn((tgt_length, batch_size, d_model))
-        memory = torch.randn((memory_length, batch_size, d_model))
-
-    output = model(tgt, memory)
-
-    if batch_first:
-        random_tgt_padding = torch.randn((batch_size, shift_size, d_model))
-        random_memory_padding = torch.randn((batch_size, shift_size, d_model))
-        padded_tgt = torch.cat([random_tgt_padding, tgt], dim=1)
-        padded_memory = torch.cat([random_memory_padding, memory], dim=1)
-    else:
-        random_tgt_padding = torch.randn((shift_size, batch_size, d_model))
-        random_memory_padding = torch.randn((shift_size, batch_size, d_model))
-        padded_tgt = torch.cat([random_tgt_padding, tgt], dim=0)
-        padded_memory = torch.cat([random_memory_padding, memory], dim=0)
-
-    tgt_key_padding_mask = torch.arange(tgt_length + shift_size) < shift_size
-    memory_key_padding_mask = torch.arange(memory_length + shift_size) < shift_size
-    tgt_key_padding_mask = tgt_key_padding_mask.expand((batch_size, tgt_length + shift_size))
-    memory_key_padding_mask = memory_key_padding_mask.expand(
-        (batch_size, memory_length + shift_size)
-    )
-
-    padded_output = model(
-        padded_tgt,
-        padded_memory,
-        tgt_key_padding_mask=tgt_key_padding_mask,
-        memory_key_padding_mask=memory_key_padding_mask,
-    )
-
-    if batch_first:
-        _, padded_output = torch.split(padded_output, [shift_size, tgt_length], dim=1)
-    else:
-        _, padded_output = torch.split(padded_output, [shift_size, tgt_length], dim=0)
-
-    allclose(output, padded_output, atol=1e-5)
-
-
-@pytest.mark.parametrize("batch_first", [True, False])
-def test_lextransformer_encoder(batch_first: bool) -> None:
+def test_legacy_lextransformer_encoder() -> None:
     torch.manual_seed(0)
 
     d_model, nhead, dim_feedforward = 8, 2, 3
     num_layers = 5
-    batch_size, src_length = 4, 16
     xpos_base = 100
-    shift_size = src_length
 
-    model = LEXTransformerEncoder(
-        d_model,
-        nhead,
-        num_layers=num_layers,
-        dim_feedforward=dim_feedforward,
-        xpos_base=xpos_base,
-        batch_first=batch_first,
+    with pytest.warns(DeprecationWarning) as e:
+        _ = LEXTransformerEncoder(
+            d_model,
+            nhead,
+            num_layers=num_layers,
+            dim_feedforward=dim_feedforward,
+            xpos_base=xpos_base,
+        )
+
+    assert len(e.list) > 0
+    assert str(e.list[-1].message) == expected_warning_message.format(
+        class_name="LEXTransformerEncoder"
     )
 
-    # to deactivate dropout
-    model.eval()
 
-    if batch_first:
-        src = torch.randn((batch_size, src_length, d_model))
-    else:
-        src = torch.randn((src_length, batch_size, d_model))
-
-    output = model(src)
-
-    if batch_first:
-        random_padding = torch.randn((batch_size, shift_size, d_model))
-        padded_src = torch.cat([random_padding, src], dim=1)
-    else:
-        random_padding = torch.randn((shift_size, batch_size, d_model))
-        padded_src = torch.cat([random_padding, src], dim=0)
-
-    src_key_padding_mask = torch.arange(src_length + shift_size) < shift_size
-    src_key_padding_mask = src_key_padding_mask.expand((batch_size, src_length + shift_size))
-
-    padded_output = model(padded_src, src_key_padding_mask=src_key_padding_mask)
-
-    if batch_first:
-        _, padded_output = torch.split(padded_output, [shift_size, src_length], dim=1)
-    else:
-        _, padded_output = torch.split(padded_output, [shift_size, src_length], dim=0)
-
-    allclose(output, padded_output, atol=1e-3)
-
-
-@pytest.mark.parametrize("batch_first", [True, False])
-def test_lextransformer_decoder(batch_first: bool) -> None:
+def test_legacy_lextransformer_decoder() -> None:
     torch.manual_seed(0)
 
     d_model, nhead, dim_feedforward = 8, 2, 3
     num_layers = 5
-    batch_size, tgt_length, memory_length = 4, 16, 20
     xpos_base = 100
-    shift_size = tgt_length
 
-    model = LEXTransformerDecoder(
-        d_model,
-        nhead,
-        num_layers=num_layers,
-        dim_feedforward=dim_feedforward,
-        xpos_base=xpos_base,
-        batch_first=batch_first,
+    with pytest.warns(DeprecationWarning) as e:
+        _ = LEXTransformerDecoder(
+            d_model,
+            nhead,
+            num_layers=num_layers,
+            dim_feedforward=dim_feedforward,
+            xpos_base=xpos_base,
+        )
+
+    assert len(e.list) > 0
+    assert str(e.list[-1].message) == expected_warning_message.format(
+        class_name="LEXTransformerDecoder"
     )
-
-    # to deactivate dropout
-    model.eval()
-
-    if batch_first:
-        tgt = torch.randn((batch_size, tgt_length, d_model))
-        memory = torch.randn((batch_size, memory_length, d_model))
-    else:
-        tgt = torch.randn((tgt_length, batch_size, d_model))
-        memory = torch.randn((memory_length, batch_size, d_model))
-
-    output = model(tgt, memory)
-
-    if batch_first:
-        random_tgt_padding = torch.randn((batch_size, shift_size, d_model))
-        random_memory_padding = torch.randn((batch_size, shift_size, d_model))
-        padded_tgt = torch.cat([random_tgt_padding, tgt], dim=1)
-        padded_memory = torch.cat([random_memory_padding, memory], dim=1)
-    else:
-        random_tgt_padding = torch.randn((shift_size, batch_size, d_model))
-        random_memory_padding = torch.randn((shift_size, batch_size, d_model))
-        padded_tgt = torch.cat([random_tgt_padding, tgt], dim=0)
-        padded_memory = torch.cat([random_memory_padding, memory], dim=0)
-
-    tgt_key_padding_mask = torch.arange(tgt_length + shift_size) < shift_size
-    memory_key_padding_mask = torch.arange(memory_length + shift_size) < shift_size
-    tgt_key_padding_mask = tgt_key_padding_mask.expand((batch_size, tgt_length + shift_size))
-    memory_key_padding_mask = memory_key_padding_mask.expand(
-        (batch_size, memory_length + shift_size)
-    )
-
-    padded_output = model(
-        padded_tgt,
-        padded_memory,
-        tgt_key_padding_mask=tgt_key_padding_mask,
-        memory_key_padding_mask=memory_key_padding_mask,
-    )
-
-    if batch_first:
-        _, padded_output = torch.split(padded_output, [shift_size, tgt_length], dim=1)
-    else:
-        _, padded_output = torch.split(padded_output, [shift_size, tgt_length], dim=0)
-
-    allclose(output, padded_output, atol=1e-5)
