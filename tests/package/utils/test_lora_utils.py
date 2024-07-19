cb02eb30389369e41fabf50be66de334f93ec5a0
tky823 <tky823.main+github@gmail.com>
2024-07-19 22:22:01 +0900

    [tests] Add test of apply_lora

--- tests/package/utils/test_lora_utils.py
+++ tests/package/utils/test_lora_utils.py
@@ -1,12 +1,16 @@
 import pytest
+import torch
 import torch.nn as nn
+from dummy import allclose
 
 from audyn.modules.lora import LoRALinear
 from audyn.utils.modules.lora import apply_lora
 
 
 @pytest.mark.parametrize("persistent", [True, False])
-def test_lora_linear(persistent: bool) -> None:
+def test_appy_lora(persistent: bool) -> None:
+    torch.manual_seed(0)
+
     rank = 2
 
     model = nn.Sequential(
@@ -41,3 +45,34 @@ def test_lora_linear(persistent: bool) -> None:
 
     for p, p_expected in zip(model.parameters(), expected_model.parameters()):
         assert p.size() == p_expected.size()
+
+
+@pytest.mark.parametrize("batch_first", [True, False])
+def test_appy_lora_to_transformer_encoder(batch_first: bool) -> None:
+    torch.manual_seed(0)
+
+    d_model, dim_feedforward = 24, 16
+    nhead = 4
+    batch_size = 5
+    length = 10
+
+    model = nn.TransformerEncoderLayer(
+        d_model,
+        nhead,
+        dim_feedforward=dim_feedforward,
+        batch_first=batch_first,
+    )
+    lora_model = apply_lora(model)
+
+    input = torch.randn((length, batch_size, d_model))
+
+    if batch_first:
+        input = input.transpose(1, 0)
+
+    model.eval()
+    lora_model.eval()
+
+    output = model(input)
+    lora_output = lora_model(input)
+
+    allclose(lora_output, output)
