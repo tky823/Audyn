92d160450fdfd63bb7828230ee988afc496a3e05
tky823 <tky823.main+github@gmail.com>
2024-07-20 11:49:05 +0900

    [src] Add random sampler for MUSDB18

---
+++ audyn/utils/data/musdb18/sampler.py
@@ -0,0 +1,43 @@
+from typing import Iterator, List, Optional
+
+from torch.utils.data import RandomSampler
+
+__all__ = [
+    "RandomStemsMUSDB18Sampler",
+]
+
+
+class RandomStemsMUSDB18Sampler(RandomSampler):
+    def __init__(
+        self,
+        track_names: List[str],
+        replacement: bool = True,
+        num_samples: Optional[int] = None,
+        generator=None,
+    ) -> None:
+        from . import sources
+
+        if num_samples is None:
+            num_samples = len(sources) * len(track_names)
+        else:
+            num_samples = len(sources) * num_samples
+
+        super().__init__(
+            track_names,
+            replacement=replacement,
+            num_samples=num_samples,
+            generator=generator,
+        )
+
+    def __iter__(self) -> Iterator[List[int]]:
+        from . import sources
+
+        indices = []
+
+        for idx in super().__iter__():
+            indices.append(idx)
+
+            if len(indices) >= len(sources):
+                yield indices
+
+                indices = []
