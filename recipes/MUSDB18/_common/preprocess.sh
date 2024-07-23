4ece8bd390d56c8215cc425e0c606f2ffa559f99
tky823 <tky823.main+github@gmail.com>
2024-07-23 20:44:39 +0900

    [recipes] Add preprocessing scripts for MUSDB18

---
+++ recipes/MUSDB18/_common/preprocess.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+stage=0
+stop_stage=0
+
+data_root="../data"
+dump_root="./dump"
+log_dir="./log"
+
+dump_format="musdb18"
+
+preprocess="musdb18"
+data="musdb18"
+
+. ../../_common/parse_options.sh || exit 1;
+
+musdb18_root="${data_root}/MUSDB18"
+
+dump_dir="${dump_root}/${data}"
+list_dir="${dump_dir}/list"
+
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+    echo "Preprocess stage 1: Split data into training/validation/test"
+
+    mkdir -p "${list_dir}"
+
+    for subset in "train" "validation" "test"; do
+        list_path="${list_dir}/${subset}.txt"
+
+        python ../_common/local/save_list.py \
+        --config-dir "./conf" \
+        hydra.run.dir="${log_dir}/$(date +"%Y%m%d-%H%M%S")" \
+        preprocess="${preprocess}" \
+        data="${data}" \
+        preprocess.dump_format="${dump_format}" \
+        preprocess.list_path="${list_path}" \
+        preprocess.musdb18_root="${musdb18_root}" \
+        preprocess.subset="${subset}"
+    done
+fi
