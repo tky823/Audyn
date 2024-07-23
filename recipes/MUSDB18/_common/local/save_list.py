ef8e00aa778a0d4a1d0854c5e668b311a7848251
tky823 <tky823.main+github@gmail.com>
2024-07-23 20:48:07 +0900

    [recipes] Add save_list.py for MUSDB18

---
+++ recipes/MUSDB18/_common/local/save_list.py
@@ -0,0 +1,73 @@
+import glob
+import os
+
+from omegaconf import DictConfig
+
+import audyn
+from audyn.utils import setup_config
+from audyn.utils.data.musdb18 import test_track_names, train_track_names, validation_track_names
+
+
+@audyn.main()
+def main(config: DictConfig) -> None:
+    setup_config(config)
+
+    dump_format = config.preprocess.dump_format
+    list_path = config.preprocess.list_path
+    musdb18_root = config.preprocess.musdb18_root
+    subset = config.preprocess.subset
+
+    assert dump_format == "musdb18", "Only dump_format=musdb18 is supported."
+    assert list_path is not None, "Specify preprocess.list_path."
+    assert subset is not None, "Specify preprocess.subset."
+
+    if subset in ["train", "validation"]:
+        subset_name = "train"
+    elif subset == "test":
+        subset_name = "test"
+    else:
+        raise ValueError(f"{subset} is not supported as subset.")
+
+    paths = sorted(glob.glob(os.path.join(musdb18_root, subset_name, "*")))
+    paths = [path for path in paths if is_included(path, subset=subset)]
+
+    filenames = []
+
+    for path in paths:
+        filename = os.path.basename(path)
+        filename, _ = os.path.splitext(filename)
+        filenames.append(filename)
+
+    with open(list_path, mode="w") as f:
+        for filename in filenames:
+            filename, _ = os.path.splitext(filename)
+            f.write(filename + "\n")
+
+
+def is_included(path: str, subset: str) -> bool:
+    filename = os.path.basename(path)
+
+    if filename.endswith(".stem.mp4"):
+        return False
+
+    if subset == "train":
+        if filename in train_track_names:
+            return True
+        else:
+            return False
+    elif subset == "validation":
+        if filename in validation_track_names:
+            return True
+        else:
+            return False
+    elif subset == "test":
+        if filename in test_track_names:
+            return True
+        else:
+            return False
+
+    return False
+
+
+if __name__ == "__main__":
+    main()
