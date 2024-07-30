4ee50018ea285158c80635e2df2e70341f90debe
tky823 <tky823.main+github@gmail.com>
2024-07-31 01:10:08 +0900

    [src] Remove temporary file for MTG if necessary

--- audyn/bin/download_mtg_jamendo.py
+++ audyn/bin/download_mtg_jamendo.py
@@ -1,4 +1,6 @@
 import os
+import shutil
+import uuid
 from urllib.request import Request, urlopen
 
 from omegaconf import DictConfig
@@ -69,11 +71,12 @@ def main(config: DictConfig) -> None:
         filename = tar_template.format(idx)
         _url = url + filename
         path = os.path.join(output, filename)
+        temp_path = path + str(uuid.uuid4())[:8]
 
         request = Request(_url)
 
         try:
-            with urlopen(request) as response, open(path, "wb") as f:
+            with urlopen(request) as response, open(temp_path, "wb") as f:
                 if IS_TQDM_AVAILABLE:
                     total_size = int(response.headers["Content-Length"])
 
@@ -81,7 +84,12 @@ def main(config: DictConfig) -> None:
                         download_by_response(response, f, chunk_size=chunk_size, pbar=pbar)
                 else:
                     download_by_response(response, f, chunk_size=chunk_size)
-        except Exception as e:
+
+            shutil.move(temp_path, path)
+        except (Exception, KeyboardInterrupt) as e:
+            if os.path.exists(temp_path):
+                os.remove(temp_path)
+
             raise e
 
 
