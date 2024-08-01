7e0243c8bbe77c25d36f34406293e3512519cbe4
tky823 <tky823.main+github@gmail.com>
2024-08-02 00:26:28 +0900

    [src] Add StackedConvNeXtBlock1d

--- audyn/modules/convnext.py
+++ audyn/modules/convnext.py
@@ -3,7 +3,8 @@
 The main purpose of the implementation is Vocos and WaveNext.
 """
 
-from typing import Callable, Optional, Union
+import copy
+from typing import Callable, List, Optional, Union
 
 import torch
 import torch.nn as nn
@@ -14,8 +15,83 @@ from torch.nn.modules.utils import _single
 from ..models.roformer import _get_activation
 
 __all__ = [
+    "StackedConvNeXtBlock1d",
     "ConvNeXtBlock1d",
 ]
+
+
+class StackedConvNeXtBlock1d(nn.Module):
+    """Stacked ConvNeXt blocks.
+
+    Args:
+        num_features (int): Number of features.
+        hidden_channels (int): Number of hidden channels.
+        kernel_size (_size_1_t): Kernel size in depthwise convolutions.
+        norm (nn.Module): Layer normalization module, which takes
+            (batch_size, length, num_features) such as ``nn.LayerNorm``.
+        activation (str, nn.Module or callable): Activation module.
+        num_blocks (int): Number of blocks. This parameter also defines the output scale
+            of each block. Default: ``12``.
+
+    """
+
+    def __init__(
+        self,
+        num_features: int,
+        hidden_channels: int,
+        kernel_size: Union[List[_size_1_t], _size_1_t],
+        norm: nn.Module,
+        activation: Union[str, nn.Module, Callable[[torch.Tensor], torch.Tensor]] = "gelu",
+        num_blocks: int = 12,
+    ) -> None:
+        super().__init__()
+
+        self.num_blocks = num_blocks
+
+        if type(kernel_size) in [int, tuple]:
+            kernel_size = [kernel_size] * num_blocks
+        else:
+            assert len(kernel_size) == num_blocks
+
+        backbone = []
+        _scale = 1 / num_blocks
+
+        for block_idx in range(num_blocks):
+            _kernel_size = kernel_size[block_idx]
+            _norm = copy.deepcopy(norm)
+            _activation = copy.deepcopy(activation)
+            block = ConvNeXtBlock1d(
+                num_features,
+                hidden_channels,
+                kernel_size=_kernel_size,
+                norm=_norm,
+                activation=_activation,
+                scale=_scale,
+            )
+            backbone.append(block)
+
+        self.backbone = nn.ModuleList(backbone)
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        """Forward pass of StackedConvNeXtBlock1d.
+
+        Args:
+            input (torch.Tensor): Input feature of shape (batch_size, num_features, length).
+
+        Returns:
+            torch.Tensor: Output feature of shape (batch_size, num_features, length).
+
+        """
+        num_blocks = self.num_blocks
+
+        x = input
+
+        for stack_idx in range(num_blocks):
+            x = self.backbone[stack_idx](x)
+
+        output = x
+
+        return output
 
 
 class ConvNeXtBlock1d(nn.Module):
