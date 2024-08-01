476971c8541bf8ada8b72af1df5cb1090cc5b3e9
tky823 <tky823.main+github@gmail.com>
2024-08-02 01:20:46 +0900

    [tests] Add test of norm=layer_norm in ConvNeXtBlock1d

--- tests/package/modules/test_convnext.py
+++ tests/package/modules/test_convnext.py
@@ -1,3 +1,4 @@
+import pytest
 import torch
 import torch.nn as nn
 
@@ -28,12 +29,18 @@ def test_stacked_convnext_block() -> None:
     assert output.size() == input.size()
 
 
-def test_convnext_block() -> None:
+@pytest.mark.parametrize("is_str_norm", [True, False])
+def test_convnext_block(is_str_norm: bool) -> None:
     torch.manual_seed(0)
 
     num_features, bottleneck_channels = 3, 5
     kernel_size = 7
-    norm = nn.LayerNorm(num_features)
+
+    if is_str_norm:
+        norm = "layer_norm"
+    else:
+        norm = nn.LayerNorm(num_features)
+
     activation = "gelu"
     scale = 1 / 6
     batch_size, length = 4, 32
