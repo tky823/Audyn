10621220a9369639b1b89e8b801f87bf7bbbb6be
tky823 <tky823.main+github@gmail.com>
2024-08-02 00:17:32 +0900

    [tests] Add test of ConvNeXtBlock1d

---
+++ tests/package/modules/test_convnext.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+
+from audyn.modules.convnext import ConvNeXtBlock1d
+
+
+def test_convnext_block() -> None:
+    torch.manual_seed(0)
+
+    num_features, hidden_channels = 3, 5
+    kernel_size = 7
+    norm = nn.LayerNorm(num_features)
+    activation = "gelu"
+    scale = 1 / 6
+    batch_size, length = 4, 32
+
+    module = ConvNeXtBlock1d(
+        num_features,
+        hidden_channels,
+        kernel_size,
+        norm,
+        activation=activation,
+        scale=scale,
+    )
+    input = torch.randn((batch_size, num_features, length))
+    output = module(input)
+
+    assert output.size() == input.size()
