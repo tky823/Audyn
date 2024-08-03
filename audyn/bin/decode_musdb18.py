c4e504d9a9cc948d765e056e7ef653ef937c41e9
tky823 <tky823.main+github@gmail.com>
2024-08-04 00:38:17 +0900

    [src] Support validation by audyn-decode-musdb18

--- audyn/bin/decode_musdb18.py
+++ audyn/bin/decode_musdb18.py
@@ -2,12 +2,25 @@ import glob
 import os
 import shutil
 import tempfile
+from typing import Optional
 
 from omegaconf import DictConfig
 from torchaudio.io import StreamReader, StreamWriter
 
-from ..utils.data.musdb18 import sources
+from ..utils.data.musdb18 import (
+    sources,
+    test_track_names,
+    train_track_names,
+    validation_track_names,
+)
 from ..utils.hydra import main as audyn_main
+
+try:
+    from tqdm import tqdm
+
+    IS_TQDM_AVAILABLE = True
+except ImportError:
+    IS_TQDM_AVAILABLE = False
 
 
 @audyn_main(config_name="decode-musdb18")
@@ -88,24 +101,61 @@ def main(config: DictConfig) -> None:
                     mp4_dir = os.path.join(mp4_root, _subset)
                     wav_dir = os.path.join(wav_root, _subset)
 
-                    decode_folder(mp4_dir, wav_dir, chunk_size=chunk_size)
+                    if _subset == "validation":
+                        mp4_dir = os.path.join(mp4_root, "train")
+
+                    decode_folder(mp4_dir, wav_dir, subset=_subset, chunk_size=chunk_size)
             else:
                 mp4_dir = os.path.join(mp4_root, subset)
                 wav_dir = os.path.join(wav_root, subset)
 
-                decode_folder(mp4_dir, wav_dir, chunk_size=chunk_size)
+                if subset == "validation":
+                    mp4_dir = os.path.join(mp4_root, "train")
+
+                decode_folder(mp4_dir, wav_dir, subset=subset, chunk_size=chunk_size)
 
 
-def decode_folder(mp4_dir: str, wav_dir: str, chunk_size: int = 4096) -> None:
+def decode_folder(
+    mp4_dir: str, wav_dir: str, subset: Optional[str] = None, chunk_size: int = 4096
+) -> None:
     """Decode .stem.mp4 files under and encode them as .wav files under wav_dir."""
-    mp4_paths = sorted(glob.glob(os.path.join(mp4_dir, "*")))
+    mp4_paths = sorted(glob.glob(os.path.join(mp4_dir, "*.stem.mp4")))
+    track_names = []
 
     for mp4_path in mp4_paths:
         track_name = os.path.basename(mp4_path)
         track_name = track_name.replace(".stem.mp4", "")
+
+        if subset is None:
+            track_names.append(subset)
+        else:
+            if subset == "train":
+                if track_name not in train_track_names:
+                    continue
+            elif subset == "validation":
+                if track_name not in validation_track_names:
+                    continue
+            elif subset == "test":
+                if track_name not in test_track_names:
+                    continue
+            else:
+                raise ValueError(f"{subset} is not supported as subset.")
+
+            track_names.append(track_name)
+
+    if IS_TQDM_AVAILABLE:
+        pbar = tqdm(track_names)
+    else:
+        pbar = track_names
+
+    for track_name in pbar:
+        mp4_path = os.path.join(mp4_dir, f"{track_name}.stem.mp4")
         wav_path = os.path.join(wav_dir, track_name, "SOURCE.wav")
 
         decode_file(mp4_path, wav_path, chunk_size=chunk_size)
+
+    if IS_TQDM_AVAILABLE:
+        pbar.close()
 
 
 def decode_file(mp4_path: str, wav_path: str, chunk_size: int = 4096) -> None:
