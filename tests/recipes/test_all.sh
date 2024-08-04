172b31cfbe959371d70d64f2e0ba4bdbcfb781dc
tky823 <tky823.main+github@gmail.com>
2024-08-04 11:13:07 +0900

    [tests] Show name of recipe in tests

--- tests/recipes/test_all.sh
+++ tests/recipes/test_all.sh
@@ -5,11 +5,15 @@ set -o pipefail
 
 (
     cd test_parse_run_command/
+    echo "start: test_parse_run_command"
     . ./train.sh
+    echo "end: test_parse_run_command"
     cd -
 )
 (
     cd test_decode_musdb18/
+    echo "start: test_decode_musdb18"
     . ./preprocess.sh
+    echo "end: test_decode_musdb18"
     cd -
 )
