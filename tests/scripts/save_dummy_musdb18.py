d8ea618450e70e6584ea81ceaf0ff9b2f217f477
tky823 <tky823.main+github@gmail.com>
2024-08-10 17:53:25 +0900

    [tests] Save .wav from .mp4

--- tests/scripts/save_dummy_musdb18.py
+++ tests/scripts/save_dummy_musdb18.py
@@ -5,8 +5,7 @@ from argparse import ArgumentParser, Namespace
 from typing import Optional
 
 import torch
-import torchaudio
-from torchaudio.io import StreamWriter
+from torchaudio.io import StreamReader, StreamWriter
 from tqdm import tqdm
 
 from audyn.utils.data.musdb18 import (
@@ -26,8 +25,8 @@ def main() -> None:
     g.manual_seed(0)
 
     num_channels = 2
-    sample_rate = 4410
-    duration = 1.5
+    sample_rate = 44100
+    duration = 1.0
     num_frames = int(duration * sample_rate)
 
     subset_name = "train"
@@ -38,8 +37,8 @@ def main() -> None:
 
         os.makedirs(track_dir, exist_ok=True)
 
-        save_wav_files(track_dir, sample_rate, num_frames, num_channels=num_channels, generator=g)
-        save_mp4_file(mp4_path, sample_rate, num_channels=num_channels)
+        save_mp4_file(mp4_path, sample_rate, num_frames, num_channels=num_channels, generator=g)
+        save_wav_files(track_dir)
 
     subset_name = "validation"
 
@@ -49,8 +48,8 @@ def main() -> None:
 
         os.makedirs(track_dir, exist_ok=True)
 
-        save_wav_files(track_dir, sample_rate, num_frames, num_channels=num_channels, generator=g)
-        save_mp4_file(mp4_path, sample_rate, num_channels=num_channels)
+        save_mp4_file(mp4_path, sample_rate, num_frames, num_channels=num_channels, generator=g)
+        save_wav_files(track_dir)
 
     subset_name = "test"
 
@@ -60,8 +59,8 @@ def main() -> None:
 
         os.makedirs(track_dir, exist_ok=True)
 
-        save_wav_files(track_dir, sample_rate, num_frames, num_channels=num_channels, generator=g)
-        save_mp4_file(mp4_path, sample_rate, num_channels=num_channels)
+        save_mp4_file(mp4_path, sample_rate, num_frames, num_channels=num_channels, generator=g)
+        save_wav_files(track_dir)
 
 
 def parse_args() -> Namespace:
@@ -76,8 +75,38 @@ def parse_args() -> Namespace:
     return parser.parse_args()
 
 
-def save_wav_files(
-    track_dir: str,
+def save_wav_files(track_dir: str) -> None:
+    mp4_path = f"{track_dir}.stem.mp4"
+
+    for stream_idx, source in enumerate(["mixture"] + sources):
+        wav_path = os.path.join(track_dir, f"{source}.wav")
+        reader = StreamReader(mp4_path)
+        writer = StreamWriter(wav_path)
+
+        stream_info = reader.get_src_stream_info(stream_idx)
+        sample_rate = stream_info.sample_rate
+        num_channels = stream_info.num_channels
+
+        assert sample_rate == 44100
+
+        sample_rate = int(sample_rate)
+
+        reader.add_audio_stream(
+            frames_per_chunk=sample_rate,
+            stream_index=stream_idx,
+        )
+        writer.add_audio_stream(
+            sample_rate=sample_rate,
+            num_channels=num_channels,
+        )
+
+        with writer.open():
+            for (chunk,) in reader.stream():
+                writer.write_audio_chunk(0, chunk)
+
+
+def save_mp4_file(
+    path: str,
     sample_rate: int,
     num_frames: int,
     num_channels: int = 2,
@@ -87,42 +116,32 @@ def save_wav_files(
         generator = torch.Generator()
         generator.manual_seed(0)
 
-    os.makedirs(track_dir, exist_ok=True)
+    writer = StreamWriter(path)
 
     mixture = 0
+    waveforms = {}
 
     for source in sources:
-        waveform = torch.randn((num_channels, num_frames), generator=generator)
+        waveform = torch.randn((num_frames, num_channels), generator=generator)
         max_amplitude = torch.max(torch.abs(waveform))
         waveform = 0.1 * (waveform / max_amplitude)
-        wav_path = os.path.join(track_dir, f"{source}.wav")
-        torchaudio.save(wav_path, waveform, sample_rate)
-
+        waveforms[source] = waveform
         mixture = mixture + waveform
 
-    wav_path = os.path.join(track_dir, "mixture.wav")
-    torchaudio.save(wav_path, mixture, sample_rate)
-
-
-def save_mp4_file(
-    path: str,
-    sample_rate: int,
-    num_channels: int = 2,
-) -> None:
-    track_dir, _ = os.path.splitext(path)
-    track_dir, _ = os.path.splitext(track_dir)
-    writer = StreamWriter(path)
+    waveforms["mixture"] = mixture
 
     for _ in ["mixture"] + sources:
         writer.add_audio_stream(
             sample_rate=sample_rate,
             num_channels=num_channels,
+            encoder="aac",
+            encoder_sample_rate=sample_rate,
+            encoder_num_channels=num_channels,
         )
 
     with writer.open():
         for stream_idx, stream in enumerate(["mixture"] + sources):
-            wav_path = os.path.join(track_dir, f"{stream}.wav")
-            waveform, _ = torchaudio.load(wav_path, channels_first=False)
+            waveform = waveforms[stream]
             writer.write_audio_chunk(stream_idx, waveform)
 
 
