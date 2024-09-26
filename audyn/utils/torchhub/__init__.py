d23cc2c9bcba937c87cbf020ad85310248959cbb
tky823 <tky823.main+github@gmail.com>
2024-09-26 09:33:56 +0900

    [src] Add torchhub utils for Music Tagging Transformer

--- audyn/utils/torchhub/__init__.py
+++ audyn/utils/torchhub/__init__.py
@@ -1,4 +1,8 @@
 from .ast import ast_base
+from .music_tagging_transformer import (
+    student_music_tagging_transformer,
+    teacher_music_tagging_transformer,
+)
 from .passt import passt_base
 from .ssast import multitask_ssast_base_400, ssast_base_400
 
@@ -7,4 +11,6 @@ __all__ = [
     "multitask_ssast_base_400",
     "ssast_base_400",
     "passt_base",
+    "teacher_music_tagging_transformer",
+    "student_music_tagging_transformer",
 ]
