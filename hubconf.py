7ff4cc242adc4d495eaef67043e83e7da80e93b7
tky823 <tky823.main+github@gmail.com>
2024-09-26 09:36:29 +0900

    [src] Register Music Tagging Transformer to torchhub

--- hubconf.py
+++ hubconf.py
@@ -1,4 +1,8 @@
 from audyn.utils.torchhub.ast import ast_base
+from audyn.utils.torchhub.music_tagging_transformer import (
+    student_music_tagging_transformer,
+    teacher_music_tagging_transformer,
+)
 from audyn.utils.torchhub.passt import passt_base
 from audyn.utils.torchhub.ssast import multitask_ssast_base_400, ssast_base_400
 
@@ -7,4 +11,6 @@ __all__ = [
     "multitask_ssast_base_400",
     "ssast_base_400",
     "passt_base",
+    "teacher_music_tagging_transformer",
+    "student_music_tagging_transformer",
 ]
