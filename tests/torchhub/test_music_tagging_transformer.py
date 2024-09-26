15a6b7ae329fc76f1caf513093c3f185b0bbd742
tky823 <tky823.main+github@gmail.com>
2024-09-26 20:19:47 +0900

    [tests] Add test of torchhub using MusicTaggingTransformerMelSpectrogram

--- tests/torchhub/test_music_tagging_transformer.py
+++ tests/torchhub/test_music_tagging_transformer.py
@@ -2,13 +2,13 @@ import pytest
 import torch
 from dummy.utils.github import retrieve_git_branch
 
-models = [
+model_parameters = [
     "teacher_music_tagging_transformer",
     "student_music_tagging_transformer",
 ]
 
 
-@pytest.mark.parametrize("model", models)
+@pytest.mark.parametrize("model", model_parameters)
 def test_music_tagging_transformer(model: str) -> None:
     torch.manual_seed(0)
 
@@ -33,3 +33,31 @@ def test_music_tagging_transformer(model: str) -> None:
         output = model(input)
 
     assert output.size() == (batch_size, 10)
+
+
+def test_music_tagging_transformer_melspectrogram() -> None:
+    torch.manual_seed(0)
+
+    branch = retrieve_git_branch()
+
+    repo = "tky823/Audyn"
+    model = "music_tagging_transformer_melspectrogram"
+    batch_size = 4
+    timesteps = 30 * 22050
+    n_bins = 128
+
+    if branch is not None and branch != "main":
+        repo = repo + ":" + branch
+
+    model = torch.hub.load(
+        repo,
+        model,
+        skip_validation=False,
+    )
+
+    input = torch.randn((batch_size, timesteps))
+
+    with torch.no_grad():
+        output = model(input)
+
+    assert output.size()[:2] == (batch_size, n_bins)
