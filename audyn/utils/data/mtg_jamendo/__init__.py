3b23881fc1c515aeb6c1593255c2a8f16873b124
tky823 <tky823.main+github@gmail.com>
2024-09-28 23:48:44 +0900

    [src] Support MTG-Jamendo_all by audyn.utils

--- audyn/utils/data/mtg_jamendo/__init__.py
+++ audyn/utils/data/mtg_jamendo/__init__.py
@@ -1,4 +1,6 @@
 from ._download import (
+    download_all_metadata,
+    download_all_tags,
     download_genre_metadata,
     download_genre_tags,
     download_instrument_metadata,
@@ -25,14 +27,17 @@ __all__ = [
     "supported_tasks",
 ]
 
+all_tags = download_all_tags()
 top50_tags = download_top50_tags()
 genre_tags = download_genre_tags()
 instrument_tags = download_instrument_tags()
 moodtheme_tags = download_moodtheme_tags()
+num_all_tags = len(all_tags)
 num_top50_tags = len(top50_tags)
 num_genre_tags = len(genre_tags)
 num_instrument_tags = len(instrument_tags)
 num_moodtheme_tags = len(moodtheme_tags)
+all_metadata = download_all_metadata()
 top50_metadata = download_top50_metadata()
 genre_metadata = download_genre_metadata()
 instrument_metadata = download_instrument_metadata()
