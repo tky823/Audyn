138c7314afc2afaabeb04a64fa29d4de145902e4
tky823 <tky823.main+github@gmail.com>
2024-09-28 16:19:38 +0900

    [src] Add tags of MTAT datset

---
+++ audyn/utils/data/mtat/_download.py
@@ -0,0 +1,64 @@
+from typing import List
+
+
+def download_tags() -> List[str]:
+    """Download MTAT tags.
+
+    Returns:
+        list: List of tags.
+
+    """
+    tags = [
+        "guitar",
+        "classical",
+        "slow",
+        "techno",
+        "strings",
+        "drums",
+        "electronic",
+        "rock",
+        "fast",
+        "piano",
+        "ambient",
+        "beat",
+        "violin",
+        "vocal",
+        "synth",
+        "female",
+        "indian",
+        "opera",
+        "male",
+        "singing",
+        "vocals",
+        "no vocals",
+        "harpsichord",
+        "loud",
+        "quiet",
+        "flute",
+        "woman",
+        "male vocal",
+        "no vocal",
+        "pop",
+        "soft",
+        "sitar",
+        "solo",
+        "man",
+        "classic",
+        "choir",
+        "voice",
+        "new age",
+        "dance",
+        "male voice",
+        "female vocal",
+        "beats",
+        "harp",
+        "cello",
+        "no voice",
+        "weird",
+        "country",
+        "metal",
+        "female voice",
+        "choral",
+    ]
+
+    return tags
