093884da506d26f5cd2cecae77705dcd433a0a81
tky823 <tky823.main+github@gmail.com>
2024-09-28 16:42:31 +0900

    [src] Implement is_supported for torch.compile

--- audyn/utils/_torch/compile.py
+++ audyn/utils/_torch/compile.py
@@ -1,8 +1,36 @@
+import sys
+
 import torch
+from packaging import version
+
+IS_WINDOWS = sys.platform == "win32"
+IS_PYTHON_GE_3_11 = sys.version_info >= (3, 11)
+IS_TORCH_LT_2_0 = version.parse(torch.__version__) < version.parse("2.0")
+IS_TORCH_LT_2_1 = version.parse(torch.__version__) < version.parse("2.1")
 
 __all__ = [
+    "is_supported",
     "is_gpu_supported",
 ]
+
+
+def is_supported() -> bool:
+    """Judge availability of ``torch.compile``.
+
+    Returns:
+        bool: Whether ``torch.compile`` is available.
+
+    """
+    if IS_WINDOWS:
+        return False
+
+    if IS_TORCH_LT_2_0:
+        return False
+
+    if IS_TORCH_LT_2_1 and IS_PYTHON_GE_3_11:
+        return False
+
+    return True
 
 
 def is_gpu_supported() -> bool:
