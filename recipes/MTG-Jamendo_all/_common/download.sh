b0c1dc075ff2b77f882eee084c0115b07beafc4a
tky823 <tky823.main+github@gmail.com>
2024-09-29 17:57:25 +0900

    [recipes] Define wav_dir for MTG-Jamendo

--- recipes/MTG-Jamendo_all/_common/download.sh
+++ recipes/MTG-Jamendo_all/_common/download.sh
@@ -10,9 +10,10 @@ quality="raw"
 
 . ../../_common/parse_options.sh || exit 1;
 
-mtg_jamendo_root="${data_root}/MTG-Jamendo/audio"
+mtg_jamendo_root="${data_root}/MTG-Jamendo"
+wav_dir="${mtg_jamendo_root}/audio"
 
 audyn-download-mtg-jamendo \
 server_type="${server_type}" \
 quality="${quality}" \
-root="${mtg_jamendo_root}"
+root="${wav_dir}"
--- recipes/MTG-Jamendo_all/_common/preprocess.sh
+++ recipes/MTG-Jamendo_all/_common/preprocess.sh
@@ -18,7 +18,8 @@ data="mtg-jamendo_all"
 
 . ../../_common/parse_options.sh || exit 1;
 
-mtg_jamendo_root="${data_root}/MTG-Jamendo/audio"
+mtg_jamendo_root="${data_root}/MTG-Jamendo"
+wav_dir="${mtg_jamendo_root}/audio"
 
 dump_dir="${dump_root}/${data}/split-${split}"
 list_dir="${dump_dir}/list"
@@ -39,8 +40,24 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
         data="${data}" \
         preprocess.dump_format="${dump_format}" \
         preprocess.list_path="${list_path}" \
-        preprocess.mtg_jamendo_root="${mtg_jamendo_root}" \
+        preprocess.wav_dir="${wav_dir}" \
         preprocess.split=${split} \
         preprocess.subset="${subset}"
+    done
+fi
+
+if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
+    echo "Preprocess stage 2: Save features"
+
+    for subset in "train" "validation" "test"; do
+        python ../_common/local/save_features.py \
+        --config-dir "./conf" \
+        hydra.run.dir="${log_dir}/$(date +"%Y%m%d-%H%M%S")" \
+        preprocess="${preprocess}" \
+        data="${data}" \
+        preprocess.dump_format="${dump_format}" \
+        preprocess.list_path="${list_dir}/${subset}.txt" \
+        preprocess.wav_dir="${wav_dir}" \
+        preprocess.feature_dir="${feature_dir}/${subset}"
     done
 fi
