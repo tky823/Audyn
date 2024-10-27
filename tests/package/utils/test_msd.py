96e51b2c15273cc5b60881f1a84453a78eaeef22
tky823 <tky823.main+github@gmail.com>
2024-10-27 20:34:06 +0900

    [tests] Add test of download_interactions

--- tests/package/utils/test_msd.py
+++ tests/package/utils/test_msd.py
@@ -1,5 +1,10 @@
-from audyn.utils.data.msd import num_tags, tags
+from audyn.utils.data.msd import download_interactions, num_tags, tags
 
 
 def test_msd() -> None:
     assert len(tags) == num_tags
+
+    interactions = download_interactions("user20-track200", subset="validate-visible")
+
+    assert len(interactions) == 50000
+    assert interactions["969cc6fb74e076a68e36a04409cb9d3765757508"]["SOABRAB12A6D4F7AAF"] == 2
