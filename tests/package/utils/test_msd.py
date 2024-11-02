b2fdd6be9de977815af1d4fc1403cc667cb2ed0d
tky823 <tky823.main+github@gmail.com>
2024-11-02 14:40:55 +0900

    [tests] Add test of download_metadata

--- tests/package/utils/test_msd.py
+++ tests/package/utils/test_msd.py
@@ -1,9 +1,16 @@
-from audyn.utils.data.msd_tagging import download_interactions, num_tags, tags
+from audyn.utils.data.msd_recommend import download_interactions
+from audyn.utils.data.msd_tagging import download_metadata, num_tags, tags
 
 
-def test_msd() -> None:
+def test_msd_tagging() -> None:
     assert len(tags) == num_tags
 
+    metadata = download_metadata(subset="labeled-train")
+
+    assert len(metadata) == 163504
+
+
+def test_msd_recommend() -> None:
     interactions = download_interactions("user20-track200", subset="validate-visible")
 
     assert len(interactions) == 50000
