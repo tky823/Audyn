3095de87264d86dfe3751f470787ebc14824df1e
tky823 <tky823.main+github@gmail.com>
2025-01-15 22:00:49 +0900

    [tests] Set DYLD_FALLBACK_LIBRARY_PATH in tests

--- tests/scripts/install_macos_dependencies.sh
+++ tests/scripts/install_macos_dependencies.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-brew install ffmpeg
-brew install libomp
+# FFmpeg
+brew install ffmpeg@6
+homebrew_prefix="$(brew --prefix)"
+echo "DYLD_FALLBACK_LIBRARY_PATH=${homebrew_prefix}/opt/ffmpeg@6/lib" >> "${GITHUB_ENV}"
+echo "${homebrew_prefix}/opt/ffmpeg@6/bin" >> "${GITHUB_PATH}"
 ffmpeg -version
+
+brew install libomp
