1bf803cff989c60daa148462910cac30387284b1
tky823 <tky823.main+github@gmail.com>
2025-01-25 14:19:05 +0900

    [tests] Add test of SwinTransformerEncoderLayer

--- tests/package/modules/test_swintransformer_modules.py
+++ tests/package/modules/test_swintransformer_modules.py
@@ -3,7 +3,48 @@ import torch
 import torch.nn as nn
 from dummy import allclose
 
-from audyn.modules.swintransformer import SwinRelativePositionalMultiheadAttention
+from audyn.modules.swintransformer import (
+    SwinRelativePositionalMultiheadAttention,
+    SwinTransformerEncoderLayer,
+)
+
+
+@pytest.mark.parametrize("bias", [True, False])
+@pytest.mark.parametrize("batch_first", [True, False])
+@pytest.mark.parametrize("share_heads", [True, False])
+def test_swin_transformer_encoder_layer(bias: bool, batch_first: bool, share_heads: bool) -> None:
+    torch.manual_seed(0)
+
+    batch_size = 5
+    embed_dim = 8
+    dim_feedforward = 16
+    num_heads = 4
+    window_height, window_width = 5, 7
+    height, width = 4 * window_height, 4 * window_width
+
+    if batch_first:
+        input = torch.randn((batch_size, height * width, embed_dim))
+    else:
+        input = torch.randn((height * width, batch_size, embed_dim))
+
+    model = SwinTransformerEncoderLayer(
+        embed_dim,
+        num_heads,
+        dim_feedforward=dim_feedforward,
+        bias=bias,
+        height=height,
+        width=width,
+        window_size=(window_height, window_width),
+        share_heads=share_heads,
+        batch_first=batch_first,
+    )
+
+    output = model(input)
+
+    if batch_first:
+        assert output.size() == (batch_size, height * width, embed_dim)
+    else:
+        assert output.size() == (height * width, batch_size, embed_dim)
 
 
 @pytest.mark.parametrize("bias", [True, False])
@@ -13,14 +54,14 @@ def test_swin_relative_positional_attn(bias: bool, batch_first: bool, share_head
     torch.manual_seed(0)
 
     batch_size = 3
-    height, width = 5, 7
     embed_dim = 8
     num_heads = 4
+    window_height, window_width = 5, 7
 
     if batch_first:
-        input = torch.randn((batch_size, height * width, embed_dim))
+        input = torch.randn((batch_size, window_height * window_width, embed_dim))
     else:
-        input = torch.randn((height * width, batch_size, embed_dim))
+        input = torch.randn((window_height * window_width, batch_size, embed_dim))
 
     mha = nn.MultiheadAttention(
         embed_dim,
@@ -32,7 +73,7 @@ def test_swin_relative_positional_attn(bias: bool, batch_first: bool, share_head
         embed_dim,
         num_heads,
         bias=bias,
-        window_size=(height, width),
+        window_size=(window_height, window_width),
         share_heads=share_heads,
         batch_first=batch_first,
     )
@@ -56,11 +97,15 @@ def test_swin_relative_positional_attn(bias: bool, batch_first: bool, share_head
     relative_output, relative_attn_weights = relative_mha(input, input, input)
 
     if batch_first:
-        assert relative_output.size() == (batch_size, height * width, embed_dim)
+        assert relative_output.size() == (batch_size, window_height * window_width, embed_dim)
     else:
-        assert relative_output.size() == (height * width, batch_size, embed_dim)
+        assert relative_output.size() == (window_height * window_width, batch_size, embed_dim)
 
-    assert relative_attn_weights.size() == (batch_size, height * width, height * width)
+    assert relative_attn_weights.size() == (
+        batch_size,
+        window_height * window_width,
+        window_height * window_width,
+    )
 
     allclose(output, relative_output, atol=1e-7)
     allclose(attn_weights, relative_attn_weights)
