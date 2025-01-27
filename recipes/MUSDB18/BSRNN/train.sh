159a7b8d774bf8e8870a40457e77ce0c9e6ce00e
tky823 <tky823.main+github@gmail.com>
2025-01-27 18:32:16 +0900

    [recipes] Add train.sh for BSRNN

---
+++ recipes/MUSDB18/BSRNN/train.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+dump_root="./dump"
+exp_root="./exp"
+tensorboard_root="./tensorboard"
+
+tag=""
+continue_from=""
+
+dump_format="torch"
+
+system="default"
+preprocess="musdb18"
+data="musdb18"
+train="bsrnn"
+model="bsrnn-v7"
+optimizer="bsrnn"
+lr_scheduler="bsrnn"
+criterion="bsrnn"
+
+. ../../_common/parse_options.sh || exit 1;
+
+dump_dir="${dump_root}/${data}"
+list_dir="${dump_dir}/list"
+feature_dir="${dump_dir}/feature"
+
+if [ -z "${tag}" ]; then
+    tag=$(date +"%Y%m%d-%H%M%S")
+fi
+
+exp_dir="${exp_root}/${tag}"
+tensorboard_dir="${tensorboard_root}/${tag}"
+
+cmd=$(
+    audyn-parse-run-command \
+    --config-dir "./conf" \
+    hydra.run.dir="./log/$(date +"%Y%m%d-%H%M%S")" \
+    system="${system}"
+)
+
+${cmd} ./local/train.py \
+--config-dir "./conf" \
+hydra.run.dir="${exp_dir}/log/$(date +"%Y%m%d-%H%M%S")" \
+system="${system}" \
+preprocess="${preprocess}" \
+data="${data}" \
+train="${train}" \
+model="${model}" \
+optimizer="${optimizer}" \
+lr_scheduler="${lr_scheduler}" \
+criterion="${criterion}" \
+preprocess.dump_format="${dump_format}" \
+train.dataset.train.list_path="${list_dir}/train.txt" \
+train.dataset.train.feature_dir="${feature_dir}/train" \
+train.dataset.validate.list_path="${list_dir}/validate.txt" \
+train.dataset.validate.feature_dir="${feature_dir}/validate" \
+train.resume.continue_from="${continue_from}" \
+train.output.exp_dir="${exp_dir}" \
+train.output.tensorboard_dir="${tensorboard_dir}"
