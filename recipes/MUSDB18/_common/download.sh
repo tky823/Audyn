f021cc2f7ff50d9e424028a8372739fbd5466197
tky823 <tky823.main+github@gmail.com>
2025-01-28 16:30:14 +0900

    [recipes] Use audyn-download-musdb18

--- recipes/MUSDB18/_common/download.sh
+++ recipes/MUSDB18/_common/download.sh
@@ -3,29 +3,21 @@
 set -eu
 set -o pipefail
 
-musdb18_url="https://zenodo.org/records/1117372/files/musdb18.zip"
 data_root="../data"
 
 . ../../_common/parse_options.sh || exit 1;
 
-musdb18_filename=$(basename "${musdb18_url}")
+type="default"  # for MUSDB18
+subset="all"
+
 musdb18_root="${data_root}/MUSDB18"
 
-mkdir -p "${data_root}"
-
-if [ ! -e "${data_root}/${musdb18_filename}" ]; then
-    wget "${musdb18_url}" -P "${data_root}"
-else
-    echo "${data_root}/${musdb18_filename} already exists."
-fi
-
-if [ ! -e "${musdb18_root}/train/A Classic Education - NightOwl.stem.mp4" ]; then
-    unzip "${data_root}/${musdb18_filename}" -d "${musdb18_root}"
-else
-    echo "${musdb18_root} already exists."
-fi
+audyn-download-musdb18 \
+type="${type}" \
+root="${data_root}" \
+musdb18_root="${musdb18_root}"
 
 audyn-decode-musdb18 \
 mp4_root="${musdb18_root}" \
 wav_root="${musdb18_root}" \
-subset="all"
+subset="${subset}"
