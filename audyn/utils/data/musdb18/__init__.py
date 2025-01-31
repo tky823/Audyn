fcc4467acb699c3e9555e050aa076863461f4640
tky823 <tky823.main+github@gmail.com>
2025-01-31 11:25:28 +0900

    [src] Remove track_names from audyn.utils.data.musdb18

--- audyn/utils/data/musdb18/__init__.py
+++ audyn/utils/data/musdb18/__init__.py
@@ -2,7 +2,7 @@ from ._download import download_track_names
 from .dataset import MUSDB18, RandomStemsMUSDB18Dataset, StemsMUSDB18Dataset
 
 __all__ = [
-    "track_names",
+    "all_track_names",
     "train_track_names",
     "validation_track_names",
     "test_track_names",
@@ -14,8 +14,7 @@ __all__ = [
     "RandomStemsMUSDB18Dataset",
 ]
 
-track_names = download_track_names()
-all_track_names = track_names
+all_track_names = download_track_names()
 train_track_names = download_track_names(subset="train")
 validation_track_names = download_track_names(subset="validation")
 test_track_names = download_track_names(subset="test")
