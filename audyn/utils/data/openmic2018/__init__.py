ba001c197776f567daefbd2a1300729729e64d92
tky823 <tky823.main+github@gmail.com>
2025-02-02 01:04:39 +0900

    [src] Add audyn.utils.data.openmic2018

---
+++ audyn/utils/data/openmic2018/__init__.py
@@ -0,0 +1,7 @@
+from ._download import download_all_tags
+
+__all__ = [
+    "all_tags",
+]
+
+all_tags = download_all_tags()
---
+++ audyn/utils/data/openmic2018/_download.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+def download_all_tags() -> List[str]:
+    """Download all instrument tags."""
+    tags = [
+        "accordion",
+        "banjo",
+        "bass",
+        "cello",
+        "clarinet",
+        "cymbals",
+        "drums",
+        "flute",
+        "guitar",
+        "mallet_percussion",
+        "mandolin",
+        "organ",
+        "piano",
+        "saxophone",
+        "synthesizer",
+        "trombone",
+        "trumpet",
+        "ukulele",
+        "violin",
+        "voice",
+    ]
+
+    return tags
