f74e0718e296433e31ae7fe8efc9a0f3b750047f
tky823 <tky823.main+github@gmail.com>
2025-02-01 20:07:28 +0900

    [tests] Add test of RandomStemsDNRDataset

--- tests/package/utils/test_dnr.py
+++ tests/package/utils/test_dnr.py
@@ -1,11 +1,14 @@
+import glob
 import os
 import tempfile
 import warnings
 from typing import Dict, List
 
+import pytest
 import torch
 import torchaudio
 from dummy import allclose
+from torch.utils.data import DataLoader
 
 from audyn.utils.data.dnr import (
     sources,
@@ -13,7 +16,12 @@ from audyn.utils.data.dnr import (
     v2_train_track_names,
     v2_validation_track_names,
 )
-from audyn.utils.data.dnr.dataset import DNR, Track
+from audyn.utils.data.dnr.dataset import (
+    DNR,
+    RandomStemsDNRDataset,
+    StemsDNRDataset,
+    Track,
+)
 
 
 def test_dnr() -> None:
@@ -47,6 +55,91 @@ def test_dnr() -> None:
             allclose(stems[3], effect)
 
             break
+
+
+@pytest.mark.parametrize("replacement", [True, False])
+@pytest.mark.parametrize("num_workers", [0, 2])
+def test_dnr_dataset(
+    replacement: bool,
+    num_workers: int,
+) -> None:
+    batch_size = 3
+    sample_rate = 24000
+    num_frames = 48000
+    duration = 1.0
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        feature_dir = os.path.join(temp_dir, "features")
+        list_dir = os.path.join(temp_dir, "list")
+        train_feature_dir = os.path.join(feature_dir, "tr")
+        train_list_path = os.path.join(list_dir, "train.txt")
+
+        os.makedirs(feature_dir, exist_ok=True)
+        os.makedirs(list_dir, exist_ok=True)
+
+        _ = _save_dummy_dnr(root=feature_dir, sample_rate=sample_rate, num_frames=num_frames)
+
+        with open(train_list_path, mode="w") as f:
+            for track_name in sorted(glob.glob(os.path.join(train_feature_dir, "*"))):
+                f.write(track_name + "\n")
+
+        if not replacement:
+            # StemsDNRDataset
+            dataset = StemsDNRDataset(train_list_path, train_feature_dir, duration=duration)
+            dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=num_workers)
+
+            filenames = []
+
+            for feature in dataloader:
+                assert set(feature.keys()) == {
+                    "speech",
+                    "music",
+                    "effect",
+                    "sample_rate",
+                    "filename",
+                }
+
+                filenames_per_batch = set()
+
+                for filename in feature["filename"]:
+                    # If ``replacement=True``, filename may be included in filenames_per_batch
+                    # with tiny probability.
+                    assert filename not in filenames_per_batch
+
+                    filenames_per_batch.add(filename)
+                    filenames.append(filename)
+
+            assert len(set(filenames)) == len(dataset.filenames)
+
+        # RandomStemsDNRDataset
+        dataset = RandomStemsDNRDataset(
+            train_list_path, train_feature_dir, duration=duration, replacement=replacement
+        )
+        dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=num_workers)
+
+        filenames = []
+
+        for feature in dataloader:
+            assert set(feature.keys()) == {
+                "speech",
+                "music",
+                "effect",
+                "sample_rate",
+                "filename",
+            }
+
+            filenames_per_batch = set()
+
+            for filename in feature["filename"]:
+                # If ``replacement=True``, filename may be included in filenames_per_batch
+                # with tiny probability.
+                assert filename not in filenames_per_batch
+
+                filenames_per_batch.add(filename)
+                filenames.append(filename)
+
+        if not replacement:
+            assert len(set(filenames)) == len(dataset.filenames)
 
 
 def _save_dummy_dnr(root: str, sample_rate: int, num_frames: int) -> Dict[str, List[int]]:
