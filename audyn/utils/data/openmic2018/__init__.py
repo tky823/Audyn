b6de35feef1d8dde412a6e0cbcc999eb24d28def
tky823 <tky823.main+github@gmail.com>
2025-02-02 10:43:08 +0900

    [src] Download metadata of OpenMIC2018

--- audyn/utils/data/openmic2018/__init__.py
+++ audyn/utils/data/openmic2018/__init__.py
@@ -1,8 +1,9 @@
-from ._download import download_all_tags
+from ._download import download_all_metadata, download_all_tags
 
 __all__ = [
     "all_tags",
     "num_all_tags",
+    "download_all_metadata",
 ]
 
 all_tags = download_all_tags()
