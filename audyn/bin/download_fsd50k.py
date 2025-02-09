b9449e80db56b0256bf2963c11caae63ab7cae3a
tky823 <tky823.main+github@gmail.com>
2025-02-09 13:10:10 +0900

    [src] Add audyn-download-fsd50k

---
+++ audyn/bin/download_fsd50k.py
@@ -0,0 +1,123 @@
+import os
+import shutil
+import tempfile
+import uuid
+import zipfile
+from typing import Optional
+
+from omegaconf import DictConfig
+
+from ..utils._hydra import main as audyn_main
+from ..utils.data.download import download_file
+
+
+@audyn_main(config_name="download-fsd50k")
+def main(config: DictConfig) -> None:
+    """Download FSD50K dataset.
+
+    .. code-block:: shell
+
+        data_root="./data"  # root directory to save .zip file.
+        fsd50k_root="${data_root}/FSD50K"
+        unpack=true  # unpack .zip or not
+        chunk_size=8192  # chunk size in byte to download
+
+        audyn-download-fsd50k \
+        root="${data_root}" \
+        fsd50k_root="${fsd50k_root}" \
+        unpack=${unpack} \
+        chunk_size=${chunk_size}
+
+    """
+    download_fsd50k(config)
+
+
+def download_fsd50k(config: DictConfig) -> None:
+    root = config.root
+    fsd50k_root = config.fsd50k_root
+    unpack = config.unpack
+    chunk_size = config.chunk_size
+
+    url = "https://zenodo.org/records/4060432/files/"
+
+    if root is None:
+        raise ValueError("Set root directory.")
+
+    if unpack is None:
+        unpack = True
+
+    if chunk_size is None:
+        chunk_size = 8192
+
+    if root:
+        os.makedirs(root, exist_ok=True)
+
+    for subset in ["dev", "eval"]:
+        zip_template = f"FSD50K.{subset}_audio.z" + "{}"
+
+        if subset == "dev":
+            num_files = 6
+        elif subset == "eval":
+            num_files = 2
+        else:
+            raise ValueError("Invalid subset is given.")
+
+        for idx in range(1, num_files + 1):
+            if idx == num_files:
+                # "FSD50K.dev_audio.zip"
+                filename = zip_template.format("ip")
+            else:
+                # "FSD50K.dev_audio.z01", ...
+                filename = zip_template.format(f"{idx:02d}")
+
+            _url = url + filename
+            path = os.path.join(root, filename)
+
+            if not os.path.exists(path):
+                _download_fsd50k(_url, path, chunk_size=chunk_size)
+
+    if unpack:
+        for subset in ["dev", "eval"]:
+            zip_template = f"FSD50K.{subset}_audio.z" + "{}"
+            merged_path = zip_template.format(f"{0:02d}")
+
+            with open(merged_path, "wb") as f_out:
+                for idx in range(num_files):
+                    filename = zip_template.format(idx)
+                    path = os.path.join(root, filename)
+
+                    with open(path, "rb") as f_in:
+                        f_out.write(f_in.read())
+
+            root = os.path.dirname(path)
+            default_name = "FSD50K"
+
+            if fsd50k_root is None:
+                fsd50k_root = os.path.join(root, default_name)
+
+            unpacked_root = os.path.join(fsd50k_root, f"FSD50K.{subset}_audio")
+            _unpack_zip(merged_path, unpacked_root=unpacked_root)
+
+
+def _download_fsd50k(url: str, path: str, chunk_size: int = 8192) -> None:
+    temp_path = path + str(uuid.uuid4())[:8]
+
+    try:
+        download_file(url, temp_path, chunk_size=chunk_size)
+        shutil.move(temp_path, path)
+    except (Exception, KeyboardInterrupt) as e:
+        if os.path.exists(temp_path):
+            os.remove(temp_path)
+
+        raise e
+
+
+def _unpack_zip(path: str, unpacked_root: Optional[str] = None) -> None:
+    os.makedirs(unpacked_root, exist_ok=True)
+
+    with zipfile.ZipFile(path) as f, tempfile.TemporaryDirectory() as temp_dir:
+        f.extractall(temp_dir)
+
+        for _filename in os.listdir(temp_dir):
+            _path = os.path.join(temp_dir, _filename)
+            shutil.move(_path, unpacked_root)
