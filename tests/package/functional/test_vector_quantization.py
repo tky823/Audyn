3d6c21f2197d929ae3cdfb89b0b441d48f4bdf6c
tky823 <tky823.main+github@gmail.com>
2025-03-21 23:50:16 +0900

    [tests] Add tests of quantize_residual_vector

--- tests/package/functional/test_vector_quantization.py
+++ tests/package/functional/test_vector_quantization.py
@@ -1,4 +1,5 @@
 import torch
+from dummy import allclose
 
 from audyn.functional.vector_quantization import quantize_residual_vector
 
@@ -15,10 +16,23 @@ def test_quantize_residual_vector_1d() -> None:
 
     # weight is tensor
     weight = torch.randn((num_layers, codebook_size, embedding_dim))
-    quantized, indices = quantize_residual_vector(input, weight)
+    quantized, residual, indices = quantize_residual_vector(input, weight)
 
     assert quantized.size() == (batch_size, num_layers, embedding_dim, length)
+    assert residual.size() == (batch_size, num_layers, embedding_dim, length)
     assert indices.size() == (batch_size, num_layers, length)
+
+    quantized = quantized.transpose(1, 0)
+    residual = residual.transpose(1, 0)
+
+    for layer_idx in range(num_layers):
+        _residual = residual[layer_idx]
+
+        if layer_idx == 0:
+            allclose(_residual, input)
+        else:
+            _quantized = torch.sum(quantized[:layer_idx], dim=0)
+            allclose(_quantized + _residual, input)
 
     # weight is list of tensors
     weight = []
@@ -27,10 +41,23 @@ def test_quantize_residual_vector_1d() -> None:
         _weight = torch.randn((codebook_size + layer_idx, embedding_dim))
         weight.append(_weight)
 
-    quantized, indices = quantize_residual_vector(input, weight)
+    quantized, residual, indices = quantize_residual_vector(input, weight)
 
     assert quantized.size() == (batch_size, num_layers, embedding_dim, length)
+    assert residual.size() == (batch_size, num_layers, embedding_dim, length)
     assert indices.size() == (batch_size, num_layers, length)
+
+    quantized = quantized.transpose(1, 0)
+    residual = residual.transpose(1, 0)
+
+    for layer_idx in range(num_layers):
+        _residual = residual[layer_idx]
+
+        if layer_idx == 0:
+            allclose(_residual, input)
+        else:
+            _quantized = torch.sum(quantized[:layer_idx], dim=0)
+            allclose(_quantized + _residual, input)
 
 
 def test_quantize_residual_vector_2d() -> None:
@@ -45,10 +72,23 @@ def test_quantize_residual_vector_2d() -> None:
 
     # weight is tensor
     weight = torch.randn((num_layers, codebook_size, embedding_dim))
-    quantized, indices = quantize_residual_vector(input, weight)
+    quantized, residual, indices = quantize_residual_vector(input, weight)
 
     assert quantized.size() == (batch_size, num_layers, embedding_dim, height, width)
+    assert residual.size() == (batch_size, num_layers, embedding_dim, height, width)
     assert indices.size() == (batch_size, num_layers, height, width)
+
+    quantized = quantized.transpose(1, 0)
+    residual = residual.transpose(1, 0)
+
+    for layer_idx in range(num_layers):
+        _residual = residual[layer_idx]
+
+        if layer_idx == 0:
+            allclose(_residual, input)
+        else:
+            _quantized = torch.sum(quantized[:layer_idx], dim=0)
+            allclose(_quantized + _residual, input, atol=1e-7)
 
     # weight is list of tensors
     weight = []
@@ -57,7 +97,20 @@ def test_quantize_residual_vector_2d() -> None:
         _weight = torch.randn((codebook_size + layer_idx, embedding_dim))
         weight.append(_weight)
 
-    quantized, indices = quantize_residual_vector(input, weight)
+    quantized, residual, indices = quantize_residual_vector(input, weight)
 
     assert quantized.size() == (batch_size, num_layers, embedding_dim, height, width)
+    assert residual.size() == (batch_size, num_layers, embedding_dim, height, width)
     assert indices.size() == (batch_size, num_layers, height, width)
+
+    quantized = quantized.transpose(1, 0)
+    residual = residual.transpose(1, 0)
+
+    for layer_idx in range(num_layers):
+        _residual = residual[layer_idx]
+
+        if layer_idx == 0:
+            allclose(_residual, input)
+        else:
+            _quantized = torch.sum(quantized[:layer_idx], dim=0)
+            allclose(_quantized + _residual, input, atol=1e-7)
