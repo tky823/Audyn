7e78174b5af080250a837e07977ddd6ae50757ce
tky823 <tky823.main+github@gmail.com>
2025-03-22 14:02:03 +0900

    [recipes] Add sampler and dataloader for GumbelVQVAE

---
+++ recipes/MNIST/PixelCNN+VQVAE/utils/sampler.py
@@ -0,0 +1,29 @@
+import torch
+from torch.utils.data.sampler import RandomSampler
+
+from .dataset import GumbelMNIST
+
+
+class GumbelVQVAERandomSampler(RandomSampler):
+
+    def __init__(
+        self,
+        data_source: GumbelMNIST,
+        replacement: bool = False,
+        num_samples: int = None,
+        generator: torch.Generator | None = None,
+    ) -> None:
+        assert isinstance(
+            data_source, GumbelMNIST
+        ), "Only GumbelMNIST is supported as data_source."
+
+        super().__init__(
+            data_source=data_source,
+            replacement=replacement,
+            num_samples=num_samples,
+            generator=generator,
+        )
+
+    def set_epoch(self, epoch: int) -> None:
+        self.data_source: GumbelMNIST
+        self.data_source.set_epoch(epoch)
