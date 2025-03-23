703c95edc11dca0bf7d11d9631d3fe1b10af4f54
tky823 <tky823.main+github@gmail.com>
2025-03-23 10:18:59 +0900

    [recipes] Use lr-scheduler

--- recipes/MNIST/PixelCNN+GumbelVQVAE/run.sh
+++ recipes/MNIST/PixelCNN+GumbelVQVAE/run.sh
@@ -55,7 +55,7 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
         --test "${test}" \
         --model "${model}" \
         --optimizer "${optimizer}" \
-        --lr_scheduler "${lr_scheduler}" \
+        --lr-scheduler "${lr_scheduler}" \
         --criterion "${criterion}"
     )
 fi
@@ -74,48 +74,6 @@ if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
         --preprocess "${preprocess}" \
         --data "${data}" \
         --train "${train}" \
-        --model "${model}"
-    )
-fi
-
-if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
-    echo "Stage 3: Training of PixelCNN"
-
-    (
-        . ./train_pixelcnn.sh \
-        --tag "${tag}" \
-        --continue-from "${continue_from}" \
-        --exp-root "${exp_root}" \
-        --tensorboard-root "${tensorboard_root}" \
-        --data-root "${data_root}" \
-        --dump-format "${dump_format}" \
-        --system "${system}" \
-        --preprocess "${preprocess}" \
-        --data "${data}" \
-        --train "${train}" \
-        --test "${test}" \
-        --model "${model}" \
-        --optimizer "${optimizer}" \
-        --lr_scheduler "${lr_scheduler}" \
-        --criterion "${criterion}"
-    )
-fi
-
-if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
-    echo "Stage 4: Generate images by PixelCNN + VQVAE"
-
-    (
-        . ./test.sh \
-        --tag "${tag}" \
-        --exp-root "${exp_root}" \
-        --pixelcnn-checkpoint "${pixelcnn_checkpoint}" \
-        --vqvae-checkpoint "${vqvae_checkpoint}" \
-        --data-root "${data_root}" \
-        --dump-format "${dump_format}" \
-        --system "${system}" \
-        --preprocess "${preprocess}" \
-        --data "${data}" \
-        --test "${test}" \
         --model "${model}"
     )
 fi
