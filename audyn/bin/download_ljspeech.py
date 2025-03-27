818cb01f4078c640cd804cfa61ac6fcec26a3fcf
tky823 <tky823.main+github@gmail.com>
2025-03-27 20:54:38 +0900

    [docs] Fix description of audyn-download-ljspeech

--- audyn/bin/download_ljspeech.py
+++ audyn/bin/download_ljspeech.py
@@ -17,9 +17,9 @@ def main(config: DictConfig) -> None:
 
     .. code-block:: shell
 
-        data_root="./data"  # root directory to save .zip file.
+        data_root="./data"  # root directory to save .tar.bz2 file.
         ljspeech_root="${data_root}/LJSpeech-1.1"
-        unpack=true  # unpack .zip or not
+        unpack=true  # unpack .tar.bz2 or not
         chunk_size=8192  # chunk size in byte to download
 
         audyn-download-ljspeech \
