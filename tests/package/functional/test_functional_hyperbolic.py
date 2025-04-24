7c6c198d23f5e376bd5c5b3641799acfc798ebc4
tky823 <tky823.main+github@gmail.com>
2025-04-24 21:12:17 +0900

    [tests] Add tests of mobius_sub

--- tests/package/functional/test_functional_hyperbolic.py
+++ tests/package/functional/test_functional_hyperbolic.py
@@ -2,7 +2,7 @@ import pytest
 import torch
 from dummy import allclose
 
-from audyn.functional.hyperbolic import mobius_add
+from audyn.functional.hyperbolic import mobius_add, mobius_sub
 
 
 @pytest.mark.parametrize("curvature", [-1, -2])
@@ -18,10 +18,34 @@ def test_mobius_add(curvature: float) -> None:
     output_by_scaler_curvature = mobius_add(input, other, curvature=curvature)
 
     curvature0 = torch.tensor([curvature])
-    curvature1 = 2 * torch.rand((batch_size - 1,)) - 2
+    curvature1 = 2 * torch.rand((batch_size - 1,)) - 3
     curvature = torch.cat([curvature0, curvature1], dim=0)
 
     output_by_tensor_curvature = mobius_add(input, other, curvature=curvature)
+
+    output_by_scaler_curvature, *_ = torch.unbind(output_by_scaler_curvature, dim=0)
+    output_by_tensor_curvature, *_ = torch.unbind(output_by_tensor_curvature, dim=0)
+
+    allclose(output_by_scaler_curvature, output_by_tensor_curvature)
+
+
+@pytest.mark.parametrize("curvature", [-1, -2])
+def test_mobius_sub(curvature: float) -> None:
+    torch.manual_seed(0)
+
+    batch_size = 10
+    num_features = 5
+
+    input = torch.rand((batch_size, num_features)) - 0.5
+    other = torch.rand((batch_size, num_features)) - 0.5
+
+    output_by_scaler_curvature = mobius_sub(input, other, curvature=curvature)
+
+    curvature0 = torch.tensor([curvature])
+    curvature1 = 2 * torch.rand((batch_size - 1,)) - 3
+    curvature = torch.cat([curvature0, curvature1], dim=0)
+
+    output_by_tensor_curvature = mobius_sub(input, other, curvature=curvature)
 
     output_by_scaler_curvature, *_ = torch.unbind(output_by_scaler_curvature, dim=0)
     output_by_tensor_curvature, *_ = torch.unbind(output_by_tensor_curvature, dim=0)
