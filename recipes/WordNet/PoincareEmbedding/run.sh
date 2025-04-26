593f14a17344f4c6831aaf2d393eb34a219fb23f
tky823 <tky823.main+github@gmail.com>
2025-04-27 01:07:49 +0900

    [recipes] Add run.sh of WordNet

---
+++ recipes/WordNet/PoincareEmbedding/run.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+stage=-1
+stop_stage=-1
+
+tag=""
+continue_from=""
+
+exp_root="./exp"
+tensorboard_root="./tensorboard"
+
+data_root="../data"
+dump_root="dump"
+
+dump_format="wordnet"
+
+system="default"
+preprocess="default"
+data="wordnet-mammal"
+train="poincare_embedding"
+model="poincare_embedding"
+optimizer="poincare_embedding"
+lr_scheduler="poincare_embedding"
+criterion="poincare_negative_sampling"
+
+. ../../_common/parse_options.sh || exit 1;
+
+if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
+    echo "Stage 0: Preprocessing"
+
+    (
+        . ./preprocess.sh \
+        --stage 1 \
+        --stop-stage 1 \
+        --data-root "${data_root}" \
+        --dump-root "${dump_root}" \
+        --dump-format "${dump_format}" \
+        --preprocess "${preprocess}" \
+        --data "${data}"
+    )
+fi
