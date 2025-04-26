9b8b09e6ea509cc29717b33a0a468689f4bae1bf
tky823 <tky823.main+github@gmail.com>
2025-04-26 21:28:33 +0900

    [src] Add indexer for WordNet

--- audyn/utils/data/wordnet/__init__.py
+++ audyn/utils/data/wordnet/__init__.py
@@ -1,10 +1,12 @@
 from ._download import load_mammal_name_to_index
 from .dataloader import WordNetDataLoader
 from .dataset import EvaluationMammalDataset, TrainingMammalDataset
+from .indexer import WordNetIndexer
 
 __all__ = [
     "TrainingMammalDataset",
     "EvaluationMammalDataset",
     "WordNetDataLoader",
+    "WordNetIndexer",
     "load_mammal_name_to_index",
 ]
---
+++ audyn/utils/data/wordnet/indexer.py
@@ -0,0 +1,79 @@
+from typing import List, Union
+
+
+class WordNetIndexer:
+    """Indexer to map name into index.
+
+    Args:
+        name_to_index (dict): Dictionary to map name to index.
+
+    Examples:
+
+        >>> from audyn.utils.data.wordnet import WordNetIndexer, load_mammal_name_to_index
+        >>> name_to_index = load_mammal_name_to_index()
+        >>> indexer = WordNetIndexer(name_to_index)
+        >>> indexer("mammal.n.01")
+        637
+        >>> indexer(["mammal.n.01", "dog.n.01"])
+        [637, 305]
+
+    """
+
+    def __init__(self, name_to_index: dict[str, int]) -> None:
+        super().__init__()
+
+        index_to_name = {}
+
+        for name, index in name_to_index.items():
+            assert index not in index_to_name
+
+            index_to_name[index] = name
+
+        self.name_to_index = name_to_index
+        self.index_to_name = index_to_name
+
+    def __call__(self, name_or_names: Union[str, List[str]]) -> Union[int, List[int]]:
+        index = self.encode(name_or_names)
+
+        return index
+
+    def __len__(self) -> int:
+        return len(self.name_to_index)
+
+    def encode(self, name_or_names: Union[str, List[str]]) -> Union[int, List[int]]:
+        index = self._name_to_index(name_or_names)
+
+        return index
+
+    def decode(self, index_or_indices: Union[int, List[int]]) -> Union[str, List[str]]:
+        name = self._index_to_name(index_or_indices)
+
+        return name
+
+    def _name_to_index(self, name_or_names: Union[str, List[str]]) -> Union[int, List[int]]:
+        if isinstance(name_or_names, str):
+            index = self.name_to_index[name_or_names]
+
+            return index
+        else:
+            indices = []
+
+            for name in name_or_names:
+                index = self._name_to_index(name)
+                indices.append(index)
+
+            return indices
+
+    def _index_to_name(self, index_or_indices: int | list[int]) -> str | list[str]:
+        if isinstance(index_or_indices, int):
+            name = self.index_to_name[index_or_indices]
+
+            return name
+        else:
+            names = []
+
+            for index in index_or_indices:
+                name = self._index_to_name(index)
+                names.append(name)
+
+            return names
