fb01959a05b0cba53b5955e5d13804d278811e77
tky823 <tky823.main+github@gmail.com>
2025-04-27 10:34:30 +0900

    [tests] Add NegativeSamplingPoincareEmbedding

--- tests/package/modules/test_negative_sampling.py
+++ tests/package/modules/test_negative_sampling.py
@@ -1,6 +1,7 @@
 import torch
 
 from audyn.modules import PoincareEmbedding
+from audyn.modules.manifold import NegativeSamplingPoincareEmbedding
 from audyn.modules.negative_sampling import NegativeSamplingModel
 
 
@@ -11,11 +12,27 @@ def test_negative_sampling_model() -> None:
     num_neg_samples = 2
     num_embeddings, embedding_dim = 10, 2
 
+    anchor = torch.randint(0, num_embeddings, (batch_size,), dtype=torch.long)
+    positive = torch.randint(0, num_embeddings, (batch_size,), dtype=torch.long)
+    negative = torch.randint(0, num_embeddings, (batch_size, num_neg_samples), dtype=torch.long)
+
+    model = NegativeSamplingPoincareEmbedding(
+        num_embeddings=num_embeddings,
+        embedding_dim=embedding_dim,
+    )
+
+    anchor, positive, negative = model(anchor, positive, negative)
+
+    assert anchor.size() == (batch_size, embedding_dim)
+    assert positive.size() == (batch_size, embedding_dim)
+    assert negative.size() == (batch_size, num_neg_samples, embedding_dim)
+
     embedding = PoincareEmbedding(
         num_embeddings=num_embeddings,
         embedding_dim=embedding_dim,
     )
     model = NegativeSamplingModel(embedding)
+
     anchor = torch.randint(0, num_embeddings, (batch_size,), dtype=torch.long)
     positive = torch.randint(0, num_embeddings, (batch_size,), dtype=torch.long)
     negative = torch.randint(0, num_embeddings, (batch_size, num_neg_samples), dtype=torch.long)
