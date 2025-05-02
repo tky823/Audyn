f41a1a54b07c8d6dcbd755324cc9a6403b97b674
tky823 <tky823.main+github@gmail.com>
2025-05-02 20:59:45 +0900

    [recipes] Add modules of AudioSet to __all__

--- recipes/AudioSet/PoincareEmbedding/utils/__init__.py
+++ recipes/AudioSet/PoincareEmbedding/utils/__init__.py
@@ -1,7 +1,10 @@
 from ._download import download_audioset_name_to_index, download_audioset_tags
+from .indexer import AudioSetIndexer
 
 __all__ = [
+    "AudioSetIndexer",
     "tags",
+    "name_to_index",
     "num_tags",
 ]
 
