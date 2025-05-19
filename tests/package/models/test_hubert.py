4a12994f843c02aef68f87799bca52c41416f1db
tky823 <tky823.main+github@gmail.com>
2025-05-20 00:09:10 +0900

    [src] Update config of HuBERT

--- tests/package/models/test_hubert.py
+++ tests/package/models/test_hubert.py
@@ -2,7 +2,7 @@ import os
 import tempfile
 
 import torch
-from dummy import allclose
+from audyn_test import allclose
 
 from audyn.models.hubert import HuBERT
 from audyn.utils._github import download_file_from_github_release
@@ -22,10 +22,27 @@ def test_hubert() -> None:
         )
         waveform = data["input"]
         expected_output = data["output"]
+        expected_embedding = data["embedding"]
 
     model.eval()
 
     waveform = waveform.unsqueeze(dim=0)
+
+    with torch.no_grad():
+        embedding = model.embedding(waveform)
+
+    embedding = embedding.squeeze(dim=0)
+
+    allclose(embedding, expected_embedding, atol=1e-4)
+
+    embedding = expected_embedding.unsqueeze(dim=0)
+
+    with torch.no_grad():
+        output = model.backbone(embedding)
+
+    output = output.squeeze(dim=0)
+
+    allclose(output, expected_output, atol=1e-3)
 
     with torch.no_grad():
         output = model(waveform)
