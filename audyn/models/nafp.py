0439ea8f99e108a832aabaf2e6c8bd97c9828ed4
tky823 <tky823.main+github@gmail.com>
2025-05-25 01:08:32 +0900

    [src] Add audio fingerprinters

---
+++ audyn/models/nafp.py
@@ -0,0 +1,65 @@
+from typing import Tuple
+
+import torch
+import torch.nn as nn
+
+__all__ = [
+    "NeuralAudioFingerprinter",
+    "ContrastiveNeuralAudioFingerprinter",
+]
+
+
+class NeuralAudioFingerprinter(nn.Module):
+    """Neural Audio Fingerprinter.
+
+    Args:
+        backbone (nn.Module): Backbone for feature extraction.
+        projection (nn.Module): Projection for embedding.
+
+    """
+
+    def __init__(self, backbone: nn.Module, projection: nn.Module) -> None:
+        super().__init__()
+
+        self.backbone = backbone
+        self.projection = projection
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        """Forward pass of NeuralAudioFingerprinter.
+
+        Args:
+            input (torch.Tensor): Spectrogram-like feature of shape (*, n_bins, n_frames).
+
+        Returns:
+            torch.Tensor: Fingerprint of shape (*, embedding_dim).
+
+        """
+        x = self.backbone(input)
+        output = self.projection(x)
+
+        return output
+
+
+class ContrastiveNeuralAudioFingerprinter(NeuralAudioFingerprinter):
+    """Wrapper class of NeuralAudioFingerprinter for contrastive learning."""
+
+    def forward(
+        self, input: torch.Tensor, other: torch.Tensor
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        """Forward pass of ContrastiveNeuralAudioFingerprinter.
+
+        Args:
+            input (torch.Tensor): Spectrogram-like feature of shape (*, n_bins, n_frames).
+            other (torch.Tensor): Augmented feature of shape (*, n_bins, n_frames).
+
+        Returns:
+            tuple: Tuple of torch.Tensor containing:
+                - torch.Tensor: Fingerprint of shape (*, embedding_dim).
+                - torch.Tensor: Fingerprint for augmented feature of shape (*, embedding_dim).
+
+        """
+        x = torch.cat([input, other], dim=0)
+        x = super().forward(x)
+        output, output_other = torch.split(x, 2, dim=0)
+
+        return output, output_other
