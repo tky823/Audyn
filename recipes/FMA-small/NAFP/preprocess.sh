406b44cc47445c44505a8e0071822868ac8d3f52
tky823 <tky823.main+github@gmail.com>
2025-05-25 00:01:19 +0900

    [recipes] Add preprocess.sh

---
+++ recipes/FMA-small/NAFP/preprocess.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+stage=0
+stop_stage=0
+
+data_root="../data"
+dump_root="./dump"
+log_dir="./log"
+
+dump_format="webdataset"
+
+preprocess="fma"
+data="fma-small"
+
+. ../../_common/parse_options.sh || exit 1;
+
+fma_type="small"
+fma_root="${data_root}/FMA/${fma_type}"
+
+dump_dir="${dump_root}/${data}"
+list_dir="${dump_dir}/list"
+feature_dir="${dump_dir}/feature"
+
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+    echo "Preprocess stage 1: Split data into training/validation/evaluation"
+
+    mkdir -p "${list_dir}"
+
+    for subset in "train" "validation" "test"; do
+        list_path="${list_dir}/${subset}.txt"
+
+        python ./local/save_list.py \
+        --config-dir "./conf" \
+        hydra.run.dir="${log_dir}/$(date +"%Y%m%d-%H%M%S")" \
+        preprocess="${preprocess}" \
+        data="${data}" \
+        preprocess.dump_format="${dump_format}" \
+        preprocess.list_path="${list_path}" \
+        preprocess.fma_root="${fma_root}" \
+        preprocess.type="${fma_type}" \
+        preprocess.subset="${subset}"
+    done
+fi
