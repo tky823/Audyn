cb038d70e3ac291ea0bca1682b9a8b6c12ee9e25
tky823 <tky823.main+github@gmail.com>
2025-06-01 23:08:30 +0900

    [src] Use CLIP-modules in AST

--- audyn/models/ast.py
+++ audyn/models/ast.py
@@ -1,7 +1,6 @@
 import copy
 import os
 import warnings
-from abc import abstractmethod
 from collections import OrderedDict
 from typing import Dict, Optional, Tuple, Union
 
@@ -11,6 +10,7 @@ import torch.nn.functional as F
 from omegaconf import OmegaConf
 from torch.nn.common_types import _size_2_t
 
+from ..models.clip import Aggregator, Head, HeadTokensAggregator
 from ..modules.ast import PositionalPatchEmbedding
 from ..utils._github import download_file_from_github_release
 
@@ -486,28 +486,6 @@ class AudioSpectrogramTransformer(BaseAudioSpectrogramTransformer):
         return output
 
 
-class Aggregator(nn.Module):
-    """Base class of module to aggregate features."""
-
-    @abstractmethod
-    def forward(
-        self,
-        input: torch.Tensor,
-        padding_mask: Optional[torch.BoolTensor] = None,
-    ) -> torch.Tensor:
-        """Forward pass of Aggregator.
-
-        Args:
-            input (torch.Tensor): Sequence of shape (batch_size, length, embedding_dim).
-            padding_mask (torch.BoolTensor, optional): Padding mask of shape (batch_size, length).
-
-        Returns:
-            torch.Tensor: Aggregated feature of shape (batch_size, embedding_dim).
-
-        """
-        pass
-
-
 class AverageAggregator(Aggregator):
     """Module of aggregation by average operation.
 
@@ -571,86 +549,6 @@ class AverageAggregator(Aggregator):
         output = x.sum(dim=-2) / non_padding_mask.sum(dim=-1, keepdim=True)
 
         return output
-
-
-class HeadTokensAggregator(Aggregator):
-    """Module of aggregation by extraction of head tokens.
-
-    Args:
-        insert_cls_token (bool): Given sequence is assumed to contain [CLS] token.
-        insert_dist_token (bool): Given sequence is assumed to contain [DIST] token.
-
-    """
-
-    def __init__(
-        self,
-        insert_cls_token: bool = True,
-        insert_dist_token: bool = True,
-    ) -> None:
-        super().__init__()
-
-        if not insert_cls_token and not insert_dist_token:
-            raise ValueError(
-                "At least one of insert_cls_token and insert_dist_token should be True."
-            )
-
-        self.insert_cls_token = insert_cls_token
-        self.insert_dist_token = insert_dist_token
-
-    def forward(
-        self,
-        input: torch.Tensor,
-        padding_mask: Optional[torch.BoolTensor] = None,
-    ) -> torch.Tensor:
-        """Forward pass of HeadTokensAggregator.
-
-        Args:
-            input (torch.Tensor): Sequence of shape (batch_size, length, embedding_dim).
-            padding_mask (torch.BoolTensor, optional): Padding mask of shape (batch_size, length).
-
-        Returns:
-            torch.Tensor: Aggregated feature of shape (batch_size, embedding_dim).
-
-        .. note::
-
-            padding_mask is ignored.
-
-        """
-        num_head_tokens = 0
-
-        if self.insert_cls_token:
-            num_head_tokens += 1
-
-        if self.insert_dist_token:
-            num_head_tokens += 1
-
-        head_tokens, _ = torch.split(
-            input, [num_head_tokens, input.size(-2) - num_head_tokens], dim=-2
-        )
-        output = torch.mean(head_tokens, dim=-2)
-
-        return output
-
-    def extra_repr(self) -> str:
-        s = []
-
-        if self.insert_cls_token:
-            s.append("cls_token=True")
-
-        if self.insert_dist_token:
-            s.append("dist_token=True")
-
-        s = ", ".join(s)
-
-        return s
-
-
-class Head(nn.Module):
-    """Base class of Head module to transform aggregated feature."""
-
-    @abstractmethod
-    def forward(self, input: torch.Tensor) -> torch.Tensor:
-        pass
 
 
 class MLPHead(Head):
