bf6363e26a13f05c4fdd37fcd071285df920b1a4
tky823 <tky823.main+github@gmail.com>
2025-10-11 19:19:27 +0900

    [tests] Use audyn-download-musdb18 in tests

--- tests/recipes/test_decode_musdb18/preprocess.sh
+++ tests/recipes/test_decode_musdb18/preprocess.sh
@@ -13,26 +13,15 @@ fi
 musdb18_7s_url="https://zenodo.org/api/files/1ff52183-071a-4a59-923f-7a31c4762d43/MUSDB18-7-STEMS.zip"
 data_root="./data"
 
-musdb18_7s_filename=$(basename "${musdb18_7s_url}")
-musdb18_7s_root="${data_root}/MUSDB18-7s"
+type="7s"
+musdb18_root="${data_root}/MUSDB18-7s"
 
-subset_names=(train test)
-
-mkdir -p "${data_root}"
-
-if [ ! -e "${data_root}/${musdb18_7s_filename}" ]; then
-    wget "${musdb18_7s_url}" -P "${data_root}"
-else
-    echo "${data_root}/${musdb18_7s_filename} already exists."
-fi
-
-if [ ! -e "${musdb18_7s_root}/train/A Classic Education - NightOwl.stem.mp4" ]; then
-    unzip "${data_root}/${musdb18_7s_filename}" -d "${musdb18_7s_root}"
-else
-    echo "${musdb18_7s_root} already exists."
-fi
+audyn-download-musdb18 \
+type="${type}" \
+root="${data_root}" \
+musdb18_root="${musdb18_root}" \
 
 audyn-decode-musdb18 \
-mp4_root="${musdb18_7s_root}" \
-wav_root="${musdb18_7s_root}" \
+mp4_root="${musdb18_root}" \
+wav_root="${musdb18_root}" \
 subset="validation"
