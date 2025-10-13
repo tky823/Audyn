48b08b4187704aef6f11c1f1c31ef57c30016a7f
tky823 <tky823.main+github@gmail.com>
2025-10-13 16:52:39 +0900

    Revert "[tests] Verify path to ffmpeg"
    This reverts commit 3404f7f1a557300db80165fec57b650166fef5cf.

--- tests/scripts/install_windows_dependencies.ps1
+++ tests/scripts/install_windows_dependencies.ps1
@@ -6,4 +6,3 @@ echo "${env:USERPROFILE}\.uv\bin" >> ${env:GITHUB_PATH}
 choco install ffmpeg --version=6.1.0
 echo "C:\ProgramData\chocolatey\bin" >> ${env:GITHUB_PATH}
 ffmpeg -version
-ls "C:\ProgramData\chocolatey\bin"
