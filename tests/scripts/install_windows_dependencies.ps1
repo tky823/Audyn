d7aaea195a0ae6d575cef75a6aa42b719e2e7cde
tky823 <tky823.main+github@gmail.com>
2025-10-19 16:15:00 +0900

    [tests] Merge installation scripts for Windows

--- tests/scripts/install_windows_dependencies.ps1
+++ tests/scripts/install_windows_dependencies.ps1
@@ -3,3 +3,8 @@ $env:UV_NO_MODIFY_PATH = "1"
 $env:UV_INSTALL_DIR = "${env:USERPROFILE}\.uv\bin"
 irm https://astral.sh/uv/install.ps1 | iex
 echo "${env:UV_INSTALL_DIR}" >> ${env:GITHUB_PATH}
+
+# FFmpeg
+choco install ffmpeg --version=6.1.0
+echo "C:\ProgramData\chocolatey\bin" >> ${env:GITHUB_PATH}
+ffmpeg -version
--- tests/scripts/install_windows_ffmpeg_dependencies.ps1
+++
@@ -1,4 +0,0 @@
-# FFmpeg
-choco install ffmpeg --version=6.1.0
-echo "C:\ProgramData\chocolatey\bin" >> ${env:GITHUB_PATH}
-ffmpeg -version
