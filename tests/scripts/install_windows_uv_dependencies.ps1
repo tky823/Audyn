b63f808f8ad2595f4bc69797d4bf618114d233dc
tky823 <tky823.main+github@gmail.com>
2025-10-19 16:08:06 +0900

    [tests] Set UV_INSTALL_DIR for Windows

--- tests/scripts/install_windows_uv_dependencies.ps1
+++ tests/scripts/install_windows_uv_dependencies.ps1
@@ -1,4 +1,5 @@
 # uv
 $env:UV_NO_MODIFY_PATH = "1"
+$env:UV_INSTALL_DIR = "${env:USERPROFILE}\.uv\bin"
 irm https://astral.sh/uv/install.ps1 | iex
-echo "${env:USERPROFILE}\.uv\bin" >> ${env:GITHUB_PATH}
+echo "${env:UV_INSTALL_DIR}" >> ${env:GITHUB_PATH}
