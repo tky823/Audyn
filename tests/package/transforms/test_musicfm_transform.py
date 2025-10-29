ffe18855d20728bbfc3273857591857ffb938445
tky823 <tky823.main+github@gmail.com>
2025-10-29 21:07:29 +0900

    [tests] Add tests for MusicFM

---
+++ tests/package/transforms/test_musicfm_transform.py
@@ -0,0 +1,25 @@
+import os
+
+import torch
+from audyn_test import allclose
+from audyn_test.utils import audyn_test_cache_dir
+
+from audyn.transforms.musicfm import MusicFMMelSpectrogram
+from audyn.utils._github import download_file_from_github_release
+
+
+def test_musicfm_melspectrogram_transform() -> None:
+    # regression test
+    url = "https://github.com/tky823/Audyn/releases/download/v0.2.0/test_official_musicfm.pth"  # noqa: E501
+    path = os.path.join(audyn_test_cache_dir, "test_official_musicfm.pth")
+    download_file_from_github_release(url, path)
+
+    data = torch.load(path, weights_only=True)
+
+    waveform = data["waveform"]
+    expected_melspectrogram = data["spectrogram"]
+
+    transform = MusicFMMelSpectrogram.build_from_pretrained()
+    melspectrogram = transform(waveform)
+
+    allclose(melspectrogram, expected_melspectrogram)
