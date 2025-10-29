13d35a8d823af1fa2133170bd5d99ef09469d5cc
tky823 <tky823.main+github@gmail.com>
2025-10-29 21:39:10 +0900

    [tests] Add torchhub test for MusicFM

---
+++ tests/torchhub/test_musicfm.py
@@ -0,0 +1,45 @@
+import pytest
+import torch
+from audyn_test.utils.github import retrieve_git_branch
+
+dataset_parameters = [
+    "msd",
+]
+
+
+@pytest.mark.parametrize("dataset", dataset_parameters)
+def test_music_tagging_transformer(dataset: str) -> None:
+    torch.manual_seed(0)
+
+    branch = retrieve_git_branch()
+
+    repo = "tky823/Audyn"
+    model = "musicfm"
+    transform = "musicfm_melspectrogram"
+    batch_size = 4
+    timesteps = 10 * 24000
+    n_bins = 128
+
+    if branch is not None and branch != "main":
+        repo = repo + ":" + branch
+
+    transform = torch.hub.load(
+        repo,
+        transform,
+        skip_validation=False,
+    )
+    model = torch.hub.load(
+        repo,
+        model,
+        skip_validation=False,
+        dataset=dataset,
+    )
+
+    input = torch.randn((batch_size, timesteps))
+
+    with torch.no_grad():
+        spectrogram = transform(input)
+        output = model(spectrogram)
+
+    assert spectrogram.size()[:2] == (batch_size, n_bins)
+    assert output.size() == (batch_size, timesteps // 960, 4096)
