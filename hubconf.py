b28fe615b74549e2bdcdad4b82551cf7dbe02eb4
tky823 <tky823.main+github@gmail.com>
2025-10-29 21:27:20 +0900

    [src] Add MusicFM to hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -3,6 +3,7 @@ from audyn.utils._torchhub.music_tagging_transformer import (
     music_tagging_transformer,
     music_tagging_transformer_melspectrogram,
 )
+from audyn.utils._torchhub.musicfm import musicfm, musicfm_melspectrogram
 from audyn.utils._torchhub.passt import passt_base
 from audyn.utils._torchhub.ssast import multitask_ssast_base_400, ssast_base_400
 
@@ -13,4 +14,6 @@ __all__ = [
     "passt_base",
     "music_tagging_transformer",
     "music_tagging_transformer_melspectrogram",
+    "musicfm",
+    "musicfm_melspectrogram",
 ]
