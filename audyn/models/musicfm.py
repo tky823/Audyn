3ac52a89f49c89ca5a94df1fc29f1f51e5cbb12a
tky823 <tky823.main+github@gmail.com>
2025-10-29 09:54:51 +0900

    [src] Support aggregator&head by MusicFM

--- audyn/models/musicfm.py
+++ audyn/models/musicfm.py
@@ -1,13 +1,32 @@
+import warnings
+from typing import Optional
+
 import torch
 import torch.nn as nn
 
 
 class MusicFM(nn.Module):
-    def __init__(self, embedding: nn.Module, backbone: nn.Module) -> None:
+    def __init__(
+        self,
+        embedding: nn.Module,
+        backbone: nn.Module,
+        aggregator: Optional[nn.Module] = None,
+        head: Optional[nn.Module] = None,
+    ) -> None:
         super().__init__()
 
         self.embedding = embedding
         self.backbone = backbone
+        self.aggregator = aggregator
+        self.head = head
+
+        if self.aggregator is None and self.head is not None:
+            warnings.warn(
+                "Head is given, but aggregator is not given, "
+                "which may lead to unexpected behavior.",
+                UserWarning,
+                stacklevel=2,
+            )
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         if input.dim() == 3:
@@ -19,5 +38,11 @@ class MusicFM(nn.Module):
 
         x = self.embedding(x)
         output = self.backbone(x)
+
+        if self.aggregator is not None:
+            output = self.aggregator(output)
+
+        if self.head is not None:
+            output = self.head(output)
 
         return output
