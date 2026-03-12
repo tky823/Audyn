a1244118caacfa028296f49e7afe980d6edd20bf
tky823 <tky823.main+github@gmail.com>
2026-03-13 00:05:47 +0900

    [tests] Overwrite torchcodec dependency by script

--- tests/scripts/is_torchcodec_required.py
+++ tests/scripts/is_torchcodec_required.py
@@ -1,12 +1,30 @@
+from argparse import ArgumentParser, Namespace
+
 import torch
 from packaging import version
 
 
 def main() -> None:
-    if version.parse(torch.__version__) >= version.parse("2.9"):
-        print("true")
+    args = parse_args()
+
+    torch_version = version.parse(torch.__version__)
+
+    if torch_version >= version.parse("2.9"):
+        torchcodec_version = "0" + "." + torch_version.minor
+
+        with open(args.path, mode="w") as f:
+            f.write("torchcodec==" + torchcodec_version)
     else:
-        print("false")
+        with open(args.path, mode="w") as f:
+            pass
+
+
+def parse_args() -> Namespace:
+    parser = ArgumentParser(description="Overwrite pyptoject.toml to specify version of torch.")
+
+    parser.add_argument("--path", type=str, help="Path to requirements.txt")
+
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
