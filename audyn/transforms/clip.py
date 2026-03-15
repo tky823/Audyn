854f2e1e0ebcf5a22dcdef8a2d1c3e91a0a1c32a
tky823 <tky823.main+github@gmail.com>
2026-03-15 10:45:38 +0900

    [src] Support torchvision < 0.16

--- audyn/transforms/clip.py
+++ audyn/transforms/clip.py
@@ -1,7 +1,10 @@
 import torch
 import torch.nn as nn
 import torchvision.transforms.v2 as vT
+from packaging import version
 from PIL.Image import Image
+
+IS_TORCHVISION_LT_0_16 = version.parse(vT.__version__) < version.parse("0.16")
 
 
 class OpenAICLIPImageTransform(nn.Module):
@@ -11,13 +14,22 @@ class OpenAICLIPImageTransform(nn.Module):
         transforms = [
             vT.Resize(size, interpolation=vT.InterpolationMode.BICUBIC),
             vT.CenterCrop(size),
-            vT.ToImage(),
-            vT.ToDtype(torch.float32, scale=True),
+        ]
+
+        if IS_TORCHVISION_LT_0_16:
+            transforms.append(vT.ToTensor())
+            transforms.append(vT.ConvertImageDtype(torch.float32))
+        else:
+            transforms.append(vT.ToImage())
+            transforms.append(vT.ToDtype(torch.float32, scale=True))
+
+        transforms.append(
             vT.Normalize(
                 [0.48145466, 0.4578275, 0.40821073],
                 [0.26862954, 0.26130258, 0.27577711],
-            ),
-        ]
+            )
+        )
+
         self.transforms = vT.Compose(transforms)
 
     def forward(self, input: torch.Tensor | Image) -> torch.Tensor:
